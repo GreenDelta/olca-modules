0d2f2f072d2de0ef6e5b8912e577aec8c76a937b
msrocka <michael.srocka@gmail.com>
2013-08-06 17:37:02 +0200

    use cache in analysis flow result generator

--- olca-core/src/main/java/org/openlca/core/results/AnalysisFlowResults.java
+++ olca-core/src/main/java/org/openlca/core/results/AnalysisFlowResults.java
@@ -1,8 +1,13 @@
 package org.openlca.core.results;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
+import org.openlca.core.database.Cache;
+import org.openlca.core.indices.FlowIndex;
+import org.openlca.core.indices.LongPair;
+import org.openlca.core.indices.ProductIndex;
 import org.openlca.core.model.descriptors.FlowDescriptor;
 import org.openlca.core.model.descriptors.ProcessDescriptor;
 
@@ -15,20 +20,44 @@ public final class AnalysisFlowResults {
 	private AnalysisFlowResults() {
 	}
 
+	public static List<FlowDescriptor> getFlows(AnalysisResult result,
+			Cache cache) {
+		FlowIndex flowIndex = result.getFlowIndex();
+		List<FlowDescriptor> flows = new ArrayList<>();
+		for (int i = 0; i < flowIndex.size(); i++) {
+			long flowId = flowIndex.getFlowAt(i);
+			FlowDescriptor flow = cache.getFlowDescriptor(flowId);
+			if (flow != null)
+				flows.add(flow);
+		}
+		return flows;
+	}
+
 	public static List<AnalysisFlowResult> getForFlow(AnalysisResult result,
-			FlowDescriptor flow, List<ProcessDescriptor> processes) {
+			FlowDescriptor flow, Cache cache) {
 		List<AnalysisFlowResult> results = new ArrayList<>();
-		for (ProcessDescriptor process : processes) {
+		ProductIndex index = result.getProductIndex();
+		HashSet<Long> handled = new HashSet<>();
+		for (int i = 0; i < index.size(); i++) {
+			LongPair processProduct = index.getProductAt(i);
+			long processId = processProduct.getFirst();
+			if (handled.contains(processId))
+				continue;
+			handled.add(processId);
+			ProcessDescriptor process = cache.getProcessDescriptor(processId);
+			if (process == null)
+				continue;
 			AnalysisFlowResult r = getResult(result, process, flow);
 			results.add(r);
+
 		}
 		return results;
 	}
 
 	public static List<AnalysisFlowResult> getForProcess(AnalysisResult result,
-			ProcessDescriptor process, List<FlowDescriptor> flows) {
+			ProcessDescriptor process, Cache cache) {
 		List<AnalysisFlowResult> results = new ArrayList<>();
-		for (FlowDescriptor flow : flows) {
+		for (FlowDescriptor flow : getFlows(result, cache)) {
 			AnalysisFlowResult r = getResult(result, process, flow);
 			results.add(r);
 		}
