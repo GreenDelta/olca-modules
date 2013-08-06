ce6d48854248126cff70f405f8425d5219e23668
msrocka <michael.srocka@gmail.com>
2013-08-06 14:13:37 +0200

    analyse function in inventory solver

--- olca-core/src/main/java/org/openlca/core/matrices/InventorySolver.java
+++ olca-core/src/main/java/org/openlca/core/matrices/InventorySolver.java
@@ -4,6 +4,7 @@ import org.openlca.core.indices.LongPair;
 import org.openlca.core.indices.ProductIndex;
 import org.openlca.core.math.IMatrix;
 import org.openlca.core.math.MatrixFactory;
+import org.openlca.core.results.AnalysisResult;
 import org.openlca.core.results.InventoryResult;
 
 public class InventorySolver {
@@ -31,6 +32,60 @@ public class InventorySolver {
 			IMatrix i = impactFactors.multiply(g);
 			result.setImpactIndex(impactMatrix.getCategoryIndex());
 			result.setImpactResults(i.getColumn(0));
+		}
+		return result;
+	}
+
+	public AnalysisResult analyze(Inventory inventory, ImpactMatrix impactMatrix) {
+
+		ProductIndex productIndex = inventory.getProductIndex();
+		ExchangeMatrix techExchanges = inventory.getTechnologyMatrix();
+		int n = productIndex.size();
+
+		AnalysisResult result = new AnalysisResult(inventory.getFlowIndex(),
+				inventory.getProductIndex());
+
+		IMatrix techMatrix = techExchanges.createRealMatrix();
+		IMatrix enviMatrix = inventory.getInterventionMatrix()
+				.createRealMatrix();
+		IMatrix inverse = techMatrix.getInverse();
+
+		IMatrix demand = demandVector(inventory);
+		IMatrix scalingFactors = inverse.multiply(demand);
+		// we now that the reference product is always in the first column
+		result.setScalingFactors(scalingFactors.getColumn(0));
+
+		// single results
+		IMatrix scalingMatrix = MatrixFactory.create(n, n);
+		for (int i = 0; i < n; i++) {
+			scalingMatrix.setEntry(i, i, scalingFactors.getEntry(i, 0));
+		}
+		IMatrix singleResult = enviMatrix.multiply(scalingMatrix);
+		result.setSingleResult(singleResult);
+
+		// total results
+		// TODO: loop correction
+		IMatrix demandMatrix = MatrixFactory.create(n, n);
+		for (int i = 0; i < productIndex.size(); i++) {
+			ExchangeCell productCell = techExchanges.getEntry(i, i);
+			if (productCell == null)
+				continue;
+			double amount = scalingFactors.getEntry(i, 0)
+					* productCell.getMatrixValue();
+			demandMatrix.setEntry(i, i, amount);
+		}
+		IMatrix totalResult = enviMatrix.multiply(inverse).multiply(
+				demandMatrix);
+		result.setTotalResult(totalResult);
+
+		if (impactMatrix != null) {
+			result.setImpactCategoryIndex(impactMatrix.getCategoryIndex());
+			IMatrix factors = impactMatrix.getValues();
+			result.setImpactFactors(factors);
+			IMatrix singleImpactResult = factors.multiply(singleResult);
+			result.setSingleImpactResult(singleImpactResult);
+			IMatrix totalImpactResult = factors.multiply(totalResult);
+			result.setTotalImpactResult(totalImpactResult);
 		}
 		return result;
 	}
