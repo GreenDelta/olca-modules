ef339efdcb687924df9190f7ea163df1eedc6d23
msrocka <michael.srocka@gmail.com>
2013-08-19 18:51:20 +0200

    scoping test added

---
+++ olca-formula/src/test/java/org/openlca/expressions/ScopingTest.java
@@ -0,0 +1,38 @@
+package org.openlca.expressions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScopingTest {
+
+	@Test
+	public void testSimpleScopes() throws Exception {
+		// example from
+		// http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-21.html#%_sec_3.2
+		FormulaInterpreter interpreter = new FormulaInterpreter();
+
+		// global scope
+		interpreter.bind("x", "3");
+		interpreter.bind("y", "5");
+
+		// I sub-scope of global scope
+		Scope a = interpreter.createScope(2);
+		a.bind("z", "6");
+		a.bind("x", "7");
+
+		// II sub-scope of global scope
+		Scope b = interpreter.createScope(3);
+		b.bind("n", "1");
+		b.bind("y", "2");
+
+		Assert.assertEquals(3.0, interpreter.eval("x"), 1e-16);
+		Assert.assertEquals(7.0, a.eval("x"), 1e-16);
+		Assert.assertEquals(3.0, b.eval("x"), 1e-16);
+
+		Assert.assertEquals(5.0, interpreter.eval("y"), 1e-16);
+		Assert.assertEquals(5.0, a.eval("y"), 1e-16);
+		Assert.assertEquals(2.0, b.eval("y"), 1e-16);
+
+	}
+
+}
