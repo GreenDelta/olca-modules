3883df05f09d59694b5837d355649cd03288ab4e
Sebastian Greve <greve@greendelta.com>
2013-08-28 16:11:28 +0200

    Implemented getTechnologyInputs & getTechnologyOutputs in ProcessDao

--- olca-core/src/main/java/org/openlca/core/database/ProcessDao.java
+++ olca-core/src/main/java/org/openlca/core/database/ProcessDao.java
@@ -1,7 +1,16 @@
 package org.openlca.core.database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.openlca.core.model.Process;
 import org.openlca.core.model.ProcessType;
+import org.openlca.core.model.descriptors.FlowDescriptor;
 import org.openlca.core.model.descriptors.ProcessDescriptor;
 
 public class ProcessDao extends
@@ -29,4 +38,50 @@ public class ProcessDao extends
 		d.setLocation((Long) queryResult[7]);
 		return d;
 	}
+
+	public List<FlowDescriptor> getTechnologyInputs(ProcessDescriptor descriptor) {
+		List<Long> flowIds = getTechnologies(descriptor, true);
+		return loadFlowDescriptors(flowIds);
+	}
+
+	public List<FlowDescriptor> getTechnologyOutputs(
+			ProcessDescriptor descriptor) {
+		List<Long> flowIds = getTechnologies(descriptor, false);
+		return loadFlowDescriptors(flowIds);
+	}
+
+	private List<Long> getTechnologies(ProcessDescriptor descriptor,
+			boolean input) {
+		if (descriptor == null)
+			return Collections.emptyList();
+		String sql = "select f_flow from tbl_exchanges where f_owner = "
+				+ descriptor.getId() + " and is_input = " + (input ? 1 : 0);
+		List<Long> ids = new ArrayList<>();
+		try (Connection con = getDatabase().createConnection();
+				Statement s = con.createStatement();
+				ResultSet rs = s.executeQuery(sql)) {
+			while (rs.next())
+				ids.add(rs.getLong("f_flow"));
+			return ids;
+		} catch (SQLException e) {
+			log.error("Error loading technologies", e);
+			return Collections.emptyList();
+		}
+	}
+
+	private List<FlowDescriptor> loadFlowDescriptors(List<Long> flowIds) {
+		if (flowIds == null || flowIds.isEmpty())
+			return Collections.emptyList();
+		// TODO: performance may could be improved if we query the
+		// database with an 'IN - query'
+		List<FlowDescriptor> results = new ArrayList<>();
+		FlowDao dao = new FlowDao(getDatabase());
+		for (Long flowId : flowIds) {
+			FlowDescriptor d = dao.getDescriptor(flowId);
+			if (d != null)
+				results.add(d);
+		}
+		return results;
+	}
+
 }
