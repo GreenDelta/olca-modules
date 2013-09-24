5027c7881eaf799276ed3e8f73f98dcaaaa9c863
msrocka <michael.srocka@gmail.com>
2013-09-24 12:06:14 +0200

    removed 'Indexable' interface and old license headers from model package

--- olca-core/src/main/java/org/openlca/core/model/ScalingFactor.java
+++ olca-core/src/main/java/org/openlca/core/model/ScalingFactor.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
- * the accompanying materials are made available under the terms of the Mozilla
- * Public License v1.1 which accompanies this distribution, and is available at
- * http://www.openlca.org/uploads/media/MPL-1.1.html
- * 
- * Contributors: GreenDeltaTC - initial API and implementation
- * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
- ******************************************************************************/
 package org.openlca.core.model;
 
 import javax.persistence.Column;
@@ -14,10 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 /**
- * <p style="margin-top: 0">
  * A scaling factor is a factor for scaling a linked process in a product
  * system.
- * </p>
  */
 @Entity
 @Table(name = "tbl_scalingfactors")
