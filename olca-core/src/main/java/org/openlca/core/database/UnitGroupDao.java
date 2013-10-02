11aa84e4cbb61ea5b5a21ebe199ee621e6aba3d1
imo <graf@greendelta.com>
2013-10-02 15:01:36 +0200

    Added method to get a unitgroup for unit.

--- olca-core/src/main/java/org/openlca/core/database/UnitGroupDao.java
+++ olca-core/src/main/java/org/openlca/core/database/UnitGroupDao.java
@@ -1,5 +1,7 @@
 package org.openlca.core.database;
 
+import java.util.Collections;
+
 import org.openlca.core.model.UnitGroup;
 import org.openlca.core.model.descriptors.UnitGroupDescriptor;
 
@@ -8,6 +10,19 @@ public class UnitGroupDao extends
 
 	public UnitGroupDao(IDatabase database) {
 		super(UnitGroup.class, UnitGroupDescriptor.class, database);
+	}
+
+	public UnitGroup getForUnit(String name) {
+		if (name == null)
+			return null;
+		String jpql = "select ug from UnitGroup ug join ug.units u where u.name = :name";
+		try {
+			return Query.on(getDatabase()).getFirst(entityType, jpql,
+					Collections.singletonMap("name", name));
+		} catch (Exception e) {
+			log.error("failed to get instance for name " + name, e);
+			return null;
+		}
 	}
 
 }
