30ddd4154632a389442761b7b03512c818d3c75f
Imo Graf <graf@greendelta.com>
2014-02-18 15:51:04 +0100

    Finished the SimaPro CSV export.

--- olca-io/src/main/java/org/openlca/io/csv/output/CSVExporter.java
+++ olca-io/src/main/java/org/openlca/io/csv/output/CSVExporter.java
@@ -1,20 +1,57 @@
 package org.openlca.io.csv.output;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.ProcessDao;
 import org.openlca.core.model.Process;
+import org.openlca.core.model.descriptors.ProcessDescriptor;
 import org.openlca.simapro.csv.CSVSeperator;
+import org.openlca.simapro.csv.model.SPProcess;
+import org.openlca.simapro.csv.model.SPReferenceData;
 import org.openlca.simapro.csv.writer.CSVWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CSVExporter {
+public class CSVExporter implements Runnable {
 
-	CSVWriter writer;
+	private CSVWriter writer;
+	private ProcessConverter converter;
+	private SPReferenceData referenceData;
+	private ProcessDao processDao;
+	private List<ProcessDescriptor> processes;
+	private Logger log = LoggerFactory.getLogger(getClass());
 
-	public CSVExporter(File file, CSVSeperator csvSeperator) {
-
+	public CSVExporter(IDatabase database, File file, char seperator,
+			List<ProcessDescriptor> processes) throws IOException {
+		if (CSVSeperator.forChar(seperator) == null)
+			throw new IllegalArgumentException("Wrong seperator type: "
+					+ seperator);
+		converter = new ProcessConverter(database,
+				(referenceData = new SPReferenceData()));
+		writer = new CSVWriter(new FileWriter(file),
+				CSVSeperator.forChar(seperator), '.');
+		writer.writeHeader("openLCA Export");
+		processDao = new ProcessDao(database);
+		this.processes = processes;
 	}
 
-	public void export(Process process) {
+	@Override
+	public void run() {
+		try {
+			for (ProcessDescriptor descriptor : processes) {
+				Process process = processDao.getForId(descriptor.getId());
+				SPProcess spProcess = converter.convert(process);
+				writer.write(spProcess);
+			}
+			writer.write(referenceData);
+			writer.close();
+		} catch (IOException e) {
+			log.error("SimaPro CSV export failed", e);
+		}
 	}
 
 }
--- olca-io/src/main/java/org/openlca/io/csv/output/ProcessConverter.java
+++ olca-io/src/main/java/org/openlca/io/csv/output/ProcessConverter.java
@@ -1,40 +1,352 @@
 package org.openlca.io.csv.output;
 
+import java.util.Map;
+
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.model.Actor;
+import org.openlca.core.model.Category;
 import org.openlca.core.model.Exchange;
+import org.openlca.core.model.Flow;
+import org.openlca.core.model.FlowType;
+import org.openlca.core.model.PedigreeMatrix;
+import org.openlca.core.model.PedigreeMatrixRow;
 import org.openlca.core.model.Process;
+import org.openlca.core.model.Source;
+import org.openlca.core.model.Uncertainty;
+import org.openlca.core.model.Unit;
+import org.openlca.core.model.UnitGroup;
+import org.openlca.io.UnitMapping;
+import org.openlca.io.maps.MapType;
+import org.openlca.io.maps.MappingBuilder;
+import org.openlca.io.maps.content.CSVElementaryCategoryContent;
+import org.openlca.io.maps.content.CSVElementaryFlowContent;
+import org.openlca.io.maps.content.CSVGeographyContent;
+import org.openlca.io.maps.content.CSVQuantityContent;
+import org.openlca.io.maps.content.CSVUnitContent;
+import org.openlca.simapro.csv.model.IDistribution;
+import org.openlca.simapro.csv.model.SPDocumentation;
 import org.openlca.simapro.csv.model.SPElementaryFlow;
+import org.openlca.simapro.csv.model.SPLiteratureReference;
+import org.openlca.simapro.csv.model.SPLiteratureReferenceEntry;
+import org.openlca.simapro.csv.model.SPLogNormalDistribution;
+import org.openlca.simapro.csv.model.SPNormalDistribution;
+import org.openlca.simapro.csv.model.SPPedigreeMatrix;
 import org.openlca.simapro.csv.model.SPProcess;
 import org.openlca.simapro.csv.model.SPProduct;
+import org.openlca.simapro.csv.model.SPProductFlow;
+import org.openlca.simapro.csv.model.SPQuantity;
+import org.openlca.simapro.csv.model.SPReferenceData;
+import org.openlca.simapro.csv.model.SPSubstance;
+import org.openlca.simapro.csv.model.SPTriangleDistribution;
+import org.openlca.simapro.csv.model.SPUniformDistribution;
+import org.openlca.simapro.csv.model.SPUnit;
 import org.openlca.simapro.csv.model.types.ElementaryFlowType;
+import org.openlca.simapro.csv.model.types.Geography;
+import org.openlca.simapro.csv.model.types.ProcessCategory;
+import org.openlca.simapro.csv.model.types.ProcessType;
+import org.openlca.simapro.csv.model.types.ProductFlowType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class ProcessConverter {
 
-	SPProcess spProcess;
-	Process process;
+	private Logger log = LoggerFactory.getLogger(getClass());
+	private SPProcess spProcess;
+	private Process process;
+	private SPReferenceData referenceData;
+	private Map<String, CSVGeographyContent> geoMap;
+	private Map<String, CSVElementaryFlowContent> elemMap;
+	private Map<String, CSVElementaryCategoryContent> categoryMap;
+	private Map<String, CSVUnitContent> unitMap;
+	private Map<String, CSVQuantityContent> quantityMap;
+	private UnitMapping unitMapping;
+
+	ProcessConverter(IDatabase database, SPReferenceData referenceData) {
+		this.referenceData = referenceData;
+		unitMapping = UnitMapping.createDefault(database);
+		MappingBuilder mappingBuilder = new MappingBuilder(database);
+		geoMap = mappingBuilder.buildExportMapping(CSVGeographyContent.class,
+				MapType.CSV_GEOGRAPHY);
+		elemMap = mappingBuilder.buildExportMapping(
+				CSVElementaryFlowContent.class, MapType.CSV_ELEMENTARY_FLOW);
+		categoryMap = mappingBuilder.buildExportMapping(
+				CSVElementaryCategoryContent.class,
+				MapType.CSV_ELEMENTARY_CATEGORY);
+		unitMap = mappingBuilder.buildExportMapping(CSVUnitContent.class,
+				MapType.CSV_UNIT);
+		quantityMap = mappingBuilder.buildExportMapping(
+				CSVQuantityContent.class, MapType.CSV_QUANTITY);
+	}
 
 	SPProcess convert(Process process) {
 		this.process = process;
 		spProcess = new SPProcess(referenceProduct());
+		generalInformations();
+		elementaryExchanges();
+		productExchanges();
+		literatureReferences();
 		return spProcess;
+	}
+
+	private void generalInformations() {
+		SPDocumentation documentation = new SPDocumentation(process.getName(),
+				ProcessCategory.MATERIAL, getType());
+		spProcess.setDocumentation(documentation);
+		documentation.setComment(process.getDescription());
+		documentation.setCreationDate(process.getDocumentation()
+				.getCreationDate().toString());
+		documentation.setInfrastructureProcess(process
+				.isInfrastructureProcess());
+		geography(documentation);
+
+		System.out.println(documentation.getGenerator());
+		if (documentation.getGenerator() != null)
+			documentation.setGenerator(documentation.getGenerator());
+	}
+
+	private String getActorAsString(Actor actor) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Name: " + actor.getName());
+		if (actor.getAddress() != null)
+			builder.append(" Address:" + actor.getAddress());
+		if (actor.getCity() != null)
+			builder.append(" City:" + actor.getCity());
+		if (actor.getCountry() != null)
+			builder.append(" e-mail: " + actor.getEmail());
+		if (actor.getTelefax() != null)
+			builder.append(" Telefax: " + actor.getTelefax());
+		if (actor.getTelephone() != null)
+			builder.append(" Telephone: " + actor.getTelephone());
+		if (actor.getWebsite() != null)
+			builder.append(" Website: " + actor.getWebsite());
+		if (actor.getZipCode() != null)
+			builder.append(" Zip code: " + actor.getZipCode());
+		return builder.toString();
+	}
+
+	private ProcessType getType() {
+		if (process.getProcessType() == org.openlca.core.model.ProcessType.UNIT_PROCESS)
+			return ProcessType.UNIT_PROCESS;
+		return ProcessType.SYSTEM;
+	}
+
+	private void geography(SPDocumentation documentation) {
+		CSVGeographyContent content = geoMap.get(process.getLocation()
+				.getRefId());
+		if (content != null)
+			documentation.setGeography(content.getGeography());
+		else
+			documentation.setGeography(Geography.UNKNOWN);
+	}
+
+	private void literatureReferences() {
+		for (Source source : process.getDocumentation().getSources()) {
+			String category = null;
+			if (source.getCategory() != null)
+				category = source.getCategory().getName();
+			else
+				category = "Others";
+			SPLiteratureReference reference = new SPLiteratureReference(
+					source.getName(), source.getTextReference(), category);
+			if (!"".equals(source.getDescription()))
+				reference.setContent(source.getDescription());
+			referenceData.add(source.getName(), reference);
+			spProcess.getDocumentation().add(
+					new SPLiteratureReferenceEntry(reference));
+		}
 	}
 
 	private SPProduct referenceProduct() {
 		Exchange exchange = process.getQuantitativeReference();
-		SPProduct product = new SPProduct(exchange.getFlow()
-				.getName(), exchange.getUnit().getName(),
-				String.valueOf(exchange.getAmountValue()));
-		product.setCategory(process.getCategory().getName());
+		SPProduct product = new SPProduct(exchange.getFlow().getName(), map(
+				exchange.getUnit()).getName(), String.valueOf(exchange
+				.getAmountValue()));
+		// TODO create a category tree
+		// product.setCategory(process.getCategory().getName());
+		product.setCategory("Others");
+
+		// TODO: check
 		return product;
 	}
 
-	private void exchanges() {
+	private void productExchanges() {
 		for (Exchange exchange : process.getExchanges()) {
-			ElementaryFlowType type = null;
-
-			SPElementaryFlow flow = new SPElementaryFlow(type, exchange
-					.getFlow().getName(), exchange.getUnit().getName(),
+			if (exchange.getFlow().getFlowType() != FlowType.PRODUCT_FLOW
+					|| exchange.getFlow().getFlowType() != FlowType.WASTE_FLOW)
+				continue;
+			Flow olcaFlow = exchange.getFlow();
+			SPProductFlow productFlow = new SPProductFlow(null,
+					olcaFlow.getName(), map(exchange.getUnit()).getName(),
 					String.valueOf(exchange.getAmountValue()));
+			productFlow.setType(getProductFlowType(exchange));
+			spProcess.add(productFlow);
+		}
+	}
+
+	private ProductFlowType getProductFlowType(Exchange exchange) {
+		if (exchange.isInput())
+			// TODO: maybe find out which flows are electricity inputs.
+			return ProductFlowType.MATERIAL_INPUT;
+		else
+			return ProductFlowType.AVOIDED_PRODUCT;
+	}
+
+	private void elementaryExchanges() {
+		for (Exchange exchange : process.getExchanges()) {
+			if (exchange.getFlow().getFlowType() != FlowType.ELEMENTARY_FLOW)
+				continue;
+			String casNumber = null;
+			SPElementaryFlow flow = null;
+			CSVElementaryFlowContent content = elemMap.get(exchange.getFlow()
+					.getRefId());
+			if (content != null) {
+				flow = content.createFlow();
+				referenceData.add(content.getUnit());
+				referenceData.add(new SPQuantity(content.getUnit()
+						.getQuantity(), new SPUnit(content.getUnit()
+						.getReferenceUnit())));
+				casNumber = content.getCasNumber();
+			} else {
+				flow = createElementaryFlow(exchange);
+				casNumber = exchange.getFlow().getCasNumber();
+			}
+			if (exchange.isInput()) {
+				flow.setType(ElementaryFlowType.RESOURCE);
+			} else {
+				if (flow.getType() != null) {
+					// TODO: throw right exception
+				}
+				// TODO: on this point the conversion will failed.
+			}
+			flow.setAmount(String.valueOf(exchange.getAmountValue()));
+			flow.setDistribution(convertDistribition(exchange.getUncertainty(),
+					exchange.getPedigreeUncertainty()));
+			SPSubstance substance = new SPSubstance(flow.getName(),
+					flow.getUnit());
+			substance.setFlowType(flow.getType());
+			substance.setCASNumber(casNumber);
+			referenceData.add(substance);
 			spProcess.add(flow);
 		}
+	}
+
+	private SPElementaryFlow createElementaryFlow(Exchange exchange) {
+		Flow olcaFlow = exchange.getFlow();
+		SPElementaryFlow flow = new SPElementaryFlow(null, olcaFlow.getName(),
+				map(exchange.getUnit()).getName(), String.valueOf(exchange
+						.getAmountValue()));
+		CSVElementaryCategoryContent categoryContent = categoryMap.get(olcaFlow
+				.getCategory().getRefId());
+		if (categoryContent != null) {
+			flow.setType(categoryContent.getType());
+			flow.setSubCompartment(categoryContent.getSubCompartment());
+		} else {
+			StringBuilder builder = new StringBuilder();
+			builder.append("Can not find category mapping for flow '");
+			builder.append(olcaFlow.getName());
+			builder.append("' in category '");
+			Category current = olcaFlow.getCategory();
+			String categoryTree = null;
+			while (current != null) {
+				if (categoryTree != null)
+					categoryTree = "/" + categoryTree;
+				categoryTree = current.getName() + categoryTree;
+				current = current.getParentCategory();
+			}
+			builder.append(categoryTree);
+			log.error(builder.toString());
+		}
+		return flow;
+	}
+
+	private SPUnit map(Unit unit) {
+		CSVUnitContent content = unitMap.get(unit.getRefId());
+		SPUnit spUnit = null;
+		if (content == null) {
+			spUnit = new SPUnit(unit.getName());
+			spUnit.setConversionFactor(unit.getConversionFactor());
+			UnitGroup unitGroup = unitMapping.getUnitGroup(unit.getName());
+			spUnit.setQuantity(map(unitGroup).getName());
+			spUnit.setReferenceUnit(unitGroup.getReferenceUnit().getName());
+		} else {
+			spUnit = content.createUnit();
+		}
+		referenceData.add(spUnit);
+		referenceData.add(new SPQuantity(spUnit.getQuantity(), new SPUnit(
+				spUnit.getReferenceUnit())));
+		return spUnit;
+	}
+
+	private SPQuantity map(UnitGroup unitGroup) {
+		CSVQuantityContent content = quantityMap.get(unitGroup.getRefId());
+		if (content == null)
+			return new SPQuantity(unitGroup.getName(), new SPUnit(unitGroup
+					.getReferenceUnit().getName(), unitGroup.getReferenceUnit()
+					.getConversionFactor()));
+		return content.getQuantity();
+	}
+
+	private IDistribution convertDistribition(Uncertainty uncertainty,
+			String pedigreeMatrix) {
+		if (uncertainty == null)
+			return null;
+		IDistribution distribution = null;
+		switch (uncertainty.getDistributionType()) {
+		case LOG_NORMAL:
+			distribution = new SPLogNormalDistribution(
+					uncertainty.getParameter2Value(),
+					convertPedigreeMatrix(pedigreeMatrix));
+			break;
+		case NORMAL:
+			distribution = new SPNormalDistribution(
+					uncertainty.getParameter2Value());
+			break;
+		case TRIANGLE:
+			distribution = new SPTriangleDistribution(
+					uncertainty.getParameter1Value(),
+					uncertainty.getParameter3Value());
+			break;
+		case UNIFORM:
+			distribution = new SPUniformDistribution(
+					uncertainty.getParameter1Value(),
+					uncertainty.getParameter2Value());
+			break;
+		case NONE:
+			break;
+		}
+		return distribution;
+	}
+
+	private SPPedigreeMatrix convertPedigreeMatrix(String pedigreeMatrix) {
+		if (pedigreeMatrix == null)
+			return null;
+		SPPedigreeMatrix spPedigreeMatrix = new SPPedigreeMatrix();
+		Map<PedigreeMatrixRow, Integer> bla = PedigreeMatrix
+				.fromString(pedigreeMatrix);
+		for (Map.Entry<PedigreeMatrixRow, Integer> entry : bla.entrySet()) {
+			switch (entry.getKey()) {
+			case COMPLETENESS:
+				spPedigreeMatrix.setCompleteness(String.valueOf(entry
+						.getValue()));
+				break;
+			case GEOGRAPHY:
+				spPedigreeMatrix.setGeographicalCorrelation(String
+						.valueOf(entry.getValue()));
+				break;
+			case RELIABILITY:
+				spPedigreeMatrix
+						.setReliability(String.valueOf(entry.getValue()));
+				break;
+			case TECHNOLOGY:
+				spPedigreeMatrix.setFurtherTechnologicalCorrelation(String
+						.valueOf(entry.getValue()));
+				break;
+			case TIME:
+				spPedigreeMatrix.setTemporalCorrelation(String.valueOf(entry
+						.getValue()));
+				break;
+			}
+		}
+		return spPedigreeMatrix;
 	}
 }
