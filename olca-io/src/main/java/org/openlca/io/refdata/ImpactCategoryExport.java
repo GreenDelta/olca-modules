41f6051b586f1efbe97d29cf71a021aef9238923
msrocka <michael.srocka@gmail.com>
2014-02-28 09:40:39 +0100

    ref-data-io: bug-fix: LCIA categories not written

--- olca-io/src/main/java/org/openlca/io/refdata/ImpactCategoryExport.java
+++ olca-io/src/main/java/org/openlca/io/refdata/ImpactCategoryExport.java
@@ -1,31 +1,43 @@
 package org.openlca.io.refdata;
 
-import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.NativeSql;
-import org.supercsv.io.CsvListWriter;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.NativeSql;
+import org.supercsv.io.CsvListWriter;
+
 public class ImpactCategoryExport extends Export {
 
 	@Override
-	protected void doIt(CsvListWriter writer, IDatabase database) throws Exception {
+	protected void doIt(final CsvListWriter writer, IDatabase database)
+			throws Exception {
 		log.trace("write impact categories");
-		String query = "select c.ref_id, c.name, c.description, c.reference_unit, " +
-				"m.ref_id from tbl_impact_categories c join tbl_impact_methods m " +
-				"on c.f_impact_method = m.id";
+		String query = "select c.ref_id, c.name, c.description, c.reference_unit, "
+				+ "m.ref_id from tbl_impact_categories c join tbl_impact_methods m "
+				+ "on c.f_impact_method = m.id";
 		final AtomicInteger count = new AtomicInteger(0);
 		NativeSql.on(database).query(query, new NativeSql.QueryResultHandler() {
 			@Override
 			public boolean nextResult(ResultSet resultSet) throws SQLException {
-				Object[] line = createLine(resultSet);
-				count.incrementAndGet();
-				return true;
+				return writeLine(resultSet, writer, count);
 			}
 		});
 		log.trace("{} impact categories written", count.get());
+	}
+
+	private boolean writeLine(ResultSet resultSet, final CsvListWriter writer,
+			final AtomicInteger count) {
+		try {
+			Object[] line = createLine(resultSet);
+			writer.write(line);
+			count.incrementAndGet();
+			return true;
+		} catch (Exception e) {
+			log.error("failed to write line", e);
+			return false;
+		}
 	}
 
 	private Object[] createLine(ResultSet resultSet) throws SQLException {
