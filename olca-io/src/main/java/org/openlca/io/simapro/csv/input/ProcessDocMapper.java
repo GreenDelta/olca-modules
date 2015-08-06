90015a12fe2af1cf169efb29c6af8da1a862825a
msrocka <michael.srocka@gmail.com>
2015-08-06 13:45:48 +0200

    SimaPro CSV import: improved documentation mapping

--- olca-io/src/main/java/org/openlca/io/simapro/csv/input/ProcessDocMapper.java
+++ olca-io/src/main/java/org/openlca/io/simapro/csv/input/ProcessDocMapper.java
@@ -11,6 +11,7 @@ import org.openlca.core.model.Source;
 import org.openlca.simapro.csv.model.enums.ValueEnum;
 import org.openlca.simapro.csv.model.process.LiteratureReferenceRow;
 import org.openlca.simapro.csv.model.process.ProcessBlock;
+import org.openlca.simapro.csv.model.process.SystemDescriptionRow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,8 +30,10 @@ class ProcessDocMapper {
 		this.block = block;
 		this.process = process;
 		mapSources();
-		mapDefaults();
+		mapDocFields();
 		mapDescription();
+		if (block.getInfrastructure() != null)
+			process.setInfrastructureProcess(block.getInfrastructure());
 	}
 
 	private void mapSources() {
@@ -42,18 +45,48 @@ class ProcessDocMapper {
 		}
 	}
 
-	private void mapDefaults() {
+	private void mapDocFields() {
 		ProcessDocumentation doc = new ProcessDocumentation();
 		process.setDocumentation(doc);
 		mapTime(doc);
+		if (block.getGeography() != null)
+			doc.setGeography(block.getGeography().getValue());
 		if (block.getTechnology() != null)
 			doc.setTechnology(block.getTechnology().getValue());
-		if (block.getInfrastructure() != null)
-			process.setInfrastructureProcess(block.getInfrastructure());
+		if (block.getRepresentativeness() != null)
+			doc.setDataSelection(block.getRepresentativeness().getValue());
 		doc.setDataTreatment(block.getDataTreatment());
 		doc.setSampling(block.getCollectionMethod());
 		doc.setReviewDetails(block.getVerification());
-		doc.setInventoryMethod(block.getAllocationRules());
+		mapInventoryMethod(doc);
+		mapCompleteness(doc);
+		mapProject(doc);
+		doc.setCreationDate(block.getDate());
+	}
+
+	private void mapInventoryMethod(ProcessDocumentation doc) {
+		String t = null;
+		t = a("Allocation rules", block.getAllocationRules(), t);
+		t = a("Multiple output allocation", block.getAllocation(), t);
+		t = a("Substitution allocation", block.getSubstitution(), t);
+		doc.setInventoryMethod(t);
+	}
+
+	private void mapCompleteness(ProcessDocumentation doc) {
+		String t = null;
+		t = a("Cut off rules", block.getCutoff(), t);
+		t = a("Capital goods", block.getCapitalGoods(), t);
+		doc.setCompleteness(t);
+	}
+
+	private void mapProject(ProcessDocumentation doc) {
+		SystemDescriptionRow r = block.getSystemDescription();
+		if (r == null || r.getName() == null)
+			return;
+		String t = r.getName();
+		if (r.getComment() != null)
+			t += " (" + r.getComment() + ")";
+		doc.setProject(t);
 	}
 
 	private void mapTime(ProcessDocumentation doc) {
@@ -84,25 +117,38 @@ class ProcessDocMapper {
 		StringBuilder builder = new StringBuilder();
 		if (block.getComment() != null)
 			builder.append(block.getComment());
-		append("Status", block.getStatus(), builder);
-		append("Representativeness", block.getRepresentativeness(), builder);
-		append("Cut off rules", block.getCutoff(), builder);
-		append("Multiple output allocation", block.getAllocation(), builder);
-		append("Substitution allocation", block.getSubstitution(), builder);
-		append("Capital goods", block.getCapitalGoods(), builder);
-		append("Boundary with nature", block.getBoundaryWithNature(), builder);
-		append("Record", block.getRecord(), builder);
-		append("Generator", block.getGenerator(), builder);
+		a("Status", block.getStatus(), builder);
+		a("Boundary with nature", block.getBoundaryWithNature(), builder);
+		a("Record", block.getRecord(), builder);
+		a("Generator", block.getGenerator(), builder);
 		process.setDescription(builder.toString());
 	}
 
-	private void append(String label, String value, StringBuilder builder) {
+	private void a(String label, String value, StringBuilder builder) {
 		if (value == null)
 			return;
 		builder.append(label).append(": ").append(value).append("\n");
 	}
 
-	private void append(String label, ValueEnum venum, StringBuilder builder) {
+	private String a(String label, String value, String field) {
+		if (value == null)
+			return field;
+		if (field == null)
+			return label + ": " + value + "\n";
+		else
+			return field + label + ": " + value + "\n";
+	}
+
+	private String a(String label, ValueEnum venum, String field) {
+		if (venum == null)
+			return field;
+		if (field == null)
+			return label + ": " + venum.getValue() + "\n";
+		else
+			return field + label + ": " + venum.getValue() + "\n";
+	}
+
+	private void a(String label, ValueEnum venum, StringBuilder builder) {
 		if (venum == null)
 			return;
 		builder.append(label).append(": ").append(venum.getValue())
