c0df6b7f8e41317c2e2fbc4ad0190e901e75b359
greve <greve@greendelta.com>
2015-10-26 15:04:31 +0100

    Implemented handling of escaped strings in CSV import

--- olca-simapro-csv/src/main/java/org/openlca/simapro/csv/CsvConfig.java
+++ olca-simapro-csv/src/main/java/org/openlca/simapro/csv/CsvConfig.java
@@ -2,22 +2,32 @@ package org.openlca.simapro.csv;
 
 public class CsvConfig {
 
-	private String separator;
+	private char separator;
+	private char delimiter;
 	private String dateFormat;
 
 	public static CsvConfig getDefault() {
 		CsvConfig config = new CsvConfig();
-		config.setSeparator(";");
+		config.setSeparator(';');
+		config.setDelimiter('"');
 		config.setDateFormat("dd.MM.yyyy");
 		return config;
 	}
 
-	public String getSeparator() {
+	public char getSeparator() {
 		return separator;
 	}
 
-	public void setSeparator(String separator) {
+	public void setSeparator(char separator) {
 		this.separator = separator;
+	}
+	
+	public char getDelimiter() {
+		return delimiter;
+	}
+	
+	public void setDelimiter(char delimiter) {
+		this.delimiter = delimiter;
 	}
 
 	public String getDateFormat() {
