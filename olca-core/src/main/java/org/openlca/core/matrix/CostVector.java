6c3d1e2f204799f41511d9dc11b7441ea806be41
msrocka <michael.srocka@gmail.com>
2016-01-26 17:05:57 +0100

    allocation was already applied

--- olca-core/src/main/java/org/openlca/core/matrix/CostVector.java
+++ olca-core/src/main/java/org/openlca/core/matrix/CostVector.java
@@ -66,7 +66,7 @@ public class CostVector {
 					return;
 				}
 				val = currencyTable.getFactor(cell.exchange.currency) * val;
-				values[col] += (val * cell.allocationFactor);
+				values[col] += val;
 			});
 		}
 	}
