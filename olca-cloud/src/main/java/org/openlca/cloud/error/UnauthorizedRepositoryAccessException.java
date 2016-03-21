aa09ce9541537c0ebd6dde2ebd3dd6b564a1c88e
greve <greve@greendelta.com>
2016-03-21 12:46:28 +0100

    Updated exception inputs

--- olca-cloud/src/main/java/org/openlca/cloud/error/UnauthorizedRepositoryAccessException.java
+++ olca-cloud/src/main/java/org/openlca/cloud/error/UnauthorizedRepositoryAccessException.java
@@ -6,7 +6,7 @@ public class UnauthorizedRepositoryAccessException extends ServerException {
 
 	private static final long serialVersionUID = -5922024730075076184L;
 
-	public UnauthorizedRepositoryAccessException(String repositoryId) {
-		super(Status.FORBIDDEN, "No access to '" + repositoryId + "'");
+	public UnauthorizedRepositoryAccessException(String repositoryId, String action) {
+		super(Status.FORBIDDEN, "No permission to perform '" + action + "'" + " on '" + repositoryId + "'");
 	}
 }
