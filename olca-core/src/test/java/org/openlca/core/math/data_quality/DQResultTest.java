b80fce115110f833cd93a53f5e87553ef8bc95bd
Sebastian Greve <greve@greendelta.com>
2016-07-12 13:13:43 +0200

    Extended data quality result test

--- olca-core/src/test/java/org/openlca/core/math/data_quality/DQResultTest.java
+++ olca-core/src/test/java/org/openlca/core/math/data_quality/DQResultTest.java
@@ -8,6 +8,7 @@ import org.openlca.core.Tests;
 import org.openlca.core.database.DQSystemDao;
 import org.openlca.core.database.FlowDao;
 import org.openlca.core.database.FlowPropertyDao;
+import org.openlca.core.database.ImpactMethodDao;
 import org.openlca.core.database.ProcessDao;
 import org.openlca.core.database.ProductSystemDao;
 import org.openlca.core.database.UnitGroupDao;
@@ -22,11 +23,15 @@ import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowProperty;
 import org.openlca.core.model.FlowPropertyFactor;
 import org.openlca.core.model.FlowType;
+import org.openlca.core.model.ImpactCategory;
+import org.openlca.core.model.ImpactFactor;
+import org.openlca.core.model.ImpactMethod;
 import org.openlca.core.model.Process;
 import org.openlca.core.model.ProcessLink;
 import org.openlca.core.model.ProductSystem;
 import org.openlca.core.model.Unit;
 import org.openlca.core.model.UnitGroup;
+import org.openlca.core.model.descriptors.Descriptors;
 import org.openlca.core.results.ContributionResult;
 
 public class DQResultTest {
@@ -36,9 +41,11 @@ public class DQResultTest {
 	private Process process2;
 	private Flow pFlow1;
 	private Flow pFlow2;
-	private Flow eFlow;
+	private Flow eFlow1;
+	private Flow eFlow2;
 	private FlowProperty property;
 	private UnitGroup unitGroup;
+	private ImpactMethod method;
 
 	@Before
 	public void setup() {
@@ -46,11 +53,13 @@ public class DQResultTest {
 		createProperty();
 		pFlow1 = createFlow(FlowType.PRODUCT_FLOW);
 		pFlow2 = createFlow(FlowType.PRODUCT_FLOW);
-		eFlow = createFlow(FlowType.ELEMENTARY_FLOW);
+		eFlow1 = createFlow(FlowType.ELEMENTARY_FLOW);
+		eFlow2 = createFlow(FlowType.ELEMENTARY_FLOW);
 		createDQSystem();
-		process1 = createProcess(pFlow1, "(1;2;3;4;5)", "(2;1;4;3;5)", pFlow2);
-		process2 = createProcess(pFlow2, "(5;4;3;2;1)", "(4;5;2;3;1)", null);
+		process1 = createProcess(pFlow1, "(1;2;3;4;5)", 2, pFlow2, 3, "(1;2;3;4;5)", 4, "(5;4;3;2;1)");
+		process2 = createProcess(pFlow2, "(5;4;3;2;1)", 5, "(5;4;3;2;1)", 6, "(1;2;3;4;5)");
 		createProductSystem();
+		createImpactMethod();
 	}
 
 	private void createDQSystem() {
@@ -85,35 +94,37 @@ public class DQResultTest {
 		pSystem = new ProductSystemDao(Tests.getDb()).insert(pSystem);
 	}
 
-	private Process createProcess(Flow pFlow, String dqEntry1, String dqEntry2, Flow inputFlow) {
+	private Process createProcess(Flow pFlow, String dqEntry1, double elemAmount1, String elemDqEntry1,
+			double elemAmount2, String elemDqEntry2) {
+		return createProcess(pFlow, dqEntry1, 0, null, elemAmount1, elemDqEntry1, elemAmount2, elemDqEntry2);
+	}
+
+	private Process createProcess(Flow pFlow, String dqEntry, double inputAmount, Flow inputFlow, double elemAmount1,
+			String elemDqEntry1, double elemAmount2, String elemDqEntry2) {
 		Process process = new Process();
 		process.dqSystem = dqSystem;
-		process.dqEntry = dqEntry1;
+		process.dqEntry = dqEntry;
 		process.exchangeDqSystem = dqSystem;
-		Exchange product = new Exchange();
-		product.setFlow(pFlow);
-		product.setFlowPropertyFactor(pFlow.getReferenceFactor());
-		product.setUnit(unitGroup.getReferenceUnit());
-		product.setAmountValue(1d);
+		Exchange product = createExchange(1, null, pFlow, false);
 		process.getExchanges().add(product);
 		process.setQuantitativeReference(product);
-		Exchange elem = new Exchange();
-		elem.setDqEntry(dqEntry2);
-		elem.setFlow(eFlow);
-		elem.setFlowPropertyFactor(eFlow.getReferenceFactor());
-		elem.setUnit(unitGroup.getReferenceUnit());
-		elem.setAmountValue(3d);
-		process.getExchanges().add(elem);
+		process.getExchanges().add(createExchange(elemAmount1, elemDqEntry1, eFlow1, true));
+		process.getExchanges().add(createExchange(elemAmount2, elemDqEntry2, eFlow2, true));
 		if (inputFlow == null)
 			return new ProcessDao(Tests.getDb()).insert(process);
-		Exchange input = new Exchange();
-		input.setAmountValue(2d);
-		input.setFlow(inputFlow);
-		input.setFlowPropertyFactor(inputFlow.getReferenceFactor());
-		input.setUnit(unitGroup.getReferenceUnit());
-		input.setInput(true);
-		process.getExchanges().add(input);
+		process.getExchanges().add(createExchange(inputAmount, null, inputFlow, true));
 		return new ProcessDao(Tests.getDb()).insert(process);
+	}
+
+	private Exchange createExchange(double amount, String dqEntry, Flow flow, boolean input) {
+		Exchange e = new Exchange();
+		e.setDqEntry(dqEntry);
+		e.setFlow(flow);
+		e.setInput(input);
+		e.setFlowPropertyFactor(flow.getReferenceFactor());
+		e.setUnit(unitGroup.getReferenceUnit());
+		e.setAmountValue(amount);
+		return e;
 	}
 
 	private Flow createFlow(FlowType type) {
@@ -143,26 +154,58 @@ public class DQResultTest {
 		property = new FlowPropertyDao(Tests.getDb()).insert(property);
 	}
 
+	private void createImpactMethod() {
+		method = new ImpactMethod();
+		ImpactCategory c = new ImpactCategory();
+		c.getImpactFactors().add(createFactor(2, eFlow1));
+		c.getImpactFactors().add(createFactor(8, eFlow2));
+		method.getImpactCategories().add(c);
+		method = new ImpactMethodDao(Tests.getDb()).insert(method);
+	}
+
+	private ImpactFactor createFactor(double factor, Flow flow) {
+		ImpactFactor f = new ImpactFactor();
+		f.setValue(factor);
+		f.setFlow(flow);
+		f.setFlowPropertyFactor(flow.getReferenceFactor());
+		f.setUnit(unitGroup.getReferenceUnit());
+		return f;
+	}
+
 	@Test
 	public void test() {
 		SystemCalculator calculator = new SystemCalculator(MatrixCache.createEager(Tests.getDb()),
 				Tests.getDefaultSolver());
 		CalculationSetup setup = new CalculationSetup(pSystem);
 		setup.setAmount(1);
+		setup.impactMethod = Descriptors.toDescriptor(method);
 		ContributionResult cResult = calculator.calculateContributions(setup);
 		DQResult result = DQResult.calculate(Tests.getDb(), cResult, AggregationType.WEIGHTED_AVERAGE, pSystem.getId());
-		Assert.assertArrayEquals(new int[] { 3, 4, 3, 3, 2 }, result.getFlowQuality(eFlow.getId()));
+		long impactId = method.getImpactCategories().get(0).getId();
+		Assert.assertArrayEquals(new int[] { 4, 4, 3, 2, 2 }, result.getFlowQuality(eFlow1.getId()));
+		Assert.assertArrayEquals(new int[] { 2, 3, 3, 4, 4 }, result.getFlowQuality(eFlow2.getId()));
+		Assert.assertArrayEquals(new int[] { 2, 3, 3, 3, 4 }, result.getImpactQuality(impactId));
+		Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, result.getFlowQuality(process1.getId(), eFlow1.getId()));
+		Assert.assertArrayEquals(new int[] { 5, 4, 3, 2, 1 }, result.getFlowQuality(process2.getId(), eFlow1.getId()));
+		Assert.assertArrayEquals(new int[] { 5, 4, 3, 2, 1 }, result.getFlowQuality(process1.getId(), eFlow2.getId()));
+		Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, result.getFlowQuality(process2.getId(), eFlow2.getId()));
+		Assert.assertArrayEquals(new int[] { 3, 3, 3, 3, 3 }, result.getImpactQuality(process1.getId(), impactId));
+		Assert.assertArrayEquals(new int[] { 3, 3, 3, 3, 3 }, result.getImpactQuality(process2.getId(), impactId));
+		Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, result.getProcessQuality(process1.getId()));
+		Assert.assertArrayEquals(new int[] { 5, 4, 3, 2, 1 }, result.getProcessQuality(process2.getId()));
 	}
 
 	@After
 	public void shutdown() {
+		new ImpactMethodDao(Tests.getDb()).delete(method);
 		new ProductSystemDao(Tests.getDb()).delete(pSystem);
 		new ProcessDao(Tests.getDb()).delete(process1);
 		new ProcessDao(Tests.getDb()).delete(process2);
 		new DQSystemDao(Tests.getDb()).delete(dqSystem);
 		new FlowDao(Tests.getDb()).delete(pFlow1);
 		new FlowDao(Tests.getDb()).delete(pFlow2);
-		new FlowDao(Tests.getDb()).delete(eFlow);
+		new FlowDao(Tests.getDb()).delete(eFlow1);
+		new FlowDao(Tests.getDb()).delete(eFlow2);
 		new FlowPropertyDao(Tests.getDb()).delete(property);
 		new UnitGroupDao(Tests.getDb()).delete(unitGroup);
 	}
