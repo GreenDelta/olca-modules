4416bb391ac56fcb6f2d6032cc5aab574f0b91ba
msrocka <michael.srocka@gmail.com>
2016-08-14 12:37:19 +0200

    update process links with updatable cursor

--- olca-core/src/main/java/org/openlca/core/database/upgrades/Upgrade5.java
+++ olca-core/src/main/java/org/openlca/core/database/upgrades/Upgrade5.java
@@ -1,6 +1,20 @@
 package org.openlca.core.database.upgrades;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.NativeSql;
+import org.openlca.core.matrix.LongPair;
+import org.openlca.core.matrix.cache.FlowTypeTable;
+import org.openlca.core.model.FlowType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gnu.trove.impl.Constants;
+import gnu.trove.map.hash.TObjectLongHashMap;
 
 /** Upgrades the database to version 5. */
 class Upgrade5 implements IUpgrade {
@@ -23,7 +37,57 @@ class Upgrade5 implements IUpgrade {
 				"f_process", "BIGINT");
 		util.createColumn("tbl_process_links", "f_exchange",
 				"f_exchange BIGINT");
-		// TODO: fill exchange IDs
+		updateLinks(db);
 	}
 
+	private void updateLinks(IDatabase db) throws Exception {
+		TObjectLongHashMap<LongPair> idx = inputIdx(db);
+		Connection con = db.createConnection();
+		con.setAutoCommit(false);
+		Statement query = con.createStatement();
+		query.setCursorName("UPDATE_LINKS");
+		ResultSet cursor = query.executeQuery(
+				"SELECT f_process, f_flow FROM tbl_process_links "
+						+ "FOR UPDATE of f_exchange");
+		PreparedStatement update = con.prepareStatement(
+				"UPDATE tbl_process_links SET f_exchange = ? " +
+						"WHERE CURRENT OF UPDATE_LINKS");
+		while (cursor.next()) {
+			long processId = cursor.getLong(1);
+			long flowId = cursor.getLong(2);
+			long exchangeId = idx.get(LongPair.of(processId, flowId));
+			if (exchangeId > 0) {
+				update.setLong(1, exchangeId);
+				update.executeUpdate();
+			}
+		}
+		cursor.close();
+		query.close();
+		update.close();
+		con.commit();
+		con.close();
+	}
+
+	private TObjectLongHashMap<LongPair> inputIdx(IDatabase db) {
+		TObjectLongHashMap<LongPair> idx = new TObjectLongHashMap<>(
+				Constants.DEFAULT_CAPACITY, Constants.DEFAULT_LOAD_FACTOR, -1);
+		FlowTypeTable flowTypes = FlowTypeTable.create(db);
+		String sql = "SELECT id, f_owner, f_flow, is_input from tbl_exchanges";
+		try {
+			NativeSql.on(db).query(sql, r -> {
+				long exchangeId = r.getLong(1);
+				long processId = r.getLong(2);
+				long flowId = r.getLong(3);
+				boolean isInput = r.getBoolean(4);
+				if (!isInput || flowTypes.get(flowId) == FlowType.ELEMENTARY_FLOW)
+					return true;
+				idx.put(LongPair.of(processId, flowId), exchangeId);
+				return true;
+			});
+		} catch (Exception e) {
+			Logger log = LoggerFactory.getLogger(getClass());
+			log.error("failed to build exchange index");
+		}
+		return idx;
+	}
 }
