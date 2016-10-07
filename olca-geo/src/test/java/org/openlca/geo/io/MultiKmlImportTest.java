bce81c7c80f673c36a002586a5577a71dad1a29d
msrocka <michael.srocka@gmail.com>
2016-10-07 16:19:46 +0200

    reduced size of test data for multi kml import test

--- olca-geo/src/test/java/org/openlca/geo/io/MultiKmlImportTest.java
+++ olca-geo/src/test/java/org/openlca/geo/io/MultiKmlImportTest.java
@@ -1,14 +1,12 @@
 package org.openlca.geo.io;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.LocationDao;
 import org.openlca.core.database.derby.DerbyDatabase;
-
-import com.google.common.io.Resources;
+import org.openlca.geo.Tests;
 
 public class MultiKmlImportTest {
 
@@ -25,12 +23,10 @@ public class MultiKmlImportTest {
 	@Test
 	public void testImport() throws Exception {
 		Assert.assertEquals(0, dao.getAll().size());
-		String kml = IOUtils.toString(Resources
-				.getResource("org/openlca/geo/US_counties-clipped.shp.kml"),
-				"utf-8");
+		String kml = Tests.getKml("US_counties-clipped.shp.kml");
 		MultiKmlImport parser = new MultiKmlImport(database, kml);
 		parser.parseAndInsert();
-		Assert.assertEquals(697, dao.getAll().size());
+		Assert.assertEquals(5, dao.getAll().size());
 	}
 
 }
