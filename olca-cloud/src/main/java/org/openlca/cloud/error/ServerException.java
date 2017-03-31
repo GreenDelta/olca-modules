2b2972c202042a0374aec3ffaef407e77360bd1b
greve <greve@greendelta.com>
2017-03-31 14:06:53 +0200

    Made server exception public

--- olca-cloud/src/main/java/org/openlca/cloud/error/ServerException.java
+++ olca-cloud/src/main/java/org/openlca/cloud/error/ServerException.java
@@ -4,14 +4,14 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-abstract class ServerException extends WebApplicationException {
+public class ServerException extends WebApplicationException {
 
 	private static final long serialVersionUID = 3597855854783144682L;
 
 	private Status status;
 	private String message;
 
-	ServerException(Status status, String message) {
+	public ServerException(Status status, String message) {
 		this.message = message;
 		this.status = status;
 	}
