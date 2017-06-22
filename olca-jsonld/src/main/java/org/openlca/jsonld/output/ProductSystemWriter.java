de0af9df47833b80ef36382dc3b3477e3c75f5ce
Sebastian Greve <greve@greendelta.com>
2017-06-22 15:00:21 +0200

    Loading elements in one call instead of each independently

--- olca-jsonld/src/main/java/org/openlca/jsonld/output/ProductSystemWriter.java
+++ olca-jsonld/src/main/java/org/openlca/jsonld/output/ProductSystemWriter.java
@@ -1,13 +1,24 @@
 package org.openlca.jsonld.output;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 import org.openlca.core.database.BaseDao;
+import org.openlca.core.database.FlowDao;
+import org.openlca.core.database.ProcessDao;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.FlowProperty;
-import org.openlca.core.model.ModelType;
 import org.openlca.core.model.Process;
 import org.openlca.core.model.ProcessLink;
 import org.openlca.core.model.ProductSystem;
 import org.openlca.core.model.RootEntity;
+import org.openlca.core.model.descriptors.FlowDescriptor;
+import org.openlca.core.model.descriptors.ProcessDescriptor;
 import org.openlca.jsonld.ExchangeKey;
 import org.openlca.util.RefIdMap;
 
@@ -16,6 +27,8 @@ import com.google.gson.JsonObject;
 
 class ProductSystemWriter extends Writer<ProductSystem> {
 
+	private ProcessDao processDao;
+	private FlowDao flowDao;
 	private BaseDao<Exchange> exchangeDao;
 	private RefIdMap<Long, String> idMap;
 	private ProductSystem system;
@@ -23,6 +36,8 @@ class ProductSystemWriter extends Writer<ProductSystem> {
 	ProductSystemWriter(ExportConfig conf) {
 		super(conf);
 		if (conf.db != null) {
+			processDao = new ProcessDao(conf.db);
+			flowDao = new FlowDao(conf.db);
 			exchangeDao = new BaseDao<>(Exchange.class, conf.db);
 			idMap = RefIdMap.internalToRef(conf.db, Process.class);
 		} else {
@@ -48,50 +63,87 @@ class ProductSystemWriter extends Writer<ProductSystem> {
 		Out.put(obj, "targetFlowProperty", property, conf, Out.REQUIRED_FIELD);
 		Out.put(obj, "targetUnit", system.getTargetUnit(), conf, Out.REQUIRED_FIELD);
 		Out.put(obj, "targetAmount", system.getTargetAmount());
-		ParameterRedefs.map(obj, system.getParameterRedefs(), conf.db, conf, (type, id) -> processRef(id));
 		if (conf.db == null)
 			return obj;
-		mapProcesses(obj);
-		mapLinks(obj);
+		Map<Long, ProcessDescriptor> processMap = mapProcesses(obj);
+		mapLinks(obj, processMap);
+		ParameterRedefs.map(obj, system.getParameterRedefs(), conf.db, conf,
+				(type, id) -> References.create(processMap.get(id)));
 		ParameterReferences.writeReferencedParameters(system, conf);
 		return obj;
 	}
 
-	private void mapLinks(JsonObject json) {
+	private void mapLinks(JsonObject json, Map<Long, ProcessDescriptor> processMap) {
 		JsonArray links = new JsonArray();
-		for (ProcessLink link : system.getProcessLinks()) {
-			JsonObject obj = new JsonObject();
-			Out.put(obj, "@type", "ProcessLink");
-			Out.put(obj, "provider", processRef(link.providerId), Out.REQUIRED_FIELD);
-			Out.put(obj, "flow", References.create(ModelType.FLOW, link.flowId, conf, true), Out.REQUIRED_FIELD);
-			JsonObject process = processRef(link.processId);
-			Out.put(obj, "process", process, Out.REQUIRED_FIELD);
-			JsonObject exchange = null;
-			if (exchangeDao != null && process != null && process.has("@id")) {
-				Exchange e = exchangeDao.getForId(link.exchangeId);
-				exchange = getExchange(process.get("@id").getAsString(), e);
+		Map<Long, FlowDescriptor> flowMap = getFlows();
+		Stack<ProcessLink> remaining = new Stack<>();
+		remaining.addAll(system.getProcessLinks());
+		while (!remaining.isEmpty()) {
+			List<ProcessLink> next = new ArrayList<>();
+			while (!remaining.isEmpty() && next.size() < 1000) {
+				next.add(remaining.pop());
 			}
-			Out.put(obj, "exchange", exchange, Out.REQUIRED_FIELD);
-			links.add(obj);
+			Map<Long, Exchange> exchangeMap = getExchanges(next);
+			for (ProcessLink link : next) {
+				JsonObject obj = new JsonObject();
+				Out.put(obj, "@type", "ProcessLink");
+				JsonObject provider = References.create(processMap.get(link.providerId));
+				Out.put(obj, "provider", provider, Out.REQUIRED_FIELD);
+				Out.put(obj, "flow", References.create(flowMap.get(link.flowId)), Out.REQUIRED_FIELD);
+				JsonObject process = References.create(processMap.get(link.processId));
+				Out.put(obj, "process", process, Out.REQUIRED_FIELD);
+				JsonObject exchange = null;
+				if (process != null && process.has("@id")) {
+					Exchange e = exchangeMap.get(link.exchangeId);
+					exchange = getExchange(process.get("@id").getAsString(), e);
+				}
+				Out.put(obj, "exchange", exchange, Out.REQUIRED_FIELD);
+				links.add(obj);
+			}
 		}
 		Out.put(json, "processLinks", links);
 	}
 
-	private void mapProcesses(JsonObject json) {
+	private Map<Long, Exchange> getExchanges(List<ProcessLink> links) {
+		Set<Long> exchangeIds = new HashSet<>();
+		for (ProcessLink link : links) {
+			exchangeIds.add(link.exchangeId);
+		}
+		List<Exchange> exchanges = exchangeDao.getForIds(exchangeIds);
+		Map<Long, Exchange> exchangeMap = new HashMap<>();
+		for (Exchange e : exchanges) {
+			exchangeMap.put(e.getId(), e);
+		}
+		return exchangeMap;
+	}
+
+	private Map<Long, FlowDescriptor> getFlows() {
+		Set<Long> flowIds = new HashSet<>();
+		for (ProcessLink link : system.getProcessLinks()) {
+			flowIds.add(link.flowId);
+		}
+		List<FlowDescriptor> flows = flowDao.getDescriptors(flowIds);
+		Map<Long, FlowDescriptor> flowMap = new HashMap<>();
+		for (FlowDescriptor f : flows) {
+			flowMap.put(f.getId(), f);
+		}
+		return flowMap;
+	}
+
+	private Map<Long, ProcessDescriptor> mapProcesses(JsonObject json) {
 		JsonArray processes = new JsonArray();
-		for (Long pId : system.getProcesses()) {
-			JsonObject ref = processRef(pId);
+		Set<Long> pIds = new HashSet<>(system.getProcesses());
+		List<ProcessDescriptor> descriptors = processDao.getDescriptors(pIds);
+		Map<Long, ProcessDescriptor> map = new HashMap<>();
+		for (ProcessDescriptor descriptor : descriptors) {
+			map.put(descriptor.getId(), descriptor);
+			JsonObject ref = References.create(descriptor);
 			if (ref == null)
 				continue;
 			processes.add(ref);
 		}
 		Out.put(json, "processes", processes);
-	}
-
-	private JsonObject processRef(Long id) {
-		if (id == null)
-			return null;
-		return References.create(ModelType.PROCESS, id, conf, true);
+		return map;
 	}
 
 	private JsonObject getExchange(String processRefId, Exchange e) {
