51ff34c5f0d6fce0030867ed449dac51c13cd63e
msrocka <michael.srocka@gmail.com>
2018-03-26 18:25:11 +0200

    improved parameter import for SimaPro CSV process data sets

--- olca-io/src/main/java/org/openlca/io/simapro/csv/input/GlobalParameterSync.java
+++ olca-io/src/main/java/org/openlca/io/simapro/csv/input/GlobalParameterSync.java
@@ -1,6 +1,7 @@
 package org.openlca.io.simapro.csv.input;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,9 +18,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Imports the project and database parameters from SimaPro as openLCA database
- * parameters. We (currently) do not support calculated parameters at the
- * database level. Thus, we try to evaluate the calculated parameters from
- * SimaPro and store them as input parameters during this import.
+ * parameters.
  */
 class GlobalParameterSync {
 
@@ -28,73 +27,89 @@ class GlobalParameterSync {
 	private final SpRefDataIndex index;
 	private final ParameterDao dao;
 
-	public GlobalParameterSync(SpRefDataIndex index, IDatabase database) {
+	public GlobalParameterSync(SpRefDataIndex index, IDatabase db) {
 		this.index = index;
-		dao = new ParameterDao(database);
+		dao = new ParameterDao(db);
 	}
 
 	public void run() {
 		log.trace("import project and database parameters");
 		List<Parameter> globals = loadGlobals();
+		HashSet<String> added = new HashSet<>();
 		for (InputParameterRow row : index.getInputParameters()) {
 			if (contains(row.getName(), globals))
 				continue;
-			Parameter param = create(row);
+			Parameter param = inputParam(row);
+			added.add(param.getName());
 			globals.add(param);
 		}
-		FormulaInterpreter interpreter = createInterpreter(globals);
 		for (CalculatedParameterRow row : index.getCalculatedParameters()) {
 			if (contains(row.getName(), globals))
 				continue;
-			Parameter param = create(row, interpreter);
+			Parameter param = calculatedParam(row);
 			globals.add(param);
+			added.add(param.getName());
 		}
+		evalAndWrite(globals, added);
 	}
 
-	private FormulaInterpreter createInterpreter(List<Parameter> globals) {
+	private void evalAndWrite(List<Parameter> globals, HashSet<String> added) {
 		FormulaInterpreter interpreter = new FormulaInterpreter();
-		for (Parameter global : globals) {
-			String name = global.getName();
-			String val = Double.toString(global.getValue());
-			interpreter.bind(name, val);
+		for (Parameter param : globals) {
+			interpreter.bind(param.getName(), param.getFormula());
 		}
-		return interpreter;
+		for (Parameter param : globals) {
+			if (!added.contains(param.getName()))
+				continue;
+			if (!param.isInputParameter()) {
+				eval(param, interpreter);
+			}
+			dao.insert(param);
+		}
 	}
 
-	private Parameter create(InputParameterRow row) {
+	private void eval(Parameter p, FormulaInterpreter interpreter) {
+		try {
+			double val = interpreter.eval(p.getFormula());
+			p.setValue(val);
+		} catch (Exception e) {
+			log.warn("failed to evaluate formula for global parameter "
+					+ p.getName() + ": set value to 1.0", e);
+			p.setInputParameter(true);
+			p.setValue(1.0);
+		}
+	}
+
+	private Parameter inputParam(InputParameterRow row) {
 		Parameter p = new Parameter();
 		p.setRefId(UUID.randomUUID().toString());
 		p.setName(row.getName());
 		p.setInputParameter(true);
 		p.setScope(ParameterScope.GLOBAL);
 		p.setValue(row.getValue());
+		p.setFormula(Double.toString(row.getValue()));
 		p.setDescription(row.getComment());
 		p.setUncertainty(Uncertainties.get(row.getValue(),
 				row.getUncertainty()));
-		dao.insert(p);
 		return p;
 	}
 
-	private Parameter create(CalculatedParameterRow row,
-			FormulaInterpreter interpreter) {
+	private Parameter calculatedParam(CalculatedParameterRow row) {
 		Parameter p = new Parameter();
 		p.setRefId(UUID.randomUUID().toString());
 		p.setName(row.getName());
 		p.setScope(ParameterScope.GLOBAL);
 		p.setDescription(row.getComment());
 		p.setInputParameter(false);
-		try {
-			String expr = row.getExpression();
-			double val = interpreter.eval(expr);
-			p.setValue(val);
-			p.setFormula(expr);
-		} catch (Exception e) {
-			log.error("failed to evaluate formula for global parameter "
-					+ row.getName() + ": set value to 1.0", e);
-			p.setInputParameter(true);
-			p.setValue(1.0);
+		String expr = row.getExpression();
+		if (expr.contains("(") && expr.contains(",")) {
+			// openLCA uses semicolons as parameter separators in functions
+			// but SimaPro uses commas here
+			log.warn("Replaced ',' with ';' for global "
+					+ "parameter formula of {}", p.getName());
+			expr = expr.replaceAll(",", ";");
 		}
-		dao.insert(p);
+		p.setFormula(expr);
 		return p;
 	}
 
