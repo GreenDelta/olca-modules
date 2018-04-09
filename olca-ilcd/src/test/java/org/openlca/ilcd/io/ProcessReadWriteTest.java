1f9f21ea5c0fbe2b6a118c6c8d72d98a85ca3c76
msrocka <michael.srocka@gmail.com>
2018-04-09 11:28:48 +0200

    ilcd: ProcessReadWriteTest test something

--- olca-ilcd/src/test/java/org/openlca/ilcd/io/ProcessReadWriteTest.java
+++ olca-ilcd/src/test/java/org/openlca/ilcd/io/ProcessReadWriteTest.java
@@ -1,5 +1,8 @@
 package org.openlca.ilcd.io;
 
+import java.io.ByteArrayInputStream;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.openlca.ilcd.commons.LangString;
 import org.openlca.ilcd.processes.DataSetInfo;
@@ -11,18 +14,27 @@ import org.openlca.ilcd.processes.ProcessName;
 public class ProcessReadWriteTest {
 
 	@Test
-	public void testReadWrite() {
+	public void testReadWrite() throws Exception {
 		Process process = new Process();
 		setNameAndComment(process);
 		createExchange(process);
+		XmlBinder binder = new XmlBinder();
+		byte[] bytes = binder.toByteArray(process);
+		process = binder.fromStream(Process.class,
+				new ByteArrayInputStream(bytes));
+		Assert.assertEquals("process name",
+				process.processInfo.dataSetInfo.name.name.get(0).value);
+		Assert.assertEquals("process description",
+				process.processInfo.dataSetInfo.comment.get(0).value);
+		Assert.assertEquals(1, process.exchanges.size());
 	}
 
 	private void setNameAndComment(Process process) {
-		DataSetInfo dataSetInformation = makeDataSetInfo(process);
+		DataSetInfo info = makeDataSetInfo(process);
 		ProcessName name = new ProcessName();
 		LangString.set(name.name, "process name", "en");
-		dataSetInformation.name = name;
-		LangString.set(dataSetInformation.comment,
+		info.name = name;
+		LangString.set(info.comment,
 				"process description", "en");
 	}
 
