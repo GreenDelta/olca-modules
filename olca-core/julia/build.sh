36506e5f20aaeeb119711cc784f00520d8728e8a
msrocka <michael.srocka@gmail.com>
2018-07-11 14:00:03 +0200

    julia libs: we have other 64bit blas symbols on linux

--- olca-core/julia/build.sh
+++ olca-core/julia/build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-BUILD_LIB="libs/libjolca.so"
-JNI_FLAGS=""
-JNI="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
-
-if [ -f $BUILD_LIB ]; then
-    echo "Delete old version of $BUILD_LIB"
-    rm $BUILD_LIB 
+# delete old versions
+if [ -f "libs/libjolcablas.so" ]; then
+    echo "Delete old version of libjolcablas"
+    rm "libs/libjolcablas.so" 
+fi
+if [ -f "libs/libjolcaumf.so" ]; then
+    echo "Delete old version of libjolcaumf"
+    rm "libs/libjolcaumf.so" 
 fi
 
-echo "Compile new version of $BUILD_LIB"
-gcc $JNI_FLAGS -O3 -DNDEBUG $JNI -L./libs -shared -o $BUILD_LIB lib.c -lopenblas64_ -lumfpack
+# compiler flags
+CFLAGS="-O3 -DNDEBUG -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -L./libs -shared"
+
+echo "Compile new version of libjolcablas"
+gcc $CFLAGS -o libs/libjolcablas.so blas_linux.c -lopenblas64_
+
+echo "Compile new version of libjolcaumf"
+gcc $CFLAGS -o libs/libjolcaumf.so umf.c -lumfpack
 
 echo "All done"
