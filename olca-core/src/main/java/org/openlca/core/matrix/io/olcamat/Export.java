b926eb8fe88355806bb55e167f4f492c1f57752c
msrocka <michael.srocka@gmail.com>
2018-10-25 17:45:47 +0200

    olcamat: export single product systems

--- olca-core/src/main/java/org/openlca/core/matrix/io/olcamat/Export.java
+++ olca-core/src/main/java/org/openlca/core/matrix/io/olcamat/Export.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.openlca.core.database.IDatabase;
+import org.openlca.core.math.CalculationSetup;
+import org.openlca.core.math.DataStructures;
 import org.openlca.core.matrix.CalcExchange;
 import org.openlca.core.matrix.Inventory;
 import org.openlca.core.matrix.InventoryMatrix;
 import org.openlca.core.matrix.LinkingConfig;
 import org.openlca.core.matrix.LongPair;
+import org.openlca.core.matrix.ParameterTable;
 import org.openlca.core.matrix.TechIndex;
 import org.openlca.core.matrix.cache.MatrixCache;
 import org.openlca.core.matrix.format.IMatrix;
@@ -18,6 +21,7 @@ import org.openlca.core.matrix.solvers.DenseSolver;
 import org.openlca.core.matrix.solvers.IMatrixSolver;
 import org.openlca.core.model.AllocationMethod;
 import org.openlca.core.model.ProcessType;
+import org.openlca.expressions.FormulaInterpreter;
 
 /**
  * Exports a matrix into the openLCA matrix (=olcamat) format.The olcamat format
@@ -25,17 +29,24 @@ import org.openlca.core.model.ProcessType;
  */
 public class Export implements Runnable {
 
+	private final CalculationSetup setup;
 	private final IDatabase db;
 	private final MatrixCache cache;
+	private final IMatrixSolver solver;
 	private final File dir;
 
 	public boolean withResults = false;
-	public AllocationMethod allocationMethod = AllocationMethod.USE_DEFAULT;
 
 	public Export(IDatabase db, File dir) {
+		this(null, db, dir);
+	}
+
+	public Export(CalculationSetup setup, IDatabase db, File dir) {
+		this.setup = setup;
 		this.db = db;
 		cache = MatrixCache.createEager(db);
 		this.dir = dir;
+		this.solver = new DenseSolver();
 	}
 
 	@Override
@@ -44,29 +55,42 @@ public class Export implements Runnable {
 			if (!dir.exists()) {
 				dir.mkdirs();
 			}
-			TechIndex techIndex = makeTechIndex();
-			Inventory inv = Inventory.build(cache, techIndex, allocationMethod);
-			DenseSolver solver = new DenseSolver();
-			InventoryMatrix imat = inv.createMatrix(solver);
-			writeMatrices(imat, solver);
-			IndexWriter iw = new IndexWriter(techIndex, imat.flowIndex);
+			InventoryMatrix imat;
+			if (setup == null) {
+				imat = dbInventory();
+			} else {
+				imat = setupInventory();
+			}
+			writeMatrices(imat);
+			IndexWriter iw = new IndexWriter(
+					imat.productIndex, imat.flowIndex);
 			iw.write(db, dir);
 		} catch (Exception e) {
 			throw new RuntimeException("Export failed", e);
 		}
 	}
 
-	private TechIndex makeTechIndex() throws Exception {
+	private InventoryMatrix dbInventory() throws Exception {
 		List<LongPair> products = cache.getProcessTable().getProviderFlows();
 		TechIndex techIndex = new TechIndex(products.get(0));
 		for (int i = 1; i < products.size(); i++) {
 			techIndex.put(products.get(i));
 		}
-		addLinks(techIndex);
-		return techIndex;
+		dbLinks(techIndex);
+		Inventory inv = Inventory.build(cache,
+				techIndex, AllocationMethod.USE_DEFAULT);
+		return inv.createMatrix(solver);
 	}
 
-	private void addLinks(TechIndex idx) throws Exception {
+	private InventoryMatrix setupInventory() {
+		Inventory inventory = DataStructures.createInventory(setup, cache);
+		ParameterTable params = DataStructures.createParameterTable(
+				db, setup, inventory);
+		FormulaInterpreter interpreter = params.createInterpreter();
+		return inventory.createMatrix(solver, interpreter);
+	}
+
+	private void dbLinks(TechIndex idx) throws Exception {
 		LinkingConfig config = new LinkingConfig();
 		config.preferredType = ProcessType.UNIT_PROCESS;
 		ProviderSearch search = new ProviderSearch(
@@ -89,7 +113,7 @@ public class Export implements Runnable {
 		}
 	}
 
-	private void writeMatrices(InventoryMatrix mat, IMatrixSolver solver)
+	private void writeMatrices(InventoryMatrix mat)
 			throws Exception {
 		Matrices.writeDenseColumn(mat.technologyMatrix, new File(dir, "A.bin"));
 		Matrices.writeDenseColumn(mat.interventionMatrix,
