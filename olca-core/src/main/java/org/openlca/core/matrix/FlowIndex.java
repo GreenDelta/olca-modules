74850fde9ced839710ab32db281664bb569334f1
msrocka <michael.srocka@gmail.com>
2018-12-13 13:22:37 +0100

    inline flow and impact builder

--- olca-core/src/main/java/org/openlca/core/matrix/FlowIndex.java
+++ olca-core/src/main/java/org/openlca/core/matrix/FlowIndex.java
@@ -1,9 +1,16 @@
 package org.openlca.core.matrix;
 
-import gnu.trove.map.hash.TLongByteHashMap;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import org.openlca.core.matrix.cache.MatrixCache;
 import org.openlca.core.model.AllocationMethod;
+import org.openlca.core.model.FlowType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gnu.trove.map.hash.TLongByteHashMap;
 
 /**
  * A flow index represents the flows in the intervention matrix. Thus, this
@@ -59,4 +66,73 @@ public class FlowIndex {
 		return flowIndex.size();
 	}
 
+	/**
+	 * Builds a flow index from a product index and exchange table. All flows
+	 * that are not contained in the product index will be added to the flow
+	 * index (except if they are allocated co-products).
+	 */
+	private static class FlowIndexBuilder {
+
+		private Logger log = LoggerFactory.getLogger(getClass());
+
+		private final MatrixCache cache;
+		private final TechIndex techIndex;
+		private final AllocationMethod allocationMethod;
+
+		FlowIndexBuilder(MatrixCache cache, TechIndex techIndex,
+				AllocationMethod allocationMethod) {
+			this.allocationMethod = allocationMethod;
+			this.cache = cache;
+			this.techIndex = techIndex;
+		}
+
+		FlowIndex build() {
+			FlowIndex index = new FlowIndex();
+			Map<Long, List<CalcExchange>> map = loadExchanges();
+			for (Long processId : techIndex.getProcessIds()) {
+				List<CalcExchange> exchanges = map.get(processId);
+				for (CalcExchange e : exchanges) {
+					if (index.contains(e.flowId))
+						continue; // already indexed as flow
+					if (techIndex.contains(e.processId, e.flowId))
+						continue; // the exchange is an output product
+					if (techIndex
+							.isLinked(LongPair.of(e.processId, e.exchangeId)))
+						continue; // the exchange is a linked exchange
+					if (e.flowType == FlowType.ELEMENTARY_FLOW)
+						indexFlow(e, index);
+					if (e.flowType == FlowType.PRODUCT_FLOW && e.isInput)
+						indexFlow(e, index); // unlinked product inputs
+					if (e.flowType == FlowType.WASTE_FLOW && !e.isInput)
+						indexFlow(e, index); // unlinked waste outputs
+					else if (allocationMethod == null
+							|| allocationMethod == AllocationMethod.NONE)
+						indexFlow(e, index); // non-allocated co-product or
+												// waste
+												// treatment -> handle
+												// like elementary flow
+				}
+			}
+			return index;
+		}
+
+		private Map<Long, List<CalcExchange>> loadExchanges() {
+			try {
+				Map<Long, List<CalcExchange>> map = cache.getExchangeCache()
+						.getAll(techIndex.getProcessIds());
+				return map;
+			} catch (Exception e) {
+				log.error("failed to load exchanges from cache", e);
+				return Collections.emptyMap();
+			}
+		}
+
+		private void indexFlow(CalcExchange e, FlowIndex index) {
+			if (e.isInput)
+				index.putInputFlow(e.flowId);
+			else
+				index.putOutputFlow(e.flowId);
+		}
+
+	}
 }
--- olca-core/src/main/java/org/openlca/core/matrix/ImpactTable.java
+++ olca-core/src/main/java/org/openlca/core/matrix/ImpactTable.java
@@ -1,9 +1,20 @@
 package org.openlca.core.matrix;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
 import org.openlca.core.matrix.cache.MatrixCache;
 import org.openlca.core.matrix.format.IMatrix;
 import org.openlca.core.matrix.solvers.IMatrixSolver;
 import org.openlca.expressions.FormulaInterpreter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The ImpactTable is the corresponding data type to the Inventory type but
@@ -61,4 +72,87 @@ public class ImpactTable {
 		factorMatrix.eval(interpreter);
 	}
 
+	private static class ImpactTableBuilder {
+
+		private Logger log = LoggerFactory.getLogger(getClass());
+
+		private final MatrixCache cache;
+		private final long methodId;
+		private final FlowIndex flowIndex;
+
+		ImpactTableBuilder(MatrixCache cache, long impactMethodId,
+				FlowIndex flowIndex) {
+			this.cache = cache;
+			this.methodId = impactMethodId;
+			this.flowIndex = flowIndex;
+		}
+
+		ImpactTable build() {
+			log.trace("Build impact factor matrix for method {}", methodId);
+			LongIndex categoryIndex = buildCategoryIndex();
+			if (categoryIndex.isEmpty() || flowIndex.isEmpty())
+				return null;
+			ImpactTable table = new ImpactTable();
+			table.categoryIndex = categoryIndex;
+			table.flowIndex = flowIndex;
+			ImpactFactorMatrix matrix = new ImpactFactorMatrix(
+					categoryIndex.size(), flowIndex.size());
+			table.factorMatrix = matrix;
+			fill(matrix, categoryIndex);
+			log.trace("Impact factor matrix ready");
+			return table;
+		}
+
+		private LongIndex buildCategoryIndex() {
+			LongIndex index = new LongIndex();
+			try (Connection con = cache.getDatabase().createConnection()) {
+				String query = "select id from tbl_impact_categories where f_impact_method = "
+						+ methodId;
+				ResultSet result = con.createStatement().executeQuery(query);
+				while (result.next()) {
+					long id = result.getLong("id");
+					index.put(id);
+				}
+				result.close();
+			} catch (Exception e) {
+				log.error("failed to build impact category index", e);
+			}
+			return index;
+		}
+
+		private void fill(ImpactFactorMatrix matrix, LongIndex categoryIndex) {
+			Map<Long, List<CalcImpactFactor>> factorMap = loadFactors(
+					categoryIndex);
+			for (int row = 0; row < categoryIndex.size(); row++) {
+				long categoryId = categoryIndex.getKeyAt(row);
+				List<CalcImpactFactor> factors = factorMap.get(categoryId);
+				if (factors == null)
+					continue;
+				for (CalcImpactFactor factor : factors) {
+					long flowId = factor.getFlowId();
+					int col = flowIndex.getIndex(flowId);
+					if (col < 0)
+						continue;
+					boolean input = flowIndex.isInput(flowId);
+					ImpactFactorCell cell = new ImpactFactorCell(factor,
+							methodId,
+							input);
+					matrix.setEntry(row, col, cell);
+				}
+			}
+		}
+
+		private Map<Long, List<CalcImpactFactor>> loadFactors(
+				LongIndex categoryIndex) {
+			try {
+				Set<Long> keys = LongStream.of(categoryIndex.getKeys())
+						.boxed()
+						.collect(Collectors.toSet());
+				return cache.getImpactCache().getAll(keys);
+			} catch (Exception e) {
+				log.error("failed to load impact factors");
+				return Collections.emptyMap();
+			}
+		}
+	}
 }
