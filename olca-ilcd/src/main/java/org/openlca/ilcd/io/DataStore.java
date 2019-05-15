0694e00f60417a8e0117f4b12abf24686b938845
msrocka <michael.srocka@gmail.com>
2019-05-15 17:41:49 +0200

    add an `each` method

--- olca-ilcd/src/main/java/org/openlca/ilcd/io/DataStore.java
+++ olca-ilcd/src/main/java/org/openlca/ilcd/io/DataStore.java
@@ -4,9 +4,12 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Iterator;
+import java.util.function.Consumer;
 
 import org.openlca.ilcd.commons.IDataSet;
 import org.openlca.ilcd.sources.Source;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface DataStore extends Closeable {
 
@@ -25,4 +28,18 @@ public interface DataStore extends Closeable {
 
 	<T> boolean contains(Class<T> type, String id) throws DataStoreException;
 
+	default <T extends IDataSet> void each(Class<T> type, Consumer<T> fn) {
+		try {
+			Iterator<T> it = iterator(type);
+			while (it.hasNext()) {
+				T t = it.next();
+				if (t != null) {
+					fn.accept(t);
+				}
+			}
+		} catch (Exception e) {
+			Logger log = LoggerFactory.getLogger(getClass());
+			log.error("failed to iterate over elements of type " + type, e);
+		}
+	}
 }
