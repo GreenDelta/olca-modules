29a59d40d60d79426d65bd615e90fd95113c1f80
msrocka <michael.srocka@gmail.com>
2019-06-04 17:41:28 +0200

    add some utilities for creating `FlowRef` objects faster
    ```clojure
    (import
      (org.openlca.core.database LocationDao)
      (org.openlca.util CategoryPathBuilder)
      (org.openlca.app.db Database))
    
    (-> (CategoryPathBuilder. (Database/get))
      (.build 973)
      (println))
    
    (.getCodes (LocationDao. (Database/get)))
    ```

--- olca-core/src/main/java/org/openlca/core/database/LocationDao.java
+++ olca-core/src/main/java/org/openlca/core/database/LocationDao.java
@@ -1,12 +1,37 @@
 package org.openlca.core.database;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openlca.core.model.Location;
 import org.openlca.core.model.descriptors.LocationDescriptor;
 
-public class LocationDao extends CategorizedEntityDao<Location, LocationDescriptor> {
+public class LocationDao
+		extends CategorizedEntityDao<Location, LocationDescriptor> {
 
 	public LocationDao(IDatabase database) {
 		super(Location.class, LocationDescriptor.class, database);
+	}
+
+	/**
+	 * Get the location codes from the database in a map: location id ->
+	 * location code.
+	 */
+	public Map<Long, String> getCodes() {
+		if (database == null)
+			return Collections.emptyMap();
+		String sql = "select id, code from tbl_locations";
+		Map<Long, String> map = new HashMap<>();
+		try {
+			NativeSql.on(database).query(sql, r -> {
+				map.put(r.getLong(1), r.getString(2));
+				return true;
+			});
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return map;
 	}
 
 }
