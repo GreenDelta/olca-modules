0a08b23a4b8fe8d9d0483b836d460c14f9c362f5
msrocka <michael.srocka@gmail.com>
2019-08-22 13:55:33 +0200

    check that the units in the mapping file
    are saved and loaded correctly

--- olca-io/src/test/java/org/openlca/io/maps/FlowMapTest.java
+++ olca-io/src/test/java/org/openlca/io/maps/FlowMapTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 
 import org.junit.Test;
 import org.openlca.core.model.descriptors.FlowDescriptor;
+import org.openlca.core.model.descriptors.UnitDescriptor;
 
 public class FlowMapTest {
 
@@ -21,9 +22,16 @@ public class FlowMapTest {
 		e.sourceFlow = new FlowRef();
 		e.sourceFlow.flow = new FlowDescriptor();
 		e.sourceFlow.flow.refId = "06d4812b-6937-4d64-8517-b69aabce3648";
+		e.sourceFlow.unit = new UnitDescriptor();
+		e.sourceFlow.unit.refId = "some-unit-id";
+		e.sourceFlow.unit.name = "kg";
+
 		e.targetFlow = new FlowRef();
 		e.targetFlow.flow = new FlowDescriptor();
 		e.targetFlow.flow.refId = "3f7e0b7e-aefd-4efd-a946-7bdad142fd50";
+		e.targetFlow.unit = new UnitDescriptor();
+		e.targetFlow.unit.refId = "some-other-unit-id";
+		e.targetFlow.unit.name = "m3";
 
 		// write & read
 		File tmpFile = Files.createTempFile(
@@ -39,6 +47,11 @@ public class FlowMapTest {
 		assertEquals(1000.0, e.factor, 1e-16);
 		assertNull(e.sourceFlow.status);
 		assertNull(e.targetFlow.status);
-	}
 
+		// check units
+		assertEquals("some-unit-id", e.sourceFlow.unit.refId);
+		assertEquals("kg", e.sourceFlow.unit.name);
+		assertEquals("some-other-unit-id", e.targetFlow.unit.refId);
+		assertEquals("m3", e.targetFlow.unit.name);
+	}
 }
