4f7eeac4424833804ec6923051ff1e240874b60a
msrocka <michael.srocka@gmail.com>
2019-09-12 10:47:27 +0200

    moving the database upgrades back to the core
    this is currently not used in the application but we will move the
    database
    upgrade functions back to the core module. the idea is to remove the
    current
    database update manager that uses a specific package format where a
    Python
    script is executed to run an update. the initial idea of this was that
    we
    can not only update the database schema but also the data in a database
    with
    attached data files in such an update package. however, we used this
    data
    update functionality and now there is the collaboration server that
    exactly
    provides this functionality.

---
+++ olca-core/src/main/java/org/openlca/core/database/upgrades/DbUtil.java
@@ -0,0 +1,164 @@
+package org.openlca.core.database.upgrades;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.NativeSql;
+import org.openlca.core.database.derby.DerbyDatabase;
+import org.openlca.core.database.mysql.MySQLDatabase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DbUtil {
+
+	private final int TYPE_DERBY = 0;
+	private final int TYPE_MYSQL = 1;
+
+	private final int dbType;
+	private IDatabase database;
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	public DbUtil(IDatabase database) {
+		this.database = database;
+		if (database instanceof MySQLDatabase)
+			dbType = TYPE_MYSQL;
+		else
+			dbType = TYPE_DERBY;
+	}
+
+	/** Get the database type for storing long text values. */
+	public String getTextType() {
+		switch (dbType) {
+		case TYPE_DERBY:
+			return "CLOB(64 K)";
+		case TYPE_MYSQL:
+			return "TEXT";
+		default:
+			return "CLOB(64 K)";
+		}
+	}
+
+	public String getBlobType() {
+		switch (dbType) {
+		case TYPE_DERBY:
+			return "BLOB(16 M)";
+		case TYPE_MYSQL:
+			return "MEDIUMBLOB";
+		default:
+			return "BLOB(16 M)";
+		}
+	}
+
+	/**
+	 * Deletes the table with the given name from the database if it exists.
+	 */
+	public void dropTable(String tableName) throws Exception {
+		log.trace("Try to drop table {}", tableName);
+		if (!tableExists(tableName)) {
+			log.trace("Table {} does not exist", tableName);
+		} else {
+			NativeSql.on(database).runUpdate("DROP TABLE " + tableName);
+		}
+	}
+
+	/**
+	 * Checks if a table with the given name exists in the database. If not it
+	 * is created using the given table definition.
+	 */
+	public void createTable(String table, String tableDef) throws Exception {
+		log.trace("Check if table {} exists", table);
+		if (tableExists(table))
+			log.trace("table exists");
+		else {
+			log.info("create table {}", table);
+			NativeSql.on(database).runUpdate(tableDef);
+		}
+	}
+
+	/**
+	 * Returns true if a table with the given name exits.
+	 */
+	public boolean tableExists(String tableName) throws Exception {
+		try (Connection con = database.createConnection()) {
+			DatabaseMetaData metaData = con.getMetaData();
+			try (ResultSet rs = metaData.getTables(null, null, "%", null)) {
+				while (rs.next()) {
+					String otherName = rs.getString(3);
+					if (tableName.equalsIgnoreCase(otherName))
+						return true;
+				}
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * Checks if a column with the given name exists in the table with the given
+	 * name. If not, it is created using the given column definition.
+	 */
+	public boolean createColumn(String table, String column, String columnDef) throws Exception {
+		log.trace("Check if column {} exists in {}", column, table);
+		if (columnExists(table, column)) {
+			log.trace("column exists");
+			return false;
+		}
+		log.info("add column {} to {}", column, table);
+		String stmt = "ALTER TABLE " + table + " ADD COLUMN " + columnDef;
+		NativeSql.on(database).runUpdate(stmt);
+		return true;
+	}
+
+	/** Deletes the given column from the given table if it exists. */
+	public boolean dropColumn(String table, String column) throws Exception {
+		log.trace("drop column {} in table {}", column, table);
+		if (!columnExists(table, column))
+			return false;
+		String stmt = "ALTER TABLE " + table + " DROP COLUMN " + column;
+		NativeSql.on(database).runUpdate(stmt);
+		return true;
+	}
+
+	/**
+	 * Returns true if the column with the given name exists in the table with
+	 * the given name.
+	 */
+	public boolean columnExists(String tableName, String columnName) throws Exception {
+		try (Connection con = database.createConnection()) {
+			DatabaseMetaData metaData = con.getMetaData();
+			try (ResultSet rs = metaData.getColumns(null, null, "%", "%")) {
+				while (rs.next()) {
+					String tName = rs.getString(3);
+					String cName = rs.getString(4);
+					if (tName.equalsIgnoreCase(tableName) && cName.equalsIgnoreCase(columnName))
+						return true;
+				}
+				return false;
+			}
+		}
+	}
+
+	public void renameColumn(String table, String oldName, String newName, String dataType) throws Exception {
+		if (columnExists(table, newName))
+			return;
+		log.trace("rename column {}.{} to {}", table, oldName, newName);
+		if (!columnExists(table, oldName)) {
+			log.error("column {}.{} does not exists", table, oldName);
+			return;
+		}
+		String query = null;
+		if (database instanceof DerbyDatabase)
+			query = "RENAME COLUMN " + table + "." + oldName + " TO " + newName;
+		else
+			query = "ALTER TABLE " + table + " CHANGE " + oldName + " " + newName + " " + dataType;
+		NativeSql.on(database).runUpdate(query);
+	}
+
+	public void setVersion(int v) throws SQLException {
+		NativeSql.on(database).runUpdate("UPDATE openlca_version SET version = " + v);
+	}
+
+}
---
+++ olca-core/src/main/java/org/openlca/core/database/upgrades/Upgrades.java
@@ -0,0 +1,56 @@
+package org.openlca.core.database.upgrades;
+
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.NativeSql;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provides methods for checking the version of a database and upgrading
+ * databases.
+ */
+public class Upgrades {
+
+	private final IUpgrade[] upgrades = {
+			new Upgrade3(),
+			new Upgrade4()	};
+
+	private Logger log = LoggerFactory.getLogger(Upgrades.class);
+	// This way of upgrading the database was removed in version 1.6.0, but is still kept for downwards compatibility
+	public static final int FINAL_UPGRADE = 4;
+	
+	private Upgrades() {
+	}
+
+
+	public static void runUpgrades(IDatabase database) throws Exception {
+		Upgrades upgrades = new Upgrades();
+		upgrades.run(database);
+	}
+
+	private void run(IDatabase database) throws Exception {
+		IUpgrade nextUpgrade = null;
+		while ((nextUpgrade = findNextUpgrade(database)) != null) {
+			log.trace("execute update from v({}) to v{}", nextUpgrade.getInitialVersions(),
+					nextUpgrade.getEndVersion());
+			nextUpgrade.exec(database);
+			updateVersion(nextUpgrade.getEndVersion(), database);
+		}
+		log.trace("no more upgrades");
+	}
+
+	private void updateVersion(int version, IDatabase database) throws Exception {
+		NativeSql.on(database).runUpdate("update openlca_version set version = " + version);
+	}
+
+	private IUpgrade findNextUpgrade(IDatabase database) {
+		int version = database.getVersion();
+		for (IUpgrade upgrade : upgrades) {
+			for (int upgradeVersion : upgrade.getInitialVersions()) {
+				if (upgradeVersion == version)
+					return upgrade;
+			}
+		}
+		return null;
+	}
+}
