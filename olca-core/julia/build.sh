55b57b3006a4c6377a17cbf64c26cc0ea680d7c4
Jaco Kruger <jkruger@ecochain.com>
2019-09-13 11:19:43 +0200

    trying to align julia native library build script with Julia solver expectations

--- olca-core/julia/build.sh
+++ olca-core/julia/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # delete old versions
-if [ -f "libs/libjolcablas.so" ]; then
-    echo "Delete old version of libjolcablas"
-    rm "libs/libjolcablas.so" 
+if [ -f "libs/libolcar.so" ]; then
+    echo "Delete old version of libolcar"
+    rm "libs/libolcar.so"
 fi
-if [ -f "libs/libjolcaumf.so" ]; then
-    echo "Delete old version of libjolcaumf"
-    rm "libs/libjolcaumf.so" 
+if [ -f "libs/libolcar_withumf.so" ]; then
+    echo "Delete old version of libolcar_withumf"
+    rm "libs/libolcar_withumf.so"
 fi
 
 # compiler flags
 CFLAGS="-O3 -DNDEBUG -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -L./libs -shared"
 
-echo "Compile new version of libjolcablas"
-gcc $CFLAGS -o libs/libjolcablas.so blas_linux.c -lopenblas64_
+echo "Compile new version of libolcar"
+gcc $CFLAGS -o libs/libolcar.so blas_linux.c -lopenblas64_
 
-echo "Compile new version of libjolcaumf"
-gcc $CFLAGS -o libs/libjolcaumf.so umf.c -lumfpack
+echo "Compile new version of libolcar_withumf"
+gcc $CFLAGS -o libs/libolcar_withumf.so umf.c -lumfpack
 
 echo "All done"
