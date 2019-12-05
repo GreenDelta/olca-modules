a7e9788151286757a0f205f4bf56cd233cf93da4
msrocka <michael.srocka@gmail.com>
2019-12-05 09:24:39 +0100

    the import works now
    ```java
    public static void main(String[] args) {
      String dbDir = "C:\\Users\\ms\\openLCA-data-1.4\\databases\\zztemp";
      String zipPath = "C:\\Users\\ms\\Desktop\\rems\\LCIA_methods_3.6.zip";
      try {
        IDatabase db = new DerbyDatabase(new File(dbDir));
        File[] files = new File[]{new File(zipPath)};
        MethodImport imp = new MethodImport(files, db);
        imp.run();
      } catch (Exception e) {
        e.printStackTrace();
      }
    }
    ```

--- olca-ecospold-2/src/main/java/spold2/ImpactIndicator.java
+++ olca-ecospold-2/src/main/java/spold2/ImpactIndicator.java
@@ -4,6 +4,8 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
+import java.util.List;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ImpactIndicator {
@@ -16,4 +18,7 @@ public class ImpactIndicator {
 
 	@XmlElement(name = "unitName")
 	public String unit;
+
+	@XmlElement(name = "factor")
+	public final List<ImpactFactor> factors = new ArrayList<>();
 }
