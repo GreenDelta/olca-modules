33b1fdf618e3c9df53e7976b80910a8955d6cdc4
msrocka <michael.srocka@gmail.com>
2019-12-10 18:21:49 +0100

    so we need internal API to close a MappedByteBuffer?
    https://stackoverflow.com/questions/25238110/how-to-properly-close-mappedbytebuffer

---
+++ olca-core/src/main/java/org/openlca/core/matrix/io/npy/HeaderReader.java
@@ -0,0 +1,202 @@
+package org.openlca.core.matrix.io.npy;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Reads the header information from an NPY file and parses NPY header strings.
+ * This parser is in principle a Java implementation of this Julia parser:
+ * https://github.com/fhs/NPZ.jl/blob/master/src/NPZ.jl
+ */
+class HeaderReader {
+
+	/**
+	 * Read the header information from an NPY file.
+	 */
+	static Header read(InputStream stream) {
+		try (BufferedInputStream in = new BufferedInputStream(stream, 16)) {
+			// The first 6 bytes are a magic string: exactly \x93NUMPY.
+			byte[] bytes = new byte[6];
+			int n = in.read(bytes);
+			if (n != 6) {
+				throw new IllegalArgumentException("Not a npy file.");
+			}
+			String numpy = new String(bytes, 1, 5);
+			if (!numpy.equals("NUMPY")) {
+				throw new IllegalArgumentException("Not a npy file.");
+			}
+
+			// check the version
+			int v = in.read();
+			if (v < 1) {
+				throw new IllegalArgumentException(
+						"Invalid npy file, a major version >= 1 is required.");
+			}
+
+			// skip the minor version
+			if (in.read() < 0) {
+				throw new IllegalArgumentException(
+						"Invalid npy file, could not read minor version.");
+			}
+
+			// read the header length
+			bytes = new byte[2];
+			n = in.read(bytes);
+			if (n != 2) {
+				throw new IllegalArgumentException("Not a npy file.");
+			}
+
+			// convert the unsigned short
+			ByteBuffer buff = ByteBuffer.wrap(bytes);
+			buff.order(ByteOrder.LITTLE_ENDIAN);
+			int headerLength = buff.getShort() & 0xffff;
+
+			// read the header string; hoping everything is ASCII
+			bytes = new byte[headerLength];
+			if (in.read(bytes) != headerLength) {
+				throw new RuntimeException(
+						"Failed to read " + headerLength + " header bytes");
+			}
+
+			Header header = HeaderReader.parse(new String(bytes));
+			header.dataOffset = headerLength + 10;
+			return header;
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to read header", e);
+		}
+	}
+
+	/**
+	 * Parses the header information from the given string.
+	 */
+	static Header parse(String s) {
+		Header header = new Header();
+		Ref ref = new Ref();
+		ref.s = s.trim();
+		parseChar(ref, '{');
+
+		for (int i = 0; i < 3; i++) {
+			ref.strip();
+			String key = parseString(ref);
+			ref.strip();
+			parseChar(ref, ':');
+			ref.strip();
+			switch (key) {
+				case "descr":
+					header.dtype = parseString(ref);
+					break;
+				case "fortran_order":
+					header.fortranOrder = parseBoolean(ref);
+					break;
+				case "shape":
+					header.shape = parseTuple(ref);
+					break;
+				default:
+					throw new IllegalStateException(
+							"parsing header failed: bad dictionary key");
+			}
+			ref.strip();
+			if (ref.s.charAt(0) == '}')
+				break;
+			parseChar(ref, ',');
+		}
+
+		ref.strip();
+		parseChar(ref, '}');
+		ref.strip();
+		if (ref.s.length() != 0) {
+			throw new IllegalStateException(
+					"malformed header");
+		}
+		return header;
+	}
+
+	private static String parseString(Ref ref) {
+		parseChar(ref, '\'');
+		int i = ref.s.indexOf('\'');
+		if (i < 0) {
+			throw new IllegalStateException(
+					"parsing header failed: malformed string");
+		}
+		String s = ref.s.substring(0, i);
+		ref.s = ref.s.substring(i + 1);
+		return s;
+	}
+
+	private static boolean parseBoolean(Ref ref) {
+		if (ref.s.substring(0, 4).equals("True")) {
+			ref.s = ref.s.substring(4);
+			return true;
+		}
+		if (ref.s.substring(0, 5).equals("False")) {
+			ref.s = ref.s.substring(5);
+			return false;
+		}
+		throw new IllegalStateException(
+				"parsing header failed: excepted True or False");
+	}
+
+	private static int parseInt(Ref ref) {
+		int len = 0;
+		for (int i = 0; i < ref.s.length(); i++) {
+			char c = ref.s.charAt(i);
+			if (!Character.isDigit(c)) {
+				break;
+			}
+			len++;
+		}
+		if (len == 0) {
+			throw new IllegalStateException(
+					"parsing header failed: no digits");
+		}
+		String intstr = ref.s.substring(0, len);
+		ref.s = ref.s.substring(len);
+		return Integer.parseInt(intstr);
+	}
+
+	private static int[] parseTuple(Ref ref) {
+		parseChar(ref, '(');
+		List<Integer> tup = new ArrayList<>();
+		while (true) {
+			ref.strip();
+			if (ref.s.charAt(0) == ')')
+				break;
+			int n = parseInt(ref);
+			tup.add(n);
+			ref.strip();
+			if (ref.s.charAt(0) == ')')
+				break;
+			parseChar(ref, ',');
+		}
+		parseChar(ref, ')');
+
+		int[] tuple = new int[tup.size()];
+		for (int i = 0; i < tup.size(); i++) {
+			tuple[i] = tup.get(i);
+		}
+		return tuple;
+	}
+
+	private static void parseChar(Ref ref, char c) {
+		char first = ref.s.charAt(0);
+		if (first != c) {
+			throw new IllegalStateException(
+					"parsing header failed: expected character '"
+							+ c + "', found '" + first + "'");
+		}
+		ref.s = ref.s.substring(1);
+	}
+
+	private static class Ref {
+		String s;
+
+		void strip() {
+			s = s.trim();
+		}
+	}
+}
--- olca-core/src/test/java/org/openlca/core/matrix/io/npy/HeaderTest.java
+++ olca-core/src/test/java/org/openlca/core/matrix/io/npy/HeaderTest.java
@@ -2,17 +2,59 @@ package org.openlca.core.matrix.io.npy;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openlca.core.matrix.format.DenseMatrix;
+import sun.misc.Cleaner;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 
 public class HeaderTest {
 
 	@Test
 	public void testParseString() {
 		String s = "{'descr': '<i4', 'fortran_order': False, 'shape': (2,), }";
-		Header h = Header.parse(s);
+		Header h = Header.read(s);
 		Assert.assertEquals("<i4", h.dtype);
 		Assert.assertArrayEquals(new int[]{2}, h.shape);
 		Assert.assertFalse(h.fortranOrder);
 		Assert.assertEquals(s, h.toString());
+	}
+
+	@Test
+	public void testFromReadFile() throws Exception {
+		DenseMatrix m = new DenseMatrix(2, 3);
+		File file = Files.createTempFile("_olca_npy_tests", ".npy").toFile();
+		System.out.println(file.getAbsolutePath());
+		Npy.save(file, m);
+		Header h = Header.read(file);
+		Header h2 = Header.read(file);
+		Assert.assertEquals(h.toString(), h2.toString());
+		Assert.assertEquals("<f8", h.dtype);
+		Assert.assertTrue(h.fortranOrder);
+		Assert.assertArrayEquals(new int[]{2, 3}, h.shape);
+		Assert.assertTrue(file.delete());
+	}
+
+	@Test
+	public void testDeleteTempFile() throws Exception {
+		File file = Files.createTempFile("_file_del_test", ".txt").toFile();
+		try (RandomAccessFile f = new RandomAccessFile(file, "rw");
+		     FileChannel chan = f.getChannel()) {
+			MappedByteBuffer buf = chan.map(
+					FileChannel.MapMode.READ_WRITE, 0, 42);
+			for (int i = 0; i < 42; i++) {
+				buf.put((byte) 42);
+			}
+			buf.force();
+			Cleaner cleaner = ((sun.nio.ch.DirectBuffer) buf).cleaner();
+			if (cleaner != null) {
+				cleaner.clean();
+			}
+		}
+		Assert.assertTrue(file.delete());
 	}
 
 }
