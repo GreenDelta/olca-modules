71f9bd2cdc2c93f7ee05b67845be986e95828c61
msrocka <michael.srocka@gmail.com>
2020-01-08 16:31:26 +0100

    getting a single openLCA LCIA method for an ILCD LCIA indicator
    is not valid anymore. thus, we need to find a better way to map
    LCIA indicators to methods in the import. unfortunately, LCIA
    method names are stored as plain strings in the ILCD datasets;
    no UUIDs can be assigned.
    
    ```java
    public ImpactMethod getFromExtension() {
       QName refIdAtt = new QName("http://openlca.org/ilcd-extensions",
            "olca_method_uuid");
      String refId = ilcdMethod.otherAttributes.get(refIdAtt);
      if (refId == null)
        return null;
      ImpactMethod method = dao.getForRefId(refId);
      return method != null ? method : createFromExtension(refId);
    }
    ```
    
    also, the type is better added to the LCIA indicator?
    
    ```java
    private String getType() {
      Modelling mav = ilcdMethod.modelling;
      if (mav == null || mav.normalisationAndWeighting == null)
        return null;
      LCIAMethodType type = mav.normalisationAndWeighting.type;
      return type == null ? null : type.value();
    }
    ```

--- olca-io/src/main/java/org/openlca/io/ilcd/input/MethodFetch.java
+++ olca-io/src/main/java/org/openlca/io/ilcd/input/MethodFetch.java
@@ -1,22 +1,25 @@
 package org.openlca.io.ilcd.input;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
-
-import javax.xml.namespace.QName;
 
 import org.openlca.core.database.ImpactMethodDao;
 import org.openlca.core.model.ImpactMethod;
 import org.openlca.ilcd.methods.LCIAMethod;
-import org.openlca.ilcd.methods.LCIAMethodType;
 import org.openlca.ilcd.methods.MethodInfo;
-import org.openlca.ilcd.methods.Modelling;
 import org.openlca.util.KeyGen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Get possible LCIA methods for the given ILCD data set. In an ILCD LCIA
+ * data set the LCIA methods of an indicator are stored as plain strings in
+ * the data set. This function tries to find corresponding LCIA methods in
+ * the openLCA database or creates them when no method can be found for a
+ * name.
+ */
 final class MethodFetch {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
@@ -29,14 +32,7 @@ final class MethodFetch {
 		this.ilcdMethod = ilcdMethod;
 	}
 
-	/**
-	 * Gets the LCIA method(s) for the given ILCD data set from the openLCA
-	 * database. If no method can be found a new one is created. Note that a
-	 * LCIA-Method data set in ILCD is the same as an LCIA-Category data set in
-	 * openLCA.
-	 */
-	public static List<ImpactMethod> getOrCreate(LCIAMethod ilcdMethod,
-			ImportConfig config) {
+	public static List<ImpactMethod> get(LCIAMethod ilcdMethod, ImportConfig config) {
 		if (ilcdMethod == null || config.db == null)
 			return Collections.emptyList();
 		else
@@ -45,62 +41,19 @@ final class MethodFetch {
 
 	private List<ImpactMethod> run() {
 		try {
-			ImpactMethod method = getFromExtension();
-			if (method != null)
-				return Arrays.asList(method);
-			else
-				return getFromNames();
+			List<String> names = getMethodNames();
+			if (names.isEmpty())
+				return Collections.emptyList();
+			List<ImpactMethod> methods = new ArrayList<>();
+			for (String name : names) {
+				ImpactMethod method = getFromName(name);
+				methods.add(method);
+			}
+			return methods;
 		} catch (Exception e) {
 			log.error("failed to search / create LCIA method in database", e);
 			return Collections.emptyList();
 		}
-	}
-
-	public ImpactMethod getFromExtension() {
-		QName refIdAtt = new QName("http://openlca.org/ilcd-extensions",
-				"olca_method_uuid");
-		String refId = ilcdMethod.otherAttributes.get(refIdAtt);
-		if (refId == null)
-			return null;
-		ImpactMethod method = dao.getForRefId(refId);
-		return method != null ? method : createFromExtension(refId);
-	}
-
-	private ImpactMethod createFromExtension(String refId) {
-		ImpactMethod method = new ImpactMethod();
-		method.refId = refId;
-		List<String> names = getMethodNames();
-		String name = names.isEmpty() ? "LCIA Method" : names.get(0);
-		method.name = name;
-		dao.insert(method);
-		return method;
-	}
-
-	private List<ImpactMethod> getFromNames() {
-		List<ImpactMethod> methods = new ArrayList<>();
-		List<String> names = getMethodNames();
-		String type = getType();
-		for (String methodName : names) {
-			String name = methodName;
-			if (type != null)
-				name += " (" + type + ")";
-			ImpactMethod method = getFromName(name);
-			if (method != null)
-				methods.add(method);
-		}
-		return methods;
-	}
-
-	private ImpactMethod getFromName(String name) {
-		String refId = KeyGen.get(name);
-		org.openlca.core.model.ImpactMethod method = dao.getForRefId(refId);
-		if (method == null) {
-			method = new org.openlca.core.model.ImpactMethod();
-			method.refId = refId;
-			method.name = name;
-			dao.insert(method);
-		}
-		return method;
 	}
 
 	private List<String> getMethodNames() {
@@ -110,11 +63,16 @@ final class MethodFetch {
 		return info.dataSetInfo.methods;
 	}
 
-	private String getType() {
-		Modelling mav = ilcdMethod.modelling;
-		if (mav == null || mav.normalisationAndWeighting == null)
-			return null;
-		LCIAMethodType type = mav.normalisationAndWeighting.type;
-		return type == null ? null : type.value();
+	private ImpactMethod getFromName(String name) {
+		String refId = KeyGen.get(name);
+		ImpactMethod method = dao.getForRefId(refId);
+		if (method == null) {
+			method = new org.openlca.core.model.ImpactMethod();
+			method.refId = refId;
+			method.name = name;
+			method.lastChange = new Date().getTime();
+			method = dao.insert(method);
+		}
+		return method;
 	}
 }
