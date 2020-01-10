74ae79c2002d47f8d4c9451cb72ccfe5fb7a3e57
msrocka <michael.srocka@gmail.com>
2020-01-10 18:08:26 +0100

    simple example if the streaming inventory builder
    -20_000_000.0
    2_290_000_000_000_000.0
    
    ```python
    >>> import numpy as np
    >>> C = np.load('impact_factors.npy')
    >>> C
    array([[0., 0., 0., ..., 0., 0., 0.],
           [0., 0., 0., ..., 0., 0., 0.],
           [0., 0., 0., ..., 0., 0., 0.],
           ...,
           [0., 0., 0., ..., 0., 0., 0.],
           [0., 0., 0., ..., 0., 0., 0.],
           [0., 0., 0., ..., 0., 0., 0.]])
    >>> np.count_nonzero(C)
    1329334
    >>> C.shape
    (446, 97012)
    >>> np.max(C)
    2290000000000000.0
    >>> np.min(C)
    -20000000.0
    >>>
    
    ```

---
+++ olca-core/src/test/java/examples/ImpactBuilderExample.java
@@ -0,0 +1,45 @@
+package examples;
+
+import java.io.File;
+
+import org.openlca.core.database.FlowDao;
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.database.ImpactCategoryDao;
+import org.openlca.core.database.derby.DerbyDatabase;
+import org.openlca.core.matrix.DIndex;
+import org.openlca.core.matrix.FlowIndex;
+import org.openlca.core.matrix.ImpactBuilder;
+import org.openlca.core.matrix.io.npy.Npy;
+import org.openlca.core.model.FlowType;
+import org.openlca.core.model.descriptors.ImpactCategoryDescriptor;
+import org.openlca.expressions.FormulaInterpreter;
+
+public class ImpactBuilderExample {
+
+	public static void main(String[] args) throws Exception {
+		String dbPath = "C:/Users/ms/openLCA-data-1.4/databases" +
+				"/openlca_lcia_v2_0_5_under_dev_201911220";
+		IDatabase db = new DerbyDatabase(new File(dbPath));
+
+		// build the LCIA category and flow indices
+		DIndex<ImpactCategoryDescriptor> impactIndex = new DIndex<>();
+		new ImpactCategoryDao(db).getDescriptors().forEach(
+				impactIndex::put);
+
+		// !Note that this is just an example. It sets all flows to output
+		// flows. Normally, the flow direction would be determined from
+		// the inventory
+		FlowIndex flowIndex = new FlowIndex();
+		new FlowDao(db).getDescriptors().forEach(d -> {
+			if (d.flowType == FlowType.ELEMENTARY_FLOW) {
+				flowIndex.putOutput(d);
+			}
+		});
+
+		ImpactBuilder.ImpactData data = new ImpactBuilder(db).build(
+				flowIndex, impactIndex, new FormulaInterpreter());
+		Npy.save(new File("impact_factors.npy"), data.impactMatrix);
+		db.close();
+	}
+
+}
