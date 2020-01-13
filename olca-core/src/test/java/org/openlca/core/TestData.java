137ef0af35bb6174bef82283961c71134fca75a3
msrocka <michael.srocka@gmail.com>
2020-01-13 18:00:38 +0100

    test uncertain LCIA parameters

--- olca-core/src/test/java/org/openlca/core/TestData.java
+++ olca-core/src/test/java/org/openlca/core/TestData.java
@@ -1,8 +1,12 @@
 package org.openlca.core;
 
+import java.util.UUID;
+
 import org.openlca.core.database.CurrencyDao;
 import org.openlca.core.database.FlowDao;
 import org.openlca.core.database.FlowPropertyDao;
+import org.openlca.core.database.ImpactCategoryDao;
+import org.openlca.core.database.ImpactMethodDao;
 import org.openlca.core.database.UnitDao;
 import org.openlca.core.database.UnitGroupDao;
 import org.openlca.core.model.Currency;
@@ -11,6 +15,12 @@ import org.openlca.core.model.FlowProperty;
 import org.openlca.core.model.FlowPropertyFactor;
 import org.openlca.core.model.FlowPropertyType;
 import org.openlca.core.model.FlowType;
+import org.openlca.core.model.ImpactCategory;
+import org.openlca.core.model.ImpactFactor;
+import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.Parameter;
+import org.openlca.core.model.ParameterScope;
+import org.openlca.core.model.Uncertainty;
 import org.openlca.core.model.Unit;
 import org.openlca.core.model.UnitGroup;
 import org.openlca.util.KeyGen;
@@ -95,6 +105,84 @@ public class TestData {
 		group.units.add(refUnit);
 		group.referenceUnit = refUnit;
 		return dao.insert(group);
+	}
+
+	public static TestImpact impact(String name) {
+		ImpactCategory impact = new ImpactCategory();
+		impact.name = name;
+		impact.refId = UUID.randomUUID().toString();
+		return new TestImpact(impact);
+	}
+
+	public static ImpactMethod method(String name, ImpactCategory... impacts) {
+		ImpactMethod m = new ImpactMethod();
+		m.refId = UUID.randomUUID().toString();
+		m.name = name;
+		for (ImpactCategory impact : impacts) {
+			m.impactCategories.add(impact);
+		}
+		ImpactMethodDao dao = new ImpactMethodDao(Tests.getDb());
+		return dao.insert(m);
+	}
+
+	public static class TestImpact {
+
+		private final ImpactCategory impact;
+
+		private TestImpact(ImpactCategory impact) {
+			this.impact = impact;
+		}
+
+		public TestImpact factor(String flow, double factor, String unit) {
+			ImpactFactor f = new ImpactFactor();
+			f.flow = TestData.flow(flow, unit, FlowType.ELEMENTARY_FLOW);
+			f.flowPropertyFactor = f.flow.getReferenceFactor();
+			f.unit = f.flow.getReferenceUnit();
+			f.value = factor;
+			impact.impactFactors.add(f);
+			return this;
+		}
+
+		public TestImpact factor(String flow, String formula, String unit) {
+			ImpactFactor f = new ImpactFactor();
+			f.flow = TestData.flow(flow, unit, FlowType.ELEMENTARY_FLOW);
+			f.flowPropertyFactor = f.flow.getReferenceFactor();
+			f.unit = f.flow.getReferenceUnit();
+			f.formula = formula;
+			impact.impactFactors.add(f);
+			return this;
+		}
+
+		public TestImpact parameter(String name, Uncertainty u) {
+			Parameter param = new Parameter();
+			param.name = name;
+			param.refId = UUID.randomUUID().toString();
+			param.isInputParameter = true;
+			param.uncertainty = u;
+			param.scope = ParameterScope.IMPACT_CATEGORY;
+			impact.parameters.add(param);
+			if (u.distributionType != null) {
+				switch (u.distributionType) {
+					case UNIFORM:
+						param.value = (u.parameter1 + u.parameter2) / 2;
+						break;
+					case TRIANGLE:
+						param.value = u.parameter3;
+						break;
+					default:
+						param.value = u.parameter1;
+				}
+			}
+			return this;
+		}
+
+		public ImpactCategory get() {
+			ImpactCategoryDao dao = new ImpactCategoryDao(Tests.getDb());
+			return impact.id == 0
+					? dao.insert(impact)
+					: dao.update(impact);
+		}
+
 	}
 
 }
--- olca-core/src/test/java/org/openlca/core/math/SimulatorTest.java
+++ olca-core/src/test/java/org/openlca/core/math/SimulatorTest.java
@@ -1,15 +1,55 @@
 package org.openlca.core.math;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.openlca.core.TestData;
 import org.openlca.core.TestProcess;
+import org.openlca.core.TestSystem;
+import org.openlca.core.Tests;
+import org.openlca.core.matrix.solvers.JavaSolver;
+import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.Process;
+import org.openlca.core.model.ProductSystem;
+import org.openlca.core.model.Uncertainty;
+import org.openlca.core.model.descriptors.Descriptors;
+import org.openlca.core.results.SimpleResult;
 
 public class SimulatorTest {
 
 	@Test
-	public void testLCIAParam() {
-		TestProcess.refProduct("p", 1.0, "kg")
-				.elemOut("ch4", 1.0, "kg")
+	public void testImpactParam() {
+
+		// create a simple model with an uncertain
+		// parameter in a LCIA category
+		Process p = TestProcess.refProduct("p", 1.0, "kg")
+				.elemOut("CH4", 1.0, "kg")
 				.get();
+		ProductSystem s = TestSystem.of(p).get();
+		ImpactMethod m = TestData.method("method",
+				TestData.impact("GWP")
+						.factor("CH4", "1 * param", "kg")
+						.parameter("param", Uncertainty.uniform(22, 26))
+						.get());
+
+		// create the simulator
+		CalculationSetup setup = new CalculationSetup(
+				CalculationType.MONTE_CARLO_SIMULATION, s);
+		setup.impactMethod = Descriptors.toDescriptor(m);
+		Simulator simulator = Simulator.create(
+				setup, Tests.getDb(), new JavaSolver());
+
+		// check the simulation results
+		for (int i = 0; i < 100; i++) {
+			SimpleResult r = simulator.nextRun();
+			double[] impacts = r.totalImpactResults;
+			Assert.assertEquals(1, impacts.length);
+			double val = impacts[0];
+			Assert.assertTrue(val >= 22 && val <= 26);
+		}
+
+		Arrays.asList(s, m, p).forEach(Tests::delete);
 	}
 
 }
