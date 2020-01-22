ff9f8543c92148a7f78ccd8e58e4f7bb951bd4f8
msrocka <michael.srocka@gmail.com>
2020-01-22 17:29:20 +0100

    add location code to LocationDescriptor

---
+++ olca-core/src/test/java/org/openlca/core/database/descriptors/LocationDescriptorTest.java
@@ -0,0 +1,29 @@
+package org.openlca.core.database.descriptors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openlca.core.Tests;
+import org.openlca.core.database.LocationDao;
+import org.openlca.core.model.Location;
+import org.openlca.core.model.descriptors.LocationDescriptor;
+
+public class LocationDescriptorTest {
+
+	@Test
+	public void test() {
+
+		Location loc = new Location();
+		loc.name = "LOC";
+		loc.refId = "LOC";
+		loc.code = "LOC";
+
+		LocationDao dao = new LocationDao(Tests.getDb());
+		loc = dao.insert(loc);
+		LocationDescriptor d = dao.descriptorMap().get(loc.id);
+		dao.delete(loc);
+
+		Assert.assertEquals(loc.name, d.name);
+		Assert.assertEquals(loc.refId, d.refId);
+		Assert.assertEquals(loc.code, d.code);
+	}
+}
