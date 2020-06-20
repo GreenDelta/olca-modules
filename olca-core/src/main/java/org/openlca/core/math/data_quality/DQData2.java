7af8da94e0d7b216a9c08254383c4a62e8ba12a3
msrocka <michael.srocka@gmail.com>
2020-06-20 10:32:58 +0200

    draft: load exchange DQ data into byte matrices

--- olca-core/src/main/java/org/openlca/core/math/data_quality/DQData2.java
+++ olca-core/src/main/java/org/openlca/core/math/data_quality/DQData2.java
@@ -1,5 +1,8 @@
 package org.openlca.core.math.data_quality;
 
+import java.util.List;
+
+import gnu.trove.map.hash.TLongObjectHashMap;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.NativeSql;
 import org.openlca.core.matrix.IndexFlow;
@@ -11,12 +14,24 @@ class DQData2 {
 	private final DQCalculationSetup setup;
 	private final BaseResult result;
 
+	/**
+	 * We store the process data in a byte matrix where the data quality
+	 * indicators are mapped to the rows and the process products to the
+	 * columns.
+	 */
 	private byte[][] processData;
+
+	/**
+	 * For the exchange data we store a flow*product matrix for each
+	 * indicator that holds the respective data quality scores of that
+	 * indicator.
+	 */
 	private BMatrix[] exchangeData;
 
 	static DQData2 of(IDatabase db, DQCalculationSetup setup, BaseResult result) {
 		var data = new DQData2(setup, result);
-		data.load(db);
+		data.loadProcessData(db);
+		data.loadExchangeData(db);
 		return data;
 	}
 
@@ -25,48 +40,111 @@ class DQData2 {
 		this.result = result;
 	}
 
-	private void load(IDatabase db) {
+	private void loadProcessData(IDatabase db) {
+		var system = setup.processSystem;
+		if (system == null)
+			return;
 
-		// load the process DQ data
-		if (setup.processSystem != null) {
-
-			var system = setup.processSystem;
-			var n = system.indicators.size();
-			processData = new byte[n][];
-			for (int i = 0; i < n; i++) {
-				processData[i] = new byte[result.techIndex.size()];
-			}
-
-			var techIndex = result.techIndex;
-			var sql = "select id, f_dq_system, dq_entry " +
-					"from tbl_processes";
-			NativeSql.on(db).query(sql, r -> {
-
-				// check that we have a valid entry
-				long systemID = r.getLong(2);
-				if (systemID != system.id)
-					return true;
-				var dqEntry = r.getString(3);
-				if (dqEntry == null)
-					return true;
-				var providers = techIndex.getProviders(r.getLong(1));
-				if (providers.isEmpty())
-					return true;
-
-				// store the values of the entry
-				int[] values = system.toValues(dqEntry);
-				int _n = Math.min(n, values.length);
-				for (int i = 0; i < _n; i++) {
-					byte[] data = processData[i];
-					byte value = (byte) values[i];
-					for (var provider : providers) {
-						int col = techIndex.getIndex(provider);
-						data[col] = value;
-					}
-				}
-				return true;
-			});
+		var n = system.indicators.size();
+		processData = new byte[n][];
+		for (int i = 0; i < n; i++) {
+			processData[i] = new byte[result.techIndex.size()];
 		}
+
+		// query the process table
+		var techIndex = result.techIndex;
+		var sql = "select id, f_dq_system, dq_entry " +
+				"from tbl_processes";
+		NativeSql.on(db).query(sql, r -> {
+
+			// check that we have a valid entry
+			long systemID = r.getLong(2);
+			if (systemID != system.id)
+				return true;
+			var dqEntry = r.getString(3);
+			if (dqEntry == null)
+				return true;
+			var providers = techIndex.getProviders(r.getLong(1));
+			if (providers.isEmpty())
+				return true;
+
+			// store the values of the entry
+			int[] values = system.toValues(dqEntry);
+			int _n = Math.min(n, values.length);
+			for (int i = 0; i < _n; i++) {
+				byte[] data = processData[i];
+				byte value = (byte) values[i];
+				for (var provider : providers) {
+					int col = techIndex.getIndex(provider);
+					data[col] = value;
+				}
+			}
+			return true;
+		});
+	}
+
+	private void loadExchangeData(IDatabase db) {
+		var system = setup.exchangeSystem;
+		if (system == null || result.flowIndex == null)
+			return;
+
+		// allocate a BMatrix for each indicator
+		var n = system.indicators.size();
+		exchangeData = new BMatrix[n];
+		var techIndex = result.techIndex;
+		var flowIndex = result.flowIndex;
+		for (int i = 0; i < n; i++) {
+			exchangeData[i] = new BMatrix(
+					flowIndex.size(), techIndex.size());
+		}
+
+		// collect the processes (providers) of the result with a
+		// matching data quality system
+		var providers = new TLongObjectHashMap<List<ProcessProduct>>();
+		var sql = "select id, f_exchange_dq_system from tbl_processes";
+		NativeSql.on(db).query(sql, r -> {
+			long sysID = r.getLong(2);
+			if (sysID != system.id)
+				return true;
+			var processID = r.getLong(1);
+			var products = result.techIndex.getProviders(processID);
+			if (products.isEmpty())
+				return true;
+			providers.put(processID, products);
+			return true;
+		});
+
+		// now, scan the exchanges table and collect all
+		// matching data quality entries
+		sql = "select f_owner, f_flow, f_location, dq_entry from tbl_exchanges";
+		NativeSql.on(db).query(sql, r -> {
+
+			// check that we have a valid entry
+			var products = providers.get(r.getLong(1));
+			if (products == null)
+				return true;
+			long flowID = r.getLong(2);
+			long locationID = r.getLong(3);
+			var dqEntry = r.getString(4);
+			if (dqEntry == null)
+				return true;
+			int row = flowIndex.of(flowID, locationID);
+			if (row < 0)
+				return true;
+
+			// store the values
+			int[] values = system.toValues(dqEntry);
+			int _n = Math.min(n, values.length);
+			for (int i = 0; i < _n; i++) {
+				var data = exchangeData[i];
+				byte value = (byte) values[i];
+				for (var product : products) {
+					int col = techIndex.getIndex(product);
+					data.set(row, col, value);
+				}
+			}
+			return true;
+		});
 	}
 
 
@@ -105,11 +183,11 @@ class DQData2 {
 
 	private static class BMatrix {
 
-		private final int columns;
+		private final int rows;
 		private final byte[] data;
 
 		BMatrix(int rows, int columns) {
-			this.columns = columns;
+			this.rows = rows;
 			this.data = new byte[rows * columns];
 		}
 
@@ -122,7 +200,7 @@ class DQData2 {
 		}
 
 		int index(int row, int column) {
-			return column + columns * row;
+			return row + rows * column;
 		}
 	}
 }
