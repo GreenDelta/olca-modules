957f3ed300d5cc967940c78e4d2fff6cbc6cdfee
msrocka <michael.srocka@gmail.com>
2020-06-21 10:36:22 +0200

    some accumulator tests

---
+++ olca-core/src/test/java/org/openlca/core/math/data_quality/AccumulatorTest.java
@@ -0,0 +1,82 @@
+package org.openlca.core.math.data_quality;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccumulatorTest {
+
+	private final int[] dqs = {1, 0, 4};
+	private final double[] weights = {0.5, 0.1, 0.4};
+
+	@Test
+	public void testNoCeilingExcludeNA() {
+		var setup = new DQCalculationSetup();
+		setup.ceiling = false;
+		setup.naHandling = NAHandling.EXCLUDE;
+		setup.aggregationType = AggregationType.WEIGHTED_AVERAGE;
+		check(setup, 2);
+	}
+
+	@Test
+	public void testCeilingExcludeNA() {
+		var setup = new DQCalculationSetup();
+		setup.ceiling = true;
+		setup.naHandling = NAHandling.EXCLUDE;
+		setup.aggregationType = AggregationType.WEIGHTED_AVERAGE;
+		check(setup, 3);
+	}
+
+	@Test
+	public void testNoCeilingNaToMax() {
+		var setup = new DQCalculationSetup();
+		setup.ceiling = false;
+		setup.naHandling = NAHandling.USE_MAX;
+		setup.aggregationType = AggregationType.WEIGHTED_AVERAGE;
+		check(setup, 3);
+	}
+
+	@Test
+	public void testAggTypeMax() {
+		var setup = new DQCalculationSetup();
+		setup.ceiling = false;
+		setup.naHandling = NAHandling.EXCLUDE;
+		setup.aggregationType = AggregationType.MAXIMUM;
+		check(setup, 4);
+
+		// now with na-handling -> max
+		setup.naHandling = NAHandling.USE_MAX;
+		check(setup, 5);
+	}
+
+	@Test
+	public void testAggTypeSquaredWeights() {
+		var setup = new DQCalculationSetup();
+		setup.ceiling = false;
+		setup.naHandling = NAHandling.EXCLUDE;
+		setup.aggregationType = AggregationType.WEIGHTED_SQUARED_AVERAGE;
+		check(setup, 2);
+
+		// now with na-handling -> max
+		setup.naHandling = NAHandling.USE_MAX;
+		check(setup, 2);
+	}
+
+	private void check(DQCalculationSetup setup, int result) {
+		var acc = new Accumulator(setup, 5);
+		assertEquals(result, acc.get(dqs, () -> weights));
+
+		// add some random stuff and reset
+		for (int i = 0; i < 100; i++) {
+			acc.add(i / 10, Math.random() * 100);
+		}
+		acc.reset();
+
+		// do the test
+		for (int i = 0; i < dqs.length; i++) {
+			acc.add(dqs[i], weights[i]);
+		}
+
+		assertEquals(result, acc.get());
+	}
+}
