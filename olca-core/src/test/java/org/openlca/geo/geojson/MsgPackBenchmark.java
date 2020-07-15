b8d55af9a9fc69a7ceb96124925e917e91924b27
msrocka <michael.srocka@gmail.com>
2020-07-15 22:32:31 +0200

    so this is interesting:
    using MessagePack just for de-/serialization is a bit faster (pack/unpack):
    
    ```
    MsgPack; warm up ...  took 3206 ms
    MsgPack; benchmark ...  took 2863 ms
    ProtoPack; warm up ...  took 4514 ms
    ProtoPack; benchmark ...  took 4398 ms
    ```
    
    but when further gz the output/input we get (packgz, unpackgz):
    
    ```
    MsgPack; warm up ...  took 48399 ms
    MsgPack; benchmark ...  took 49882 ms
    ProtoPack; warm up ...  took 28915 ms
    ProtoPack; benchmark ...  took 28351 ms
    ```
    
    this is because ProtoPack generates smaller outputs ... so we should be faster
    in the end when switching to ProtoPack ...nice.

--- olca-core/src/test/java/org/openlca/geo/geojson/MsgPackBenchmark.java
+++ olca-core/src/test/java/org/openlca/geo/geojson/MsgPackBenchmark.java
@@ -20,25 +20,33 @@ public class MsgPackBenchmark {
 		FeatureCollection coll = FeatureCollection.of(multiPoint);
 
 		System.out.print("MsgPack; warm up ...");
-		int t = (int) msgPack(coll, ITERATIONS);
+		int t = (int) msgPack(coll);
 		System.out.println("  took " + t + " ms");
 
 		System.out.print("MsgPack; benchmark ...");
-		t = (int) msgPack(coll, ITERATIONS);
+		t = (int) msgPack(coll);
 		System.out.println("  took " + t + " ms");
 
 		System.out.print("JSON; warm up ...");
-		t = (int) json(coll, ITERATIONS);
+		t = (int) json(coll);
 		System.out.println("  took " + t + " ms");
 
 		System.out.print("JSON; benchmark ...");
-		t = (int) json(coll, ITERATIONS);
+		t = (int) json(coll);
+		System.out.println("  took " + t + " ms");
+
+		System.out.print("ProtoPack; warm up ...");
+		t = (int) protoPack(coll);
+		System.out.println("  took " + t + " ms");
+
+		System.out.print("ProtoPack; benchmark ...");
+		t = (int) protoPack(coll);
 		System.out.println("  took " + t + " ms");
 	}
 
-	private static double json(FeatureCollection coll, int iterations) {
+	private static double json(FeatureCollection coll) {
 		long start = System.nanoTime();
-		for (int i = 0; i < iterations; i++) {
+		for (int i = 0; i < ITERATIONS; i++) {
 			StringWriter writer = new StringWriter();
 			GeoJSON.write(coll, writer);
 			writer.flush();
@@ -52,11 +60,24 @@ public class MsgPackBenchmark {
 		return time / 1e6;
 	}
 
-	private static double msgPack(FeatureCollection coll, int iterations) {
+	private static double msgPack(FeatureCollection coll) {
 		long start = System.nanoTime();
-		for (int i = 0; i < iterations; i++) {
+		for (int i = 0; i < ITERATIONS; i++) {
 			byte[] data = MsgPack.pack(coll);
 			FeatureCollection r = MsgPack.unpack(data);
+			if (((MultiPoint) r.features.get(0).geometry).points.size() != SIZE) {
+				throw new RuntimeException("invalid result");
+			}
+		}
+		long time = System.nanoTime() - start;
+		return time / 1e6;
+	}
+
+	private static double protoPack(FeatureCollection coll) {
+		long start = System.nanoTime();
+		for (int i = 0; i < ITERATIONS; i++) {
+			byte[] data = ProtoPack.pack(coll);
+			var r = ProtoPack.unpack(data);
 			if (((MultiPoint) r.features.get(0).geometry).points.size() != SIZE) {
 				throw new RuntimeException("invalid result");
 			}
