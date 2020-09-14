ccacd0bb0352b09321c2b5570e7022979b3f375b
msrocka <michael.srocka@gmail.com>
2020-09-14 17:12:20 +0200

    this test crashes with our Julia solver, but not with
    our Java solver
    
    ```java
    var libDir = "C:/Users/ms/Projects/openLCA/repos/olca-rust/bin";
    Julia.loadFromDir(new File(libDir));
    var solver = new JuliaSolver();
    var provider = LazySolutionProvider.create(data, solver);
    ```

---
+++ olca-core/src/test/java/org/openlca/core/results/solutions/LazySolutionProviderTest.java
@@ -0,0 +1,45 @@
+package org.openlca.core.results.solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openlca.core.matrix.MatrixData;
+import org.openlca.core.matrix.ProcessProduct;
+import org.openlca.core.matrix.TechIndex;
+import org.openlca.core.matrix.format.HashPointMatrix;
+import org.openlca.core.matrix.solvers.JavaSolver;
+import org.openlca.core.model.descriptors.FlowDescriptor;
+import org.openlca.core.model.descriptors.ProcessDescriptor;
+
+public class LazySolutionProviderTest {
+
+	@Test
+	public void testScalingVector() {
+		var data = new MatrixData();
+		data.techMatrix = new HashPointMatrix(new double[][]{
+				{1.0, 0.0, 0.0},
+				{-1.0, 1.0, 0.0},
+				{0.0, -1.0, 1.0},
+		});
+		data.techIndex = new TechIndex(product(1));
+		data.techIndex.put(product(2));
+		data.techIndex.put(product(3));
+		data.techIndex.setDemand(1.0);
+
+		var provider = LazySolutionProvider.create(
+				data, new JavaSolver());
+		var scaling = provider.scalingVector();
+		Assert.assertArrayEquals(
+				new double[]{1.0, 1.0, 1.0}, scaling, 1e-10);
+	}
+
+	private ProcessProduct product(int i) {
+		var process = new ProcessDescriptor();
+		process.id = i;
+		process.name = "process " + i;
+		var flow = new FlowDescriptor();
+		flow.id = i;
+		flow.name = "product " + i;
+		return ProcessProduct.of(process, flow);
+	}
+
+}
