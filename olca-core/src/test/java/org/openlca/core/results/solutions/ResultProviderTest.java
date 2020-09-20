d05b5eb8eaed3a7c9be36bc4449e171f6c0ba8c1
msrocka <michael.srocka@gmail.com>
2020-09-20 19:24:30 +0200

    more tests

--- olca-core/src/test/java/org/openlca/core/results/solutions/ResultProviderTest.java
+++ olca-core/src/test/java/org/openlca/core/results/solutions/ResultProviderTest.java
@@ -61,18 +61,18 @@ public class ResultProviderTest {
 		data.techIndex = new TechIndex(product.apply(1));
 		data.techIndex.setDemand(1.0);
 		data.techIndex.put(product.apply(2));
-		data.techMatrix = JavaMatrix.of(new double[][]{
-				{0.5, -0.5},
-				{-0.5, 1.0},
+		data.techMatrix = JavaMatrix.of(new double[][] {
+				{ 0.5, -0.5 },
+				{ -0.5, 1.0 },
 		});
 
 		// env. flows
 		data.flowIndex = FlowIndex.create();
 		data.flowIndex.putOutput(flow.apply(1));
 		data.flowIndex.putInput(flow.apply(2));
-		data.flowMatrix = JavaMatrix.of(new double[][]{
-				{1.0, 2.0},
-				{-3.0, -3.0},
+		data.flowMatrix = JavaMatrix.of(new double[][] {
+				{ 1.0, 2.0 },
+				{ -3.0, -3.0 },
 		});
 
 		return data;
@@ -111,6 +111,150 @@ public class ResultProviderTest {
 	public void testTotalRequirementsOf() {
 		assertEquals(2.0, provider.totalRequirementsOf(0), 1e-10);
 		assertEquals(2.0, provider.totalRequirementsOf(1), 1e-10);
+	}
+
+	@Test
+	public void testTechColumnOf() {
+		assertArrayEquals(d(0.5, -0.5), provider.techColumnOf(0), 1e-10);
+		assertArrayEquals(d(-0.5, 1.0), provider.techColumnOf(1), 1e-10);
+	}
+
+	@Test
+	public void testTechValueOf() {
+		double[][] expected = {
+				{ 0.5, -0.5 },
+				{ -0.5, 1.0 },
+		};
+		for (int row = 0; row < expected.length; row++) {
+			for (int col = 0; col < expected[row].length; col++) {
+				assertEquals(
+						expected[row][col],
+						provider.techValueOf(row, col),
+						1e-10);
+			}
+		}
+	}
+
+	@Test
+	public void testScaledTechValueOf() {
+		double[][] expected = {
+				{ 2.0, -1.0 },
+				{ -2.0, 2.0 },
+		};
+		for (int row = 0; row < expected.length; row++) {
+			for (int col = 0; col < expected[row].length; col++) {
+				assertEquals(
+						expected[row][col],
+						provider.scaledTechValueOf(row, col),
+						1e-20);
+			}
+		}
+	}
+
+	@Test
+	public void testSolutionOfOne() {
+		assertArrayEquals(d(4.0, 2.0), provider.solutionOfOne(0), 1e-10);
+		assertArrayEquals(d(2.0, 2.0), provider.solutionOfOne(1), 1e-10);
+	}
+
+	@Test
+	public void testLoopFactorOf() {
+		assertEquals(0.5, provider.loopFactorOf(0), 1e-10);
+		assertEquals(0.5, provider.loopFactorOf(1), 1e-10);
+	}
+
+	@Test
+	public void testTotalFactorOf() {
+		assertEquals(1.0, provider.totalFactorOf(0), 1e-10);
+		assertEquals(1.0, provider.totalFactorOf(1), 1e-10);
+	}
+
+	@Test
+	public void testUnscaledFlowsOf() {
+		assertArrayEquals(d(1.0, -3.0), provider.unscaledFlowsOf(0), 1e-10);
+		assertArrayEquals(d(2.0, -3.0), provider.unscaledFlowsOf(1), 1e-10);
+	}
+
+	@Test
+	public void testUnscaledFlowOf() {
+		double[][] expected = {
+				{ 1.0, 2.0 },
+				{ -3 - 0, -3.0 }
+		};
+		for (int flow = 0; flow < expected.length; flow++) {
+			for (int product = 0; product < expected[flow].length; product++) {
+				assertEquals(
+						expected[flow][product],
+						provider.unscaledFlowOf(flow, product),
+						1e-10);
+			}
+		}
+	}
+
+	@Test
+	public void testDirectFlowsOf() {
+		assertArrayEquals(d(4.0, -12.0), provider.directFlowsOf(0), 1e-10);
+		assertArrayEquals(d(4.0, -6.0), provider.directFlowsOf(1), 1e-10);
+	}
+
+	@Test
+	public void testDirectFlowOf() {
+		double[][] expected = {
+				{ 4.0, 4.0 },
+				{ -12.0, -6.0 }
+		};
+		for (int flow = 0; flow < expected.length; flow++) {
+			for (int product = 0; product < expected[flow].length; product++) {
+				assertEquals(
+						expected[flow][product],
+						provider.directFlowOf(flow, product),
+						1e-10);
+			}
+		}
+	}
+
+	@Test
+	public void testTotalFlowsOfOne() {
+		assertArrayEquals(d(8, -18), provider.totalFlowsOfOne(0), 1e-10);
+		assertArrayEquals(d(6, -12), provider.totalFlowsOfOne(1), 1e-10);
+	}
+
+	@Test
+	public void testTotalFlowOfOne() {
+		double[][] expected = {
+				{ 8, 6 },
+				{ -18, -12 }
+		};
+		for (int flow = 0; flow < expected.length; flow++) {
+			for (int product = 0; product < expected[flow].length; product++) {
+				assertEquals(
+						expected[flow][product],
+						provider.totalFlowOfOne(flow, product),
+						1e-10);
+			}
+		}
+	}
+
+	@Test
+	public void totalFlowsOf() {
+		assertArrayEquals(d(8, -18), provider.totalFlowsOf(0), 1e-10);
+		assertArrayEquals(d(6, -12), provider.totalFlowsOf(1), 1e-10);
+	}
+
+	@Test
+	public void totalFlowOf() {
+		double[][] expected = {
+				{ 8, 6 },
+				{ -18, -12 }
+		};
+		for (int flow = 0; flow < expected.length; flow++) {
+			for (int product = 0; product < expected[flow].length; product++) {
+				assertEquals(
+						expected[flow][product],
+						provider.totalFlowOf(flow, product),
+						1e-10);
+			}
+		}
 	}
 
 	@Test
