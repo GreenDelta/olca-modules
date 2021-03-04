56d6ed407732d7f5c745c4592de8aa23fd95b323
msrocka <michael.srocka@gmail.com>
2021-03-04 17:58:11 +0100

    avoid touching the store
    example is to import the method package into an in-memory database:
    
    ```java
      public static void main(String[] args) {
        var zipPath = "C:/Users/Win10/Downloads/openLCA_lcia_method_package_2_1 (1).zip";
        try (var db = DerbyDatabase.createInMemory();
            var store = ZipStore.open(new File(zipPath))) {
          var imp = new JsonImport(store, db);
    
          var start = System.currentTimeMillis();
          imp.run();
          var time = System.currentTimeMillis() - start;
          System.out.printf("took: %.3f secs%n", time / 1000.0);
    
        } catch (Exception e) {
          e.printStackTrace();
        }
      }
    ```
    
    ```
    before: 254.972 secs
    after:  182.845 secs
    ```
    
    So it is now ~28% faster

--- olca-core/src/main/java/org/openlca/jsonld/input/BaseImport.java
+++ olca-core/src/main/java/org/openlca/jsonld/input/BaseImport.java
@@ -3,10 +3,8 @@ package org.openlca.jsonld.input;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 import org.openlca.core.database.FileStore;
-import org.openlca.core.database.IDatabase;
 import org.openlca.core.model.ModelType;
 import org.openlca.core.model.RootEntity;
 import org.slf4j.Logger;
@@ -31,10 +29,22 @@ abstract class BaseImport<T extends RootEntity> {
 		if (refId == null || conf == null)
 			return null;
 		try {
+
 			T model = get(refId);
-			JsonObject json = conf.store.get(modelType, refId);
+
+			// first check if we can directly return the model
+			// without touching the store
+			if (model != null) {
+				if (conf.updateMode == UpdateMode.NEVER
+						|| conf.hasVisited(modelType, refId))
+					return model;
+			}
+			var json = conf.store.get(modelType, refId);
+			if (json == null)
+				return model;
 			if (!doImport(model, json))
 				return model;
+
 			importBinFiles();
 			conf.visited(modelType, refId);
 			model = map(json, model);
@@ -47,13 +57,8 @@ abstract class BaseImport<T extends RootEntity> {
 	}
 
 	private boolean doImport(T model, JsonObject json) {
-		if (model == null)
-			return true;
-		if (json == null
-				|| conf.updateMode == UpdateMode.NEVER
-				|| conf.hasVisited(modelType, refId))
-			return false;
-		if (conf.updateMode == UpdateMode.ALWAYS)
+		if (model == null
+				|| conf.updateMode == UpdateMode.ALWAYS)
 			return true;
 
 		// check version and date
@@ -62,7 +67,7 @@ abstract class BaseImport<T extends RootEntity> {
 		if (jsonVersion < model.version)
 			return false;
 		return jsonVersion != model.version
-				|| jsonDate > model.lastChange;
+					 || jsonDate > model.lastChange;
 	}
 
 	/**
@@ -76,25 +81,26 @@ abstract class BaseImport<T extends RootEntity> {
 	}
 
 	private void importBinFiles() {
-		IDatabase db = conf.db.getDatabase();
+		var db = conf.db.getDatabase();
 		if (db == null || db.getFileStorageLocation() == null)
 			return;
-		FileStore fs = new FileStore(db.getFileStorageLocation());
+		var fs = new FileStore(db.getFileStorageLocation());
 		try {
-			File dir = fs.getFolder(modelType, refId);
-			for (String path : conf.store.getBinFiles(modelType, refId)) {
+			var dir = fs.getFolder(modelType, refId);
+			for (var path : conf.store.getBinFiles(modelType, refId)) {
 				byte[] data = conf.store.get(path);
 				if (data == null)
 					return;
-				String fileName = Paths.get(path).getFileName().toString();
-				if (!dir.exists())
-					dir.mkdirs();
-				File file = new File(dir, fileName);
-				Files.write(file.toPath(), data, StandardOpenOption.CREATE);
+				var name = Paths.get(path).getFileName().toString();
+				if (!dir.exists()) {
+					Files.createDirectories(dir.toPath());
+				}
+				File file = new File(dir, name);
+				Files.write(file.toPath(), data);
 			}
 		} catch (Exception e) {
-			log.error("failed to import bin files for " + modelType + ":"
-					+ refId, e);
+			log.error("failed to import bin files for "
+								+ modelType + ":" + refId, e);
 		}
 	}
 
