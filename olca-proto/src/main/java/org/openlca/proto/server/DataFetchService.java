db2c52423d1fd871af1a27783fbb936b3deceea8
msrocka <michael.srocka@gmail.com>
2021-04-13 15:21:02 +0200

    now we can also get the content of root- or
    non-categories

--- olca-proto/src/main/java/org/openlca/proto/server/DataFetchService.java
+++ olca-proto/src/main/java/org/openlca/proto/server/DataFetchService.java
@@ -1,17 +1,25 @@
 package org.openlca.proto.server;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
+import org.openlca.core.database.CategoryDao;
+import org.openlca.core.database.Daos;
 import org.openlca.core.database.IDatabase;
+import org.openlca.core.model.CategorizedEntity;
+import org.openlca.core.model.Category;
 import org.openlca.core.model.RootEntity;
 import org.openlca.proto.generated.Proto;
+import org.openlca.proto.generated.Proto.Ref;
 import org.openlca.proto.generated.data.DataFetchServiceGrpc;
 import org.openlca.proto.generated.data.DataSet;
 import org.openlca.proto.generated.data.FindRequest;
 import org.openlca.proto.generated.data.GetAllRequest;
 import org.openlca.proto.generated.data.GetAllResponse;
+import org.openlca.proto.generated.data.GetCategoryContentRequest;
 import org.openlca.proto.generated.data.GetRequest;
 import org.openlca.proto.input.In;
+import org.openlca.proto.output.Out;
 import org.openlca.util.Strings;
 
 import io.grpc.stub.StreamObserver;
@@ -83,14 +91,14 @@ class DataFetchService extends
       return;
 
     var pageSize = req.getPageSize() > 0
-      ? req.getPageSize()
-      : 100;
+        ? req.getPageSize()
+        : 100;
     var page = req.getPage() > 0
-      ? req.getPage()
-      : 1;
+        ? req.getPage()
+        : 1;
     var response = GetAllResponse.newBuilder()
-      .setPageSize(pageSize)
-      .setPage(page);
+        .setPageSize(pageSize)
+        .setPage(page);
 
     var descriptors = db.allDescriptorsOf(type);
     var totalCount = descriptors.size();
@@ -104,11 +112,53 @@ class DataFetchService extends
 
     var end = Math.min(totalCount, offset + pageSize);
     descriptors.subList(offset, end)
-      .stream()
-      .map(d -> db.get(type, d.id))
-      .map(e -> DataUtil.toDataSet(db, e))
-      .forEach(response::addDataSet);
+        .stream()
+        .map(d -> db.get(type, d.id))
+        .map(e -> DataUtil.toDataSet(db, e))
+        .forEach(response::addDataSet);
     resp.onNext(response.build());
+    resp.onCompleted();
+  }
+
+  @Override
+  public void getCategoryContent(
+      GetCategoryContentRequest req, StreamObserver<Ref> resp) {
+
+    // check that the request is for a categorized entity type
+    var modelType = In.modelTypeOf(req.getModelType());
+    var modelClass = modelType != null
+        ? modelType.getClass()
+        : null;
+    if (modelType == null
+        || modelClass == null
+        || !CategorizedEntity.class.isAssignableFrom(modelClass)) {
+      Response.invalidArg(resp, "Not a categorized type: " + modelType);
+      return;
+    }
+
+    // find the category
+    Optional<Category> category;
+    var catID = req.getCategory();
+    if (Strings.nullOrEmpty(catID) || catID.equals("/")) {
+      category = Optional.empty();
+    } else {
+      var dao = new CategoryDao(db);
+      var cat = dao.getForRefId(catID);
+      if (cat == null) {
+        cat = dao.getForPath(modelType, catID);
+      }
+      if (cat == null) {
+        Response.notFound(resp,
+            "Category with ID ot path =" + catID + " does not exists");
+        return;
+      }
+      category = Optional.of(cat);
+    }
+
+    var dao = Daos.categorized(db, modelType);
+    for (var d : dao.getDescriptors(category)) {
+      resp.onNext(Out.refOf(d).build());
+    }
     resp.onCompleted();
   }
 
