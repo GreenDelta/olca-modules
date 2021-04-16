40c52fbb0a67dad5b8c2044743ade2841deda2de
msrocka <michael.srocka@gmail.com>
2021-04-16 18:48:31 +0200

    the crud test fails for projects...

--- olca-proto/src/test/java/org/openlca/proto/server/DataUpdateTest.java
+++ olca-proto/src/test/java/org/openlca/proto/server/DataUpdateTest.java
@@ -5,17 +5,24 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openlca.core.database.IDatabase;
 import org.openlca.core.model.ModelType;
+import org.openlca.core.model.Version;
 import org.openlca.proto.Messages;
+import org.openlca.proto.Tests;
 import org.openlca.proto.generated.data.DataFetchServiceGrpc;
+import org.openlca.proto.generated.data.DataSet;
 import org.openlca.proto.generated.data.DataUpdateServiceGrpc;
+import org.openlca.proto.generated.data.DeleteRequest;
 import org.openlca.proto.generated.data.FindRequest;
 import org.openlca.proto.output.Out;
 
 public class DataUpdateTest {
 
+  private final IDatabase db = Tests.db();
+
   @Test
-  public void testInsert() {
+  public void testCrud() {
     ServiceTests.on(chan -> {
 
       var fetch = DataFetchServiceGrpc.newBlockingStub(chan);
@@ -31,15 +38,55 @@ public class DataUpdateTest {
           i++;
           var id = UUID.randomUUID().toString();
 
-          // create an instance
+          // check that it does not yet exist
+          var dataSet = fetch.find(findRequestOf(type, id));
+          assertTrue(Messages.isEmpty(dataSet));
+
+          // create an instance and insert it
           var instance = type.getModelClass()
             .getConstructor()
             .newInstance();
           instance.refId = id;
+          instance.version = Version.valueOf(0,0,1);
+          var ref = update.put(DataUtil.toDataSet(db, instance).build());
+          if (type != ModelType.CATEGORY) {
+            assertEquals(id, ref.getId());
+          }
+          assertEquals("00.00.001", ref.getVersion());
 
-          // check that it does not yet exist
-          var dataSet = fetch.find(findRequestOf(type, id));
+          // check that we can get it
+          dataSet = fetch.find(findRequestOf(type, id));
+          if (type != ModelType.CATEGORY) {
+            assertEquals(id, getField(type, dataSet, "id"));
+          }
+          assertEquals("00.00.001", getField(type, dataSet, "version"));
+
+          // update it
+          instance.name = type.name();
+          instance.version = Version.valueOf(1,0,0);
+          ref = update.put(DataUtil.toDataSet(db, instance).build());
+          if (type != ModelType.CATEGORY) {
+            assertEquals(id, ref.getId());
+          }
+          assertEquals(type.name(), ref.getName());
+          assertEquals("01.00.000", ref.getVersion());
+
+          // check that we get the updated thing
+          dataSet = fetch.find(findRequestOf(type, id));
+          if (type != ModelType.CATEGORY) {
+            assertEquals(id, getField(type, dataSet, "id"));
+          }
+          assertEquals("01.00.000", getField(type, dataSet, "version"));
+          assertEquals(type.name(), getField(type, dataSet, "name"));
+
+          // delete it and check that we do not get it anymore
+          update.delete(DeleteRequest.newBuilder()
+            .setModelType(Out.modelTypeOf(type))
+            .setId(id)
+            .build());
+          dataSet = fetch.find(findRequestOf(type, id));
           assertTrue(Messages.isEmpty(dataSet));
+
         }
       } catch (Exception e) {
         throw new RuntimeException(e);
@@ -57,4 +104,32 @@ public class DataUpdateTest {
       .build();
   }
 
+  private String getField(ModelType type, DataSet ds, String field) {
+    var proto = switch (type) {
+      case ACTOR -> ds.getActor();
+      case CATEGORY -> ds.getCategory();
+      case CURRENCY -> ds.getCurrency();
+      case DQ_SYSTEM -> ds.getDqSystem();
+      case FLOW -> ds.getFlow();
+      case FLOW_PROPERTY -> ds.getFlowProperty();
+      case IMPACT_CATEGORY -> ds.getImpactCategory();
+      case IMPACT_METHOD -> ds.getImpactMethod();
+      case LOCATION -> ds.getLocation();
+      case PARAMETER -> ds.getParameter();
+      case PROCESS -> ds.getProcess();
+      case PRODUCT_SYSTEM -> ds.getProductSystem();
+      case PROJECT -> ds.getProject();
+      case SOCIAL_INDICATOR -> ds.getSocialIndicator();
+      case SOURCE -> ds.getSource();
+      case UNIT_GROUP -> ds.getUnitGroup();
+      default -> null;
+    };
+    assertNotNull(proto);
+    var fieldDef = proto.getDescriptorForType()
+      .findFieldByName(field);
+    var value = proto.getField(fieldDef);
+    return value instanceof String
+      ? value.toString()
+      : null;
+  }
 }
