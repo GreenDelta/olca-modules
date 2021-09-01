58cd4d903544c615bd248f81c4f6223e943a5f67
msrocka <michael.srocka@gmail.com>
2021-09-01 13:22:09 +0200

    add an exit-hook for the Repl

--- olca-formula/src/main/java/org/openlca/expressions/Repl.java
+++ olca-formula/src/main/java/org/openlca/expressions/Repl.java
@@ -10,10 +10,11 @@ import java.io.PrintStream;
  */
 public class Repl {
 
-	private BufferedReader in;
-	private PrintStream out;
-	private PrintStream err;
-	FormulaInterpreter evaluator = new FormulaInterpreter();
+	private final BufferedReader in;
+	private final PrintStream out;
+	private final PrintStream err;
+	private final FormulaInterpreter evaluator = new FormulaInterpreter();
+	private Runnable exitHook;
 
 	public Repl(InputStream in, PrintStream out, PrintStream err) {
 		this.in = new BufferedReader(new InputStreamReader(in));
@@ -21,73 +22,91 @@ public class Repl {
 		this.err = err;
 	}
 
+	/**
+	 * Registers an exit-hook for this Repl. The given runnable is called when
+	 * the user quits this Repl instance.
+	 *
+	 * @param fn the exit-hook
+	 */
+	public Repl onExit(Runnable fn) {
+		this.exitHook = fn;
+		return this;
+	}
+
 	public void start() {
 		printWelcome();
-		String line = null;
+		String line;
 		try {
 			while ((line = in.readLine()) != null) {
 				String expression = line.trim();
 				if (expression.equalsIgnoreCase("exit")
-						|| expression.equalsIgnoreCase("quit"))
+					|| expression.equalsIgnoreCase("quit"))
 					break;
 				eval(expression);
-				out.print("olca<< ");
+				out.print(">>> ");
 			}
 		} catch (Exception e) {
 			err.println("An unexpected error occurred: " + e.getMessage());
 		}
 		out.flush();
 		err.flush();
+		if (exitHook != null) {
+			exitHook.run();
+		}
 	}
 
 	private void eval(String expression) {
-		if (expression.isEmpty())
-			out.println("olca>> ");
-		else if (expression.equalsIgnoreCase("help"))
+		if (expression.isEmpty()) {
+			out.println();
+		} else if (expression.equalsIgnoreCase("help")) {
 			printHelp();
-		else if (expression.startsWith("var "))
+		} else if (expression.startsWith("var ")) {
 			evalVariable(expression);
-		else
+		} else {
 			evalFormula(expression);
+		}
 	}
 
 	private void printWelcome() {
 		out.println("openLCA Formula Interpreter");
-		out.println("type 'help' to display the help message");
-		out.println("");
-		out.print("olca<< ");
+		out.println("Type \"help\" for more information.");
+		out.println();
+		out.print(">>> ");
 	}
 
 	private void printHelp() {
-		out.println(" evaluate an expression: \t type in the expression and press enter, e.g. sin(42)");
-		out.println(" define a variable: \t\t type var <variable name> = <expression>, e.g. var a = sin(42)");
-		out.println(" exit the interpreter: \t\t type 'exit' or 'quit' and press enter");
+		out.println(" evaluate an expression: " +
+			"\t type in the expression and press enter, e.g. sin(42)");
+		out.println(" define a variable: " +
+			"\t\t type var <variable name> = <expression>, e.g. var a = sin(42)");
+		out.println(" exit the interpreter: " +
+			"\t\t type 'exit' or 'quit' and press enter");
 	}
 
 	private void evalVariable(String expression) {
 		StringBuffer variableNameBuf = new StringBuffer();
 		StringBuffer formulaBuf = new StringBuffer();
 		parseVariableExpression(expression.substring(4), variableNameBuf,
-				formulaBuf);
+			formulaBuf);
 		String variableName = variableNameBuf.toString().trim();
 		String formula = formulaBuf.toString().trim();
 		if (variableName.isEmpty() || formula.isEmpty()) {
-			err.println("error>> Wrong variable declaration (syntax is "
-					+ "'var <identifier> = <expression>'). ");
+			err.println("Error: wrong variable declaration (syntax is "
+				+ "'var <identifier> = <expression>'). ");
 		} else {
 			evalVariable(variableName, formula);
 		}
 	}
 
 	private void parseVariableExpression(String expression,
-			StringBuffer variableNameBuf, StringBuffer formulaBuf) {
+		StringBuffer variableNameBuf, StringBuffer formulaBuf) {
 		boolean after = false;
 		for (char c : expression.toCharArray()) {
 			if (c != '=' && !after) {
 				variableNameBuf.append(c);
 			} else if (c == '=' && !after) {
 				after = true;
-			} else if (after) {
+			} else {
 				formulaBuf.append(c);
 			}
 		}
@@ -97,24 +116,25 @@ public class Repl {
 		try {
 			evaluator.getGlobalScope().bind(variableName, formula);
 			double val = evaluator.eval(variableName);
-			out.println("olca>> " + variableName + " = " + val);
+			out.println(variableName + " = " + val);
 		} catch (Exception e) {
-			err.println("error>> " + e.getMessage());
+			err.println("Error: " + e.getMessage());
 		}
 	}
 
 	private void evalFormula(String expression) {
 		try {
 			Object result = evaluator.eval(expression);
-			out.println("olca>> " + result);
+			out.println(result);
 		} catch (Exception e) {
-			err.println("error>> " + e.getMessage());
+			err.println("Error: " + e.getMessage());
 		}
 	}
 
 	public static void main(String[] args) {
-		Repl repl = new Repl(System.in, System.out, System.err);
-		repl.start();
+		new Repl(System.in, System.out, System.err)
+			.onExit(() -> System.out.println("Bye!"))
+			.start();
 	}
 
 }
