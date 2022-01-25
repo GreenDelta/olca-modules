2b1c32cbb3e4e85bd604feeef9057e665058f092
msrocka <michael.srocka@gmail.com>
2022-01-25 10:16:35 +0100

    get the matrix solver from the SolverContext

--- olca-core/src/main/java/org/openlca/core/results/providers/ResultProviders.java
+++ olca-core/src/main/java/org/openlca/core/results/providers/ResultProviders.java
@@ -1,17 +1,11 @@
 package org.openlca.core.results.providers;
 
-import org.openlca.core.matrix.solvers.JavaSolver;
-import org.openlca.core.matrix.solvers.MatrixSolver;
-import org.openlca.julia.Julia;
-import org.openlca.julia.JuliaSolver;
-
 public final class ResultProviders {
 
 	private ResultProviders() {
 	}
 
 	public static ResultProvider eagerOf(SolverContext context) {
-		var solver = getSolver();
 		if (context.hasLibraryLinks())
 			return LazyLibraryProvider.of(context);
 
@@ -20,25 +14,20 @@ public final class ResultProviders {
 			&& data.techMatrix.rows() < 3000;
 		if (isSmall)
 			return EagerResultProvider.create(data);
+		var solver = context.solver();
 		return data.isSparse() && solver.hasSparseSupport()
 			? LazyResultProvider.create(data)
 			: EagerResultProvider.create(data);
 	}
 
 	public static ResultProvider lazyOf(SolverContext context) {
-		var solver = getSolver();
 		if (context.hasLibraryLinks())
 			return LazyLibraryProvider.of(context);
 
+		var solver = context.solver();
 		var data = context.matrixData();
 		return data.isSparse() && solver.hasSparseSupport()
 			? LazyResultProvider.create(data)
 			: EagerResultProvider.create(data);
-	}
-
-	public static MatrixSolver getSolver() {
-		return Julia.isLoaded()
-			? new JuliaSolver()
-			: new JavaSolver();
 	}
 }
