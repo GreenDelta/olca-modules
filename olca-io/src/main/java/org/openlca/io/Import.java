e0ff5bbfd50f970d6d093994b930bfdfbfbc383d
msrocka <michael.srocka@gmail.com>
2022-02-10 20:15:28 +0100

    simple Import interface

--- olca-io/src/main/java/org/openlca/io/Import.java
+++ olca-io/src/main/java/org/openlca/io/Import.java
@@ -1,16 +1,13 @@
 package org.openlca.io;
 
+import org.openlca.core.io.Cancelable;
 import org.openlca.core.io.ImportLog;
 
-public interface FileImport extends Runnable {
-
-	/**
-	 * Cancels the import.
-	 */
-	void cancel();
+public interface Import extends Cancelable {
 
 	/**
 	 * Returns the import log of this import.
 	 */
 	ImportLog log();
+
 }
