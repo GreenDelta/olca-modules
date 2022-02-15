5551fcd0d8248919eb6d5e29132fbe5b9feaface
msrocka <michael.srocka@gmail.com>
2022-02-15 10:57:28 +0100

    write new project fields

--- olca-core/src/main/java/org/openlca/jsonld/output/ProjectWriter.java
+++ olca-core/src/main/java/org/openlca/jsonld/output/ProjectWriter.java
@@ -1,6 +1,5 @@
 package org.openlca.jsonld.output;
 
-import org.openlca.core.model.FlowProperty;
 import org.openlca.core.model.Project;
 import org.openlca.core.model.ProjectVariant;
 
@@ -18,12 +17,15 @@ class ProjectWriter extends Writer<Project> {
 		JsonObject obj = super.write(p);
 		if (obj == null)
 			return null;
+		Out.put(obj, "isWithCosts", p.isWithCosts);
+		Out.put(obj, "isWithRegionalization", p.isWithRegionalization);
 		if (p.impactMethod != null) {
 			Out.put(obj, "impactMethod", p.impactMethod, conf);
 		}
 		if (p.nwSet != null) {
 			Out.put(obj, "nwSet", p.nwSet, conf);
 		}
+
 		mapVariants(obj, p);
 		GlobalParameters.sync(p, conf);
 		return obj;
@@ -34,19 +36,19 @@ class ProjectWriter extends Writer<Project> {
 		for (ProjectVariant v : p.variants) {
 			JsonObject obj = new JsonObject();
 			array.add(obj);
-			Out.put(obj, "@type", ProjectVariant.class.getSimpleName());
 			Out.put(obj, "name", v.name);
 			Out.put(obj, "productSystem", v.productSystem, conf, Out.REQUIRED_FIELD);
 			Out.put(obj, "amount", v.amount);
 			Out.put(obj, "unit", v.unit, conf, Out.REQUIRED_FIELD);
 			Out.put(obj, "allocationMethod", v.allocationMethod);
-			FlowProperty prop = null;
-			if (v.flowPropertyFactor != null) {
-				prop = v.flowPropertyFactor.flowProperty;
-			}
+			Out.put(obj, "description", v.description);
+			Out.put(obj, "isDisabled", v.isDisabled);
+			var prop = v.flowPropertyFactor != null
+				? v.flowPropertyFactor.flowProperty
+				: null;
 			Out.put(obj, "flowProperty", prop, conf, Out.REQUIRED_FIELD);
 			if (!v.parameterRedefs.isEmpty()) {
-				JsonArray redefs = ParameterRedefs.map(v.parameterRedefs, conf);
+				var redefs = ParameterRedefs.map(v.parameterRedefs, conf);
 				Out.put(obj, "parameterRedefs", redefs);
 			}
 		}
