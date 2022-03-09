e5def66802b6ecf3412ec07c0f4962b543f818d3
msrocka <michael.srocka@gmail.com>
2022-03-09 16:11:21 +0100

    usage search: fix last compiler errors

--- olca-core/src/main/java/org/openlca/core/database/usage/EmptyUseSearch.java
+++ olca-core/src/main/java/org/openlca/core/database/usage/EmptyUseSearch.java
@@ -1,14 +1,14 @@
 package org.openlca.core.database.usage;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 
 import gnu.trove.set.TLongSet;
 import org.openlca.core.model.descriptors.RootDescriptor;
 
 record EmptyUseSearch() implements IUseSearch {
 	@Override
-	public List<RootDescriptor> find(TLongSet ids) {
-		return Collections.emptyList();
+	public Set<? extends RootDescriptor> find(TLongSet ids) {
+		return Collections.emptySet();
 	}
 }
