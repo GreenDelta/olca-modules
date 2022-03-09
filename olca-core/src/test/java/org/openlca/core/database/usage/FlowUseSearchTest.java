78c1465fa5783b06d46fb7c10142fd64b0eafd21
msrocka <michael.srocka@gmail.com>
2022-03-09 16:52:58 +0100

    fix usage tests II

--- olca-core/src/test/java/org/openlca/core/database/usage/FlowUseSearchTest.java
+++ olca-core/src/test/java/org/openlca/core/database/usage/FlowUseSearchTest.java
@@ -1,16 +1,11 @@
 package org.openlca.core.database.usage;
 
-import java.util.List;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openlca.core.Tests;
-import org.openlca.core.database.FlowDao;
 import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.ImpactCategoryDao;
-import org.openlca.core.database.ProcessDao;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.Flow;
 import org.openlca.core.model.ImpactCategory;
@@ -18,70 +13,62 @@ import org.openlca.core.model.ImpactFactor;
 import org.openlca.core.model.ModelType;
 import org.openlca.core.model.Process;
 import org.openlca.core.model.descriptors.Descriptor;
-import org.openlca.core.model.descriptors.RootDescriptor;
-import org.openlca.core.model.descriptors.FlowDescriptor;
 
 public class FlowUseSearchTest {
 
-	private IDatabase database = Tests.getDb();
-	private IUseSearch<FlowDescriptor> search;
+	private final IDatabase db = Tests.getDb();
+	private final IUseSearch search = IUseSearch.of(ModelType.FLOW, db);
 	private Flow flow;
 
 	@Before
 	public void setup() {
 		flow = new Flow();
 		flow.name = "flow";
-		flow = new FlowDao(database).insert(flow);
-		search = IUseSearch.FACTORY.createFor(ModelType.FLOW, database);
+		flow = db.insert(flow);
 	}
 
 	@After
 	public void tearDown() {
-		new FlowDao(database).delete(flow);
+		db.delete(flow);
 	}
 
 	@Test
 	public void testFindNoUsage() {
-		List<RootDescriptor> models = search.findUses(Descriptor
-				.of(flow));
+		var models = search.find(flow.id);
 		Assert.assertNotNull(models);
 		Assert.assertTrue(models.isEmpty());
 	}
 
 	@Test
 	public void testFindInImpactCategories() {
-		ImpactFactor iFactor = new ImpactFactor();
-		iFactor.flow = flow;
-		ImpactCategory ic = new ImpactCategory();
-		ic.impactFactors.add(iFactor);
-		ImpactCategoryDao dao = new ImpactCategoryDao(database);
-		dao.insert(ic);
-		List<RootDescriptor> results = search.findUses(
-				Descriptor.of(flow));
-		dao.delete(ic);
+		var factor = new ImpactFactor();
+		factor.flow = flow;
+		var impact = new ImpactCategory();
+		impact.impactFactors.add(factor);
+		db.insert(impact);
+		var results = search.find(flow.id);
+		db.delete(impact);
 		Assert.assertEquals(1, results.size());
-		Assert.assertEquals(
-				Descriptor.of(ic), results.get(0));
+		Assert.assertEquals(Descriptor.of(impact), results.iterator().next());
 	}
 
 
 	@Test
 	public void testFindInProcesses() {
-		Process process = createProcess();
-		List<RootDescriptor> results = search.findUses(Descriptor
-				.of(flow));
-		new ProcessDao(database).delete(process);
-		Descriptor expected = Descriptor.of(process);
+		var process = createProcess();
+		var results = search.find(flow.id);
+		db.delete(process);
+		var expected = Descriptor.of(process);
 		Assert.assertEquals(1, results.size());
-		Assert.assertEquals(expected, results.get(0));
+		Assert.assertEquals(expected, results.iterator().next());
 	}
 
 	private Process createProcess() {
-		Process process = new Process();
+		var process = new Process();
 		process.name = "process";
-		Exchange exchange = new Exchange();
+		var exchange = new Exchange();
 		exchange.flow = flow;
 		process.exchanges.add(exchange);
-		return new ProcessDao(database).insert(process);
+		return db.insert(process);
 	}
 }
