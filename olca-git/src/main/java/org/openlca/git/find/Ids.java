494a48bf09ffb27cae5d6de4d2c9f63969567636
Sebastian Greve <greve@greendelta.com>
2022-05-11 12:18:43 +0200

    Optimized finding objectids

--- olca-git/src/main/java/org/openlca/git/find/Ids.java
+++ olca-git/src/main/java/org/openlca/git/find/Ids.java
@@ -3,7 +3,6 @@ package org.openlca.git.find;
 import java.io.IOException;
 
 import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.openlca.git.util.GitUtil;
@@ -45,25 +44,11 @@ public class Ids {
 	ObjectId get(ObjectId treeId, String path) {
 		if (Strings.nullOrEmpty(path))
 			return treeId;
-		var name = path.contains("/") ? path.substring(0, path.indexOf("/")) : path;
-		var rest = path.contains("/") ? path.substring(path.indexOf("/") + 1) : null;
-		try (var walk = new TreeWalk(repo)) {
-			walk.addTree(treeId);
-			walk.setRecursive(false);
-			while (walk.next()) {
-				if (rest != null && walk.getFileMode() != FileMode.TREE)
-					continue;
-				if (!name.equals(walk.getNameString()))
-					continue;
-				var subTreeId = walk.getObjectId(0);
-				if (rest == null)
-					return subTreeId;
-				return get(subTreeId, rest);
-			}
-			return ObjectId.zeroId();
+		try {
+			return TreeWalk.forPath(repo, path, treeId).getObjectId(0);
 		} catch (IOException e) {
-			log.error("Error finding sub tree for " + path);
-			return ObjectId.zeroId();
+			log.error("Error finding id for " + path);
+			return null;
 		}
 	}
 
