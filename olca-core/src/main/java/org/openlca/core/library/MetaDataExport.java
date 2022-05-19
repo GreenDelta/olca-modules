8fb6cade6db60f3e1b40a06307e0325442e77903
msrocka <michael.srocka@gmail.com>
2022-05-19 14:14:03 +0200

    write all supported meta-data in library export

--- olca-core/src/main/java/org/openlca/core/library/MetaDataExport.java
+++ olca-core/src/main/java/org/openlca/core/library/MetaDataExport.java
@@ -1,14 +1,26 @@
 package org.openlca.core.library;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.List;
 
 import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.ImpactCategoryDao;
-import org.openlca.core.database.ImpactMethodDao;
-import org.openlca.core.database.ProcessDao;
+import org.openlca.core.model.Actor;
+import org.openlca.core.model.Currency;
+import org.openlca.core.model.DQSystem;
+import org.openlca.core.model.Flow;
+import org.openlca.core.model.FlowProperty;
+import org.openlca.core.model.ImpactCategory;
+import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.Location;
+import org.openlca.core.model.Process;
+import org.openlca.core.model.SocialIndicator;
+import org.openlca.core.model.Source;
+import org.openlca.core.model.UnitGroup;
 import org.openlca.jsonld.ZipStore;
 import org.openlca.jsonld.output.JsonExport;
 import org.openlca.util.Exchanges;
+import org.openlca.util.Strings;
 
 /**
  * Writes the meta-data (JSON-LD) package in a library export.
@@ -26,81 +38,79 @@ class MetaDataExport implements Runnable {
 	@Override
 	public void run() {
 		try (var zip = ZipStore.open(new File(export.folder, "meta.zip"))) {
-			var exp = new JsonExport(db, zip);
-				writeProcesses(exp);
-				writeImpactCategories(exp);
-				writeImpactMethods(exp);
+			var exp = new JsonExport(db, zip)
+				.withDefaultProviders(false)
+				.skipLibraryData(true);
+			var types = List.of(
+				Actor.class,
+				Source.class,
+				Location.class,
+				UnitGroup.class,
+				FlowProperty.class,
+				Flow.class,
+				Currency.class,
+				SocialIndicator.class,
+				DQSystem.class,
+				Process.class,
+				ImpactCategory.class,
+				ImpactMethod.class);
+
+			var dependencies = new HashSet<String>();
+			for (var type : types) {
+				var descriptors = db.getDescriptors(type);
+				for (var d : descriptors) {
+					// filter out library data
+					if (Strings.notEmpty(d.library)) {
+						dependencies.add(d.library);
+						continue;
+					}
+					var full = db.get(type, d.id);
+					if (full instanceof Process process) {
+						writeProcess(process, exp);
+					}
+					if (full instanceof ImpactCategory impact) {
+						writeImpact(impact, exp);
+					} else {
+						exp.write(full);
+					}
+				}
+			}
+
+			export.info.dependencies().addAll(dependencies);
 		} catch (Exception e) {
 			throw new RuntimeException(
 				"failed to write meta data in library export", e);
 		}
 	}
 
-	private void writeProcesses(JsonExport exp) {
-		var dao = new ProcessDao(db);
-		for (var d : dao.getDescriptors()) {
-			var process = dao.getForId(d.id);
-			if (process == null)
+	private void writeProcess(Process process, JsonExport exp) {
+		if (process == null)
+			return;
+		var libProc = process.copy();
+		libProc.refId = process.refId;
+		libProc.id = process.id;
+		libProc.exchanges.removeIf(
+			e -> !Exchanges.isProviderFlow(e));
+		libProc.allocationFactors.clear();
+		libProc.parameters.clear();
+		for (var e : libProc.exchanges) {
+			e.amount = 1.0;
+			e.formula = null;
+			if (e.flow == null)
 				continue;
-
-			// make sure to write the used flows and locations
-			for (var e : process.exchanges) {
-				if (e.flow != null) {
-					exp.write(e.flow);
-				}
-				if (e.location != null) {
-					exp.write(e.location);
-				}
-			}
-
-			// write the library stub of the process
-			var libProc = process.copy();
-			libProc.refId = process.refId;
-			libProc.id = process.id;
-			libProc.exchanges.removeIf(
-				e -> !Exchanges.isProviderFlow(e));
-			libProc.allocationFactors.clear();
-			libProc.parameters.clear();
-			for (var e : libProc.exchanges) {
-				e.amount = 1.0;
-				e.formula = null;
-				if (e.flow == null)
-					continue;
-				e.unit = e.flow.getReferenceUnit();
-				e.flowPropertyFactor = e.flow.getReferenceFactor();
-			}
-			exp.write(libProc);
+			e.unit = e.flow.getReferenceUnit();
+			e.flowPropertyFactor = e.flow.getReferenceFactor();
 		}
+		exp.write(libProc);
 	}
 
-	private void writeImpactCategories(JsonExport exp) {
-		var dao = new ImpactCategoryDao(db);
-		for (var d : dao.getDescriptors()) {
-			var impact = dao.getForId(d.id);
-
-			// make sure to write the used flows and locations
-			for (var f : impact.impactFactors) {
-				if (f.flow != null) {
-					exp.write(f.flow);
-				}
-				if (f.location != null) {
-					exp.write(f.location);
-				}
-			}
-
+	private void writeImpact(ImpactCategory impact, JsonExport exp) {
 			var libImpact = impact.copy();
 			libImpact.id = impact.id;
 			libImpact.refId = impact.refId;
 			libImpact.impactFactors.clear();
+			libImpact.parameters.clear();
 			exp.write(libImpact);
-		}
 	}
 
-	private void writeImpactMethods(JsonExport exp) {
-		var dao = new ImpactMethodDao(db);
-		for (var d : dao.getDescriptors()) {
-			var method = dao.getForId(d.id);
-			exp.write(method);
-		}
-	}
 }
