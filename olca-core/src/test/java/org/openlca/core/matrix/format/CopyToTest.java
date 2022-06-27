de67ea7ab61def0c7b457938e27c60d56e82e30e
msrocka <michael.srocka@gmail.com>
2022-06-27 10:51:29 +0200

    test more matrix types in the copy-to test
    because we want to implement optimizations for specific matrix types.

--- olca-core/src/test/java/org/openlca/core/matrix/format/CopyToTest.java
+++ olca-core/src/test/java/org/openlca/core/matrix/format/CopyToTest.java
@@ -1,34 +1,59 @@
 package org.openlca.core.matrix.format;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class CopyToTest {
+@RunWith(Parameterized.class)
+public record CopyToTest(MatrixReader source, Matrix target) {
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> setup() {
+		var data = new double[][]{
+			{1, 1, 1, 1},
+			{1, 1, 1, 1},
+			{1, 1, 1, 1}
+		};
+
+		var sources = List.of(
+			DenseMatrix.of(data),
+			HashPointMatrix.of(data),
+			CSCMatrix.of(data),
+			JavaMatrix.of(data));
+		int n = 10;
+		var targets = List.of(
+			new DenseMatrix(n, n),
+			new HashPointMatrix(n, n),
+			new JavaMatrix(n, n));
+		var pairs = new ArrayList<Object[]>();
+		for (var source : sources) {
+			for (var target : targets) {
+				pairs.add(new Object[]{source, target});
+			}
+		}
+		return pairs;
+	}
 
 	@Test
 	public void testCopyOffset() {
-
-		var source = HashPointMatrix.of(new double[][] {
-			{1,1,1,1},
-			{1,1,1,1},
-			{1,1,1,1}
-		});
-
 		for (var rowOffset = 0; rowOffset <= 10; rowOffset++) {
 			for (var colOffset = 0; colOffset <= 10; colOffset++) {
-				var target = new DenseMatrix(10, 10);
+				var target = this.target.copy();
 				source.copyTo(target, rowOffset, colOffset);
-				// System.out.println(target);
-
 				for (int i = 0; i < 10; i++) {
 					for (int j = 0; j < 10; j++) {
 						if (i >= rowOffset
-							&& i < (rowOffset + source.rows)
+							&& i < (rowOffset + source.rows())
 							&& j >= colOffset
-							&& j < (colOffset + source.cols)) {
+							&& j < (colOffset + source.columns())) {
 							assertEquals(1.0, target.get(i, j), 1e-16);
-						}else {
+						} else {
 							assertEquals(0.0, target.get(i, j), 1e-16);
 						}
 					}
