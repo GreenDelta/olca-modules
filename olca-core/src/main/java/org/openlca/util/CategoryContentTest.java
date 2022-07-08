d776cae2c54ae2bcf07f2159c417c96a3f8e291a
msrocka <michael.srocka@gmail.com>
2022-07-08 11:26:52 +0200

    making the CategoryContentTest simpler and faster

--- olca-core/src/main/java/org/openlca/util/CategoryContentTest.java
+++ olca-core/src/main/java/org/openlca/util/CategoryContentTest.java
@@ -1,13 +1,12 @@
 package org.openlca.util;
 
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Supplier;
 
-import gnu.trove.map.hash.TLongByteHashMap;
-import gnu.trove.map.hash.TLongObjectHashMap;
+import gnu.trove.set.hash.TLongHashSet;
 import jakarta.persistence.Table;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.NativeSql;
@@ -22,20 +21,20 @@ public class CategoryContentTest {
 
 	private final IDatabase db;
 
-	private final byte TRUE = 1;
-	private final byte FALSE = 2;
-	private final TLongByteHashMap anyLibContent = new TLongByteHashMap();
-	private final TLongByteHashMap nonLibContent = new TLongByteHashMap();
-	private final TLongObjectHashMap<Map<String, Byte>> libContent = new TLongObjectHashMap<>();
+	private final EnumMap<ModelType, Cache> caches = new EnumMap<>(ModelType.class);
 
 	public CategoryContentTest(IDatabase db) {
 		this.db = Objects.requireNonNull(db);
 	}
 
 	public void clearCache() {
-		anyLibContent.clear();
-		nonLibContent.clear();
-		libContent.clear();
+		caches.clear();
+	}
+
+	private Cache cacheOf(Category category) {
+		return category == null || category.modelType == null
+			? Cache.empty()
+			: caches.computeIfAbsent(category.modelType, type -> Cache.load(db, type));
 	}
 
 	/**
@@ -45,37 +44,11 @@ public class CategoryContentTest {
 	public boolean hasLibraryContent(Category category, String library) {
 		if (category == null || library == null)
 			return false;
-		var cache = libContent.get(category.id);
-		if (cache != null) {
-			var b = cache.get(library);
-			if (b != null) {
-				return b == TRUE;
-			}
-		}
-		if (cache == null) {
-			cache = new HashMap<>();
-			libContent.put(category.id, cache);
-		}
-
-		var test = ContentTest.forLibrary(db, category.modelType, library);
-		if (test == null) {
-			cache.put(library, FALSE);
+		var cache = cacheOf(category);
+		var ids = cache.libs.get(library);
+		if (ids == null)
 			return false;
-		}
-		if (test.test(category)) {
-			cache.put(library, TRUE);
-			return true;
-		}
-
-		for (var child : category.childCategories) {
-			if (hasLibraryContent(child, library)) {
-				cache.put(library, TRUE);
-				return true;
-			}
-		}
-
-		cache.put(library, FALSE);
-		return false;
+		return test(category, ids);
 	}
 
 	/**
@@ -83,10 +56,10 @@ public class CategoryContentTest {
 	 * contain model elements from a library.
 	 */
 	public boolean hasLibraryContent(Category category) {
-		return testRecursively(
-			category,
-			anyLibContent,
-			() -> ContentTest.forAnyLibrary(db, category.modelType));
+		if (category == null)
+			return false;
+		var cache = cacheOf(category);
+		return test(category, cache.anyLib);
 	}
 
 	/**
@@ -94,88 +67,62 @@ public class CategoryContentTest {
 	 * contain model elements that do not belong to a library.
 	 */
 	public boolean hasNonLibraryContent(Category category) {
-		return testRecursively(
-			category,
-			nonLibContent,
-			() -> ContentTest.forNonLibrary(db, category.modelType));
+		if (category == null)
+			return false;
+		var cache = cacheOf(category);
+		return test(category, cache.nonLib);
 	}
 
-	private boolean testRecursively(
-		Category category, TLongByteHashMap cache, Supplier<ContentTest> testFn) {
-		if (category == null || category.modelType == null)
-			return false;
-		byte b = cache.get(category.id);
-		if (b == TRUE)
+	private boolean test(Category category, TLongHashSet set) {
+		if (set.contains(category.id))
 			return true;
-		if (b == FALSE)
-			return false;
-
-		var test = testFn.get();
-		if (test == null) {
-			cache.put(category.id, FALSE);
-			return false;
-		}
-		if (test.test(category)) {
-			cache.put(category.id, TRUE);
-			return true;
-		}
-
 		for (var child : category.childCategories) {
-			if (testRecursively(child, cache, testFn)) {
-				cache.put(category.id, TRUE);
+			if (test(child, set))
 				return true;
-			}
 		}
-		cache.put(category.id, FALSE);
 		return false;
 	}
 
+	private record Cache(
+		TLongHashSet nonLib,
+		TLongHashSet anyLib,
+		Map<String, TLongHashSet> libs) {
 
-	private record ContentTest(NativeSql sql, String prefix, String suffix) {
-
-		public static ContentTest forAnyLibrary(IDatabase db, ModelType type) {
-			return of(db, type, " and library is not null");
+		static Cache empty() {
+			return new Cache(
+				new TLongHashSet(0),
+				new TLongHashSet(0),
+				Collections.emptyMap());
 		}
 
-		public static ContentTest forLibrary(
-			IDatabase db, ModelType type, String library) {
-			return of(db, type, " and library='" + library + "'");
-		}
-
-		public static ContentTest forNonLibrary(IDatabase db, ModelType type) {
-			return of(db, type, " and library is null");
-		}
-
-		private static ContentTest of(IDatabase db, ModelType type, String suffix) {
-			var prefix = prefixOf(type);
-			if (prefix == null || db == null)
-				return null;
-			return new ContentTest(NativeSql.on(db), prefix, suffix);
-		}
-
-		private static String prefixOf(ModelType type) {
-			if (type == null || !type.isRoot())
-				return null;
-			var modelClass = type.getModelClass();
-			if (modelClass == null)
-				return null;
-			var table = modelClass.getAnnotation(Table.class);
+		static Cache load(IDatabase db, ModelType type) {
+			if (db == null || type == null || !type.isRoot())
+				return empty();
+			var clazz = type.getModelClass();
+			if (clazz == null)
+				return empty();
+			var table = clazz.getAnnotation(Table.class);
 			if (table == null)
-				return null;
-			return "select count(id) from " + table.name() + " where f_category = ";
-		}
+				return empty();
 
-		private boolean test(Category category) {
-			if (category == null)
-				return false;
-			var b = new AtomicBoolean(false);
-			sql.query(prefix + category.id + suffix, r -> {
-				var count = r.getInt(1);
-				b.set(count > 0);
-				return false;
+			var nonLib = new TLongHashSet();
+			var anyLib = new TLongHashSet();
+			var libs = new HashMap<String, TLongHashSet>();
+			var sql = "select distinct f_category, library from " + table.name();
+			NativeSql.on(db).query(sql, r -> {
+				long category = r.getLong(1);
+				String lib = r.getString(2);
+				if (Strings.nullOrEmpty(lib)) {
+					nonLib.add(category);
+				} else {
+					anyLib.add(category);
+					libs.computeIfAbsent(lib, l -> new TLongHashSet())
+						.add(category);
+				}
+				return true;
 			});
-			return b.get();
+
+			return new Cache(nonLib, anyLib, libs);
 		}
 	}
-
 }
