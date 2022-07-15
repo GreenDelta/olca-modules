c4dcfe9524cc5d82fd10b33b87093c27fba9f054
msrocka <michael.srocka@gmail.com>
2022-07-15 17:20:06 +0200

    we will remove the formula fields from uncertainty distributions
    so no need to fix these errors now

--- olca-core/src/test/java/org/openlca/jsonld/output/ParameterReferencesTest.java
+++ olca-core/src/test/java/org/openlca/jsonld/output/ParameterReferencesTest.java
@@ -37,69 +37,26 @@ public class ParameterReferencesTest extends AbstractZipTest {
 
 	@Test
 	public void testParameter() {
-		Parameter p1 = createParameter("p1", "2*2", null, null);
-		Parameter p2 = createParameter("p2", "2*p1", null, null);
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(p2);
+		var p1 = db.insert(Parameter.global("p1", "2*2"));
+		var p2 = db.insert(Parameter.global("p2", "2*p1"));
+		with(store -> {
+			new JsonExport(db, store).write(p2);
 			assertNotNull(store.get(ModelType.PARAMETER, p1.refId));
 			assertNotNull(store.get(ModelType.PARAMETER, p2.refId));
-		});
-	}
-
-	@Test
-	public void testParameterUncertainty() {
-		Uncertainty u2 = createUncertainty("1", "2", "3*p4");
-		Uncertainty u4 = createUncertainty("p1", "2*p1", "3*p2");
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p2 = createParameter("p2", "2*2", null, u2);
-		Parameter p3 = createParameter("p3", null, null, null);
-		Parameter p4 = createParameter("p4", "2*2", null, u4);
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(p4);
-			assertNotNull(store.get(ModelType.PARAMETER, p1.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p2.refId));
-			assertNull(store.get(ModelType.PARAMETER, p3.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p4.refId));
 		});
 	}
 
 	@Test
 	public void testExchange() {
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p1Intern = createParameter("p1", null,
-				ParameterScope.PROCESS, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		Exchange e1 = createExchange("2*p1", null);
-		Exchange e2 = createExchange("2*p2", null);
-		Process process = createProcess(new Exchange[] { e1, e2 },
-				new Parameter[] { p1Intern });
+		var p1 = db.insert(Parameter.global("p1", 1));
+		var p1Intern = Parameter.process("p1", 1);
+		var p2 = db.insert(Parameter.global("p2", 1));
+		var e1 = exchangeOf("2*p1");
+		var e2 = exchangeOf("2*p2");
+		var process = createProcess(new Exchange[]{e1, e2},
+			new Parameter[]{p1Intern});
 		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(process);
-			assertNotNull(store.get(ModelType.PROCESS, process.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1Intern.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p2.refId));
-		});
-	}
-
-	@Test
-	public void testExchangeUncertainty() {
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p1Intern = createParameter("p1", null,
-				ParameterScope.PROCESS, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		Uncertainty u1 = createUncertainty("1", "2", "3*p2");
-		Uncertainty u2 = createUncertainty("p1", "2*p1", "3*p2");
-		Exchange e1 = createExchange("2", u1);
-		Exchange e2 = createExchange("2", u2);
-		Process process = createProcess(new Exchange[] { e1, e2 },
-				new Parameter[] { p1Intern });
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(process);
+			new JsonExport(db, store).write(process);
 			assertNotNull(store.get(ModelType.PROCESS, process.refId));
 			assertNull(store.get(ModelType.PARAMETER, p1.refId));
 			assertNull(store.get(ModelType.PARAMETER, p1Intern.refId));
@@ -109,17 +66,14 @@ public class ParameterReferencesTest extends AbstractZipTest {
 
 	@Test
 	public void testProcessParameter() {
-		Parameter p1Global = createParameter("p1", "2*2", null, null);
-		Parameter p1 = createParameter("p1", "2*2", ParameterScope.PROCESS,
-				null);
-		Parameter p2 = createParameter("p2", "2*p1*p3", ParameterScope.PROCESS,
-				null);
-		Parameter p3Global = createParameter("p3", "2*p4", null, null);
-		Parameter p4Global = createParameter("p4", "2", null, null);
-		Process process = createProcess(null, new Parameter[] { p1, p2 });
+		var p1Global = db.insert(Parameter.global("p1", "2*2"));
+		var p1 = Parameter.process("p1", "2*2");
+		var p2 = Parameter.process("p2", "2*p1*p3");
+		var p3Global = db.insert(Parameter.global("p3", "2*p4"));
+		var p4Global = db.insert(Parameter.global("p4", "2"));
+		var process = createProcess(null, new Parameter[]{p1, p2});
 		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(process);
+			new JsonExport(db, store).write(process);
 			assertNotNull(store.get(ModelType.PROCESS, process.refId));
 			assertNull(store.get(ModelType.PARAMETER, p1.refId));
 			assertNull(store.get(ModelType.PARAMETER, p1Global.refId));
@@ -130,62 +84,14 @@ public class ParameterReferencesTest extends AbstractZipTest {
 	}
 
 	@Test
-	public void testProcessParameterUncertainty() {
-		Uncertainty u1 = createUncertainty("1", "p2", "p3");
-		Uncertainty u2 = createUncertainty("p1", "2", "3");
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		Parameter p3 = createParameter("p3", null, null, null);
-		Parameter p1Intern = createParameter("p1", "2*2",
-				ParameterScope.PROCESS, u1);
-		Parameter p2Intern = createParameter("p2", "2*2",
-				ParameterScope.PROCESS, u2);
-		Process process = createProcess(null, new Parameter[] { p1Intern,
-				p2Intern });
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(process);
-			assertNotNull(store.get(ModelType.PROCESS, process.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1Intern.refId));
-			assertNull(store.get(ModelType.PARAMETER, p2.refId));
-			assertNull(store.get(ModelType.PARAMETER, p2Intern.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p3.refId));
-		});
-	}
-
-	@Test
 	public void testImpactFactor() {
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p1Intern = createParameter("p1", null,
-				ParameterScope.PROCESS, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		ImpactFactor f1 = createImpactFactor("2*p1", null);
-		ImpactFactor f2 = createImpactFactor("2*p2", null);
-		ImpactCategory impact = createImpactCategory(
-				new ImpactFactor[] { f1, f2 }, new Parameter[] { p1Intern });
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(impact);
-			assertNotNull(store.get(ModelType.IMPACT_CATEGORY, impact.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1Intern.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p2.refId));
-		});
-	}
-
-	@Test
-	public void testImpactFactorUncertainty() {
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p1Intern = createParameter("p1", null,
-				ParameterScope.PROCESS, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		Uncertainty u1 = createUncertainty("1", "2", "3*p2");
-		Uncertainty u2 = createUncertainty("p1", "2*p1", "3*p2");
-		ImpactFactor f1 = createImpactFactor("2", u1);
-		ImpactFactor f2 = createImpactFactor("2", u2);
-		ImpactCategory impact = createImpactCategory(
-				new ImpactFactor[] { f1, f2 }, new Parameter[] { p1Intern });
+		var p1 = db.insert(Parameter.global("p1", 1));
+		var p1Intern = Parameter.process("p1",1);
+		var p2 = db.insert(Parameter.global("p2", 1));
+		var f1 = impactFactorOf("2*p1");
+		var f2 = impactFactorOf("2*p2");
+		var impact = impactCategoryOf(
+			new ImpactFactor[]{f1, f2}, new Parameter[]{p1Intern});
 		with((store) -> {
 			JsonExport export = new JsonExport(db, store);
 			export.write(impact);
@@ -203,8 +109,8 @@ public class ParameterReferencesTest extends AbstractZipTest {
 		Parameter p2 = createParameter("p2", "2*p1*p3", ParameterScope.PROCESS, null);
 		Parameter p3Global = createParameter("p3", "2*p4", null, null);
 		Parameter p4Global = createParameter("p4", "2", null, null);
-		ImpactCategory impact = createImpactCategory(
-				null, new Parameter[] { p1, p2 });
+		ImpactCategory impact = impactCategoryOf(
+			null, new Parameter[]{p1, p2});
 		with((store) -> {
 			JsonExport export = new JsonExport(db, store);
 			export.write(impact);
@@ -214,31 +120,6 @@ public class ParameterReferencesTest extends AbstractZipTest {
 			assertNull(store.get(ModelType.PARAMETER, p2.refId));
 			assertNotNull(store.get(ModelType.PARAMETER, p3Global.refId));
 			assertNotNull(store.get(ModelType.PARAMETER, p4Global.refId));
-		});
-	}
-
-	@Test
-	public void testImpactCategoryParameterUncertainty() {
-		Uncertainty u1 = createUncertainty("1", "p2", "p3");
-		Uncertainty u2 = createUncertainty("p1", "2", "3");
-		Parameter p1 = createParameter("p1", null, null, null);
-		Parameter p2 = createParameter("p2", null, null, null);
-		Parameter p3 = createParameter("p3", null, null, null);
-		Parameter p1Intern = createParameter("p1", "2*2",
-				ParameterScope.PROCESS, u1);
-		Parameter p2Intern = createParameter("p2", "2*2",
-				ParameterScope.PROCESS, u2);
-		ImpactCategory impact = createImpactCategory(null, new Parameter[] {
-				p1Intern, p2Intern });
-		with((store) -> {
-			JsonExport export = new JsonExport(db, store);
-			export.write(impact);
-			assertNotNull(store.get(ModelType.IMPACT_CATEGORY, impact.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1.refId));
-			assertNull(store.get(ModelType.PARAMETER, p1Intern.refId));
-			assertNull(store.get(ModelType.PARAMETER, p2.refId));
-			assertNull(store.get(ModelType.PARAMETER, p2Intern.refId));
-			assertNotNull(store.get(ModelType.PARAMETER, p3.refId));
 		});
 	}
 
@@ -253,7 +134,7 @@ public class ParameterReferencesTest extends AbstractZipTest {
 			JsonExport export = new JsonExport(db, store);
 			export.write(system);
 			assertNotNull(store.get(ModelType.PRODUCT_SYSTEM,
-					system.refId));
+				system.refId));
 			assertNotNull(store.get(ModelType.PARAMETER, p1.refId));
 			assertNull(store.get(ModelType.PARAMETER, p2.refId));
 		});
@@ -276,7 +157,7 @@ public class ParameterReferencesTest extends AbstractZipTest {
 	}
 
 	private Parameter createParameter(String name, String formula,
-			ParameterScope scope, Uncertainty u) {
+		ParameterScope scope, Uncertainty u) {
 		Parameter p = new Parameter();
 		p.name = name;
 		p.value = 1;
@@ -296,24 +177,9 @@ public class ParameterReferencesTest extends AbstractZipTest {
 		return new ParameterDao(db).insert(p);
 	}
 
-	private Uncertainty createUncertainty(String f1, String f2, String f3) {
-		if (f3 == null) {
-			Uncertainty u = Uncertainty.logNormal(1, 1);
-			u.formula1 = f1;
-			u.formula2 = f2;
-			return u;
-		}
-		Uncertainty u = Uncertainty.triangle(1, 1, 1);
-		u.formula1 = f1;
-		u.formula2 = f2;
-		u.formula3 = f3;
-		return u;
-	}
-
-	private Exchange createExchange(String formula, Uncertainty u) {
+	private Exchange exchangeOf(String formula) {
 		Exchange e = new Exchange();
 		e.formula = formula;
-		e.uncertainty = u;
 		return e;
 	}
 
@@ -327,15 +193,14 @@ public class ParameterReferencesTest extends AbstractZipTest {
 		return p;
 	}
 
-	private ImpactFactor createImpactFactor(String formula, Uncertainty u) {
+	private ImpactFactor impactFactorOf(String formula) {
 		ImpactFactor f = new ImpactFactor();
 		f.formula = formula;
-		f.uncertainty = u;
 		return f;
 	}
 
-	private ImpactCategory createImpactCategory(ImpactFactor[] factors,
-			Parameter[] parameters) {
+	private ImpactCategory impactCategoryOf(ImpactFactor[] factors,
+		Parameter[] parameters) {
 		ImpactCategory c = new ImpactCategory();
 		c.refId = UUID.randomUUID().toString();
 		if (parameters != null) {
