72f6991e4df8ad41329484dd28e10ab1e9ad2110
msrocka <michael.srocka@gmail.com>
2022-07-18 17:34:39 +0200

    write in separate thread

--- olca-core/src/main/java/org/openlca/jsonld/input/BatchImport.java
+++ olca-core/src/main/java/org/openlca/jsonld/input/BatchImport.java
@@ -1,6 +1,10 @@
 package org.openlca.jsonld.input;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.openlca.core.model.ModelType;
 import org.openlca.core.model.RootEntity;
@@ -11,6 +15,7 @@ class BatchImport<T extends RootEntity> {
 	private final Class<T> clazz;
 	private final ModelType type;
 	private final int batchSize;
+	private final Writer writer = new Writer();
 
 	private final ArrayList<RootEntity> inserts = new ArrayList<>();
 	private final ArrayList<RootEntity> updates = new ArrayList<>();
@@ -51,6 +56,7 @@ class BatchImport<T extends RootEntity> {
 		if (updates.size() > 0) {
 			flushUpdates();
 		}
+		writer.close();
 	}
 
 	private void insert(RootEntity entity) {
@@ -68,15 +74,60 @@ class BatchImport<T extends RootEntity> {
 	}
 
 	private void flushInserts() {
-		imp.db().transaction(em -> inserts.forEach(em::persist));
-		inserts.forEach(imp::visited);
+		writer.insert(inserts);
 		inserts.clear();
 	}
 
 	private void flushUpdates() {
-		imp.db().transaction(em -> updates.replaceAll(em::merge));
-		updates.forEach(imp::visited);
+		writer.update(updates);
 		updates.clear();
+	}
+
+	private class Writer {
+
+		private final ExecutorService exec = Executors.newFixedThreadPool(1);
+		private final ArrayList<RootEntity> buffer = new ArrayList<>();
+		private volatile Future<?> task;
+
+		void insert(List<RootEntity> batch) {
+			next(batch);
+			task = exec.submit(() -> {
+				imp.db().transaction(em -> buffer.forEach(em::persist));
+				buffer.forEach(imp::visited);
+				buffer.clear();
+			});
+		}
+
+		void update(List<RootEntity> batch) {
+			next(batch);
+			task = exec.submit(() -> {
+				imp.db().transaction(em -> buffer.replaceAll(em::merge));
+				buffer.forEach(imp::visited);
+				buffer.clear();
+			});
+		}
+
+		private void next(List<RootEntity> batch) {
+			flush();
+			buffer.clear();
+			buffer.addAll(batch);
+		}
+
+		private void flush() {
+			if (task != null) {
+				try {
+					task.get();
+					task = null;
+				} catch (Exception e) {
+					throw new RuntimeException("failed to wait for worker", e);
+				}
+			}
+		}
+
+		private void close() {
+			flush();
+			exec.shutdown();
+		}
 	}
 
 }
