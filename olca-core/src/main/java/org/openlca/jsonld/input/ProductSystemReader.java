b0a015a6c577d1095953847e18dd9cd2f4ec2b1c
msrocka <michael.srocka@gmail.com>
2022-07-19 18:21:29 +0200

    a new way to resolve process links
    it will only cache IDs from the exchange table
    of processes and flows that are really used.
    before it cached exchange ID pair etc

--- olca-core/src/main/java/org/openlca/jsonld/input/ProductSystemReader.java
+++ olca-core/src/main/java/org/openlca/jsonld/input/ProductSystemReader.java
@@ -1,15 +1,23 @@
 package org.openlca.jsonld.input;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 import com.google.gson.JsonObject;
+import gnu.trove.impl.Constants;
+import gnu.trove.map.hash.TIntLongHashMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
+import gnu.trove.set.hash.TLongHashSet;
+import org.openlca.core.database.NativeSql;
 import org.openlca.core.io.EntityResolver;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.Flow;
 import org.openlca.core.model.ParameterRedefSet;
 import org.openlca.core.model.Process;
+import org.openlca.core.model.ProcessLink;
 import org.openlca.core.model.ProductSystem;
 import org.openlca.core.model.Result;
 import org.openlca.core.model.descriptors.Descriptor;
@@ -19,7 +27,6 @@ public class ProductSystemReader implements EntityReader<ProductSystem> {
 
 	private final EntityResolver resolver;
 	private final Map<String, Descriptor> processes = new HashMap<>();
-	private final Map<String, Descriptor> flows = new HashMap<>();
 
 	public ProductSystemReader(EntityResolver resolver) {
 		this.resolver = Objects.requireNonNull(resolver);
@@ -37,7 +44,6 @@ public class ProductSystemReader implements EntityReader<ProductSystem> {
 
 		// clear resources in case the reader is re-used
 		processes.clear();
-		flows.clear();
 
 		Util.mapBase(system, json, resolver);
 		mapQRef(json, system);
@@ -50,17 +56,23 @@ public class ProductSystemReader implements EntityReader<ProductSystem> {
 
 		// add link references and cache them
 		system.processLinks.clear();
+		var linkRefs = new ArrayList<LinkRef>();
 		Json.forEachObject(json, "processLinks", obj -> {
-			addProcess(system, Json.getObject(obj, "provider"));
-			addProcess(system, Json.getObject(obj, "process"));
+			var provider = addProcess(system, Json.getObject(obj, "provider"));
+			var process = addProcess(system, Json.getObject(obj, "process"));
 			String flowRefId = Json.getRefId(obj, "flow");
 			var flow = resolver.getDescriptor(Flow.class, flowRefId);
-			if (flow != null) {
-				flows.put(flowRefId, flow);
-			}
+			var exchangeRef = Json.getObject(obj, "exchange");
+			var exchangeId = Json.getInt(exchangeRef, "internalId");
+			if (provider == null
+				|| process == null
+				|| flow == null
+				|| exchangeId.isEmpty())
+				return;
+			linkRefs.add(new LinkRef(provider, process, flow, exchangeId.getAsInt()));
 		});
 
-		ProductSystemLinks.map(json, resolver, system);
+		resolveLinks(system, linkRefs);
 	}
 
 	private void mapQRef(JsonObject json, ProductSystem system) {
@@ -127,12 +139,15 @@ public class ProductSystemReader implements EntityReader<ProductSystem> {
 		});
 	}
 
-	private void addProcess(ProductSystem s, JsonObject ref) {
+	private Descriptor addProcess(ProductSystem s, JsonObject ref) {
 		if (ref == null)
-			return;
+			return null;
 		String refId = Json.getString(ref, "@id");
-		if (refId == null || processes.containsKey(refId))
-			return;
+		if (refId == null)
+			return null;
+		var d = processes.get(refId);
+		if (d != null)
+			return d;
 
 		String type = Json.getString(ref, "@type");
 		var clazz = type == null
@@ -144,9 +159,78 @@ public class ProductSystemReader implements EntityReader<ProductSystem> {
 		};
 
 		var p = resolver.getDescriptor(clazz, refId);
-		if (p != null) {
-			processes.put(refId, p);
-			s.processes.add(p.id);
+		if (p == null)
+			return null;
+		processes.put(refId, p);
+		s.processes.add(p.id);
+		return p;
+	}
+
+	private void resolveLinks(ProductSystem system, List<LinkRef> refs) {
+		var db = resolver.db();
+		if (db == null)
+			return;
+
+		var processIds = new TLongHashSet();
+		var flowIds = new TLongHashSet();
+		for (var ref : refs) {
+			processIds.add(ref.provider.id);
+			processIds.add(ref.process.id);
+			flowIds.add(ref.flow.id);
 		}
+
+		var map = new TLongObjectHashMap<TIntLongHashMap>();
+		var sql = "select id, internal_id, f_owner, f_flow from tbl_exchanges";
+		NativeSql.on(db).query(sql, r -> {
+			long processId = r.getLong(3);
+			if (!processIds.contains(processId))
+				return true;
+			long flowId = r.getLong(4);
+			if (!flowIds.contains(flowId))
+				return true;
+
+			long eid = r.getLong(1);
+			int iid = r.getInt(2);
+			var imap = map.get(processId);
+			if (imap == null) {
+				imap = new TIntLongHashMap(
+					Constants.DEFAULT_CAPACITY, Constants.DEFAULT_LOAD_FACTOR, -1, -1);
+				map.put(processId, imap);
+			}
+			imap.put(iid, eid);
+			return true;
+		});
+
+		for (var ref : refs) {
+			var link = ref.resolve(map);
+			if (link != null) {
+				system.processLinks.add(link);
+			}
+		}
+	}
+
+	private record LinkRef(
+		Descriptor provider,
+		Descriptor process,
+		Descriptor flow,
+		int exchange) {
+
+		ProcessLink resolve(TLongObjectHashMap<TIntLongHashMap> map) {
+			long processId = process.id;
+			var imap = map.get(processId);
+			if (imap == null)
+				return null;
+			long exchangeId = imap.get(exchange);
+			if (exchangeId < 0)
+				return null;
+			var link = new ProcessLink();
+			link.providerId = provider.id;
+			link.setProviderType(provider.type);
+			link.processId = processId;
+			link.flowId = flow.id;
+			link.exchangeId = exchangeId;
+			return link;
+		}
+
 	}
 }
