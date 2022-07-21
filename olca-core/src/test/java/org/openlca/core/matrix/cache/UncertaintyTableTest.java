a66cb55b8b7a7023e60ea4bac23f1585e1b73805
msrocka <michael.srocka@gmail.com>
2022-07-21 16:57:18 +0200

    fix failing test

--- olca-core/src/test/java/org/openlca/core/matrix/cache/UncertaintyTableTest.java
+++ olca-core/src/test/java/org/openlca/core/matrix/cache/UncertaintyTableTest.java
@@ -1,43 +1,39 @@
 package org.openlca.core.matrix.cache;
 
-import java.util.HashSet;
-import java.util.Set;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.openlca.core.Tests;
 import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.ProcessDao;
-import org.openlca.core.matrix.CalcExchange;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.Process;
+import org.openlca.core.model.Uncertainty;
 
 /**
  * Tests that we get all uncertainty information from the matrix-table loaders.
  */
 public class UncertaintyTableTest {
 
-	private IDatabase database = Tests.getDb();
-	private MatrixCache cache = MatrixCache.createLazy(database);
+	private final IDatabase db = Tests.getDb();
+	private final MatrixCache cache = MatrixCache.createLazy(db);
 
 	@Test
 	public void testForExchange() throws Exception {
-		Exchange exchange = new Exchange();
-		Process process = new Process();
+
+		var exchange = new Exchange();
+		exchange.uncertainty = Uncertainty.triangle(1, 2, 3);
+		var process = new Process();
 		process.exchanges.add(exchange);
-		ProcessDao dao = new ProcessDao(database);
-		dao.insert(process);
-		Set<Long> set = new HashSet<>();
-		set.add(process.id);
-		var exchanges = cache.getExchangeCache().get(process.id);
-		checkExchange(exchanges.get(0));
-		dao.delete(process);
-	}
+		db.insert(process);
 
-	private void checkExchange(CalcExchange e) {
-		Assert.assertEquals(1, e.parameter1, 1e-16);
-		Assert.assertEquals(2, e.parameter2, 1e-16);
-		Assert.assertEquals(3, e.parameter3, 1e-16);
-	}
+		var e = cache.getExchangeCache()
+			.get(process.id)
+			.get(0);
+		assertEquals(1, e.parameter1, 1e-16);
+		assertEquals(2, e.parameter2, 1e-16);
+		assertEquals(3, e.parameter3, 1e-16);
 
+		db.delete(process);
+	}
 }
