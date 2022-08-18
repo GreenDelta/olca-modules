f5f4d78e8db067acc25136ea5c63d0cbb0d07f64
msrocka <michael.srocka@gmail.com>
2022-08-18 12:09:22 +0200

    finished ProjectReader

--- olca-proto-io/src/main/java/org/openlca/proto/io/input/ProjectReader.java
+++ olca-proto-io/src/main/java/org/openlca/proto/io/input/ProjectReader.java
@@ -3,7 +3,9 @@ package org.openlca.proto.io.input;
 
 import org.openlca.core.io.EntityResolver;
 import org.openlca.core.model.Project;
+import org.openlca.core.model.ProjectVariant;
 import org.openlca.proto.ProtoProject;
+import org.openlca.util.Strings;
 
 public record ProjectReader(EntityResolver resolver)
 	implements EntityReader<Project, ProtoProject> {
@@ -18,6 +20,66 @@ public record ProjectReader(EntityResolver resolver)
 	@Override
 	public void update(Project project, ProtoProject proto) {
 		Util.mapBase(project, ProtoWrap.of(proto), resolver);
+		mapAttrs(project, proto);
+		mapVariants(project, proto);
+	}
 
+	private void mapAttrs(Project p, ProtoProject proto) {
+		p.isWithCosts = proto.getIsWithCosts();
+		p.isWithRegionalization = proto.getIsWithRegionalization();
+		p.impactMethod = Util.getImpactMethod(resolver, proto.getImpactMethod());
+
+		// nw-set
+		p.nwSet = null;
+		if (p.impactMethod == null)
+			return;
+		var nwSetId = proto.getNwSet().getId();
+		if (Strings.nullOrEmpty(nwSetId))
+			return;
+		p.nwSet = p.impactMethod.nwSets.stream()
+			.filter(nwSet -> nwSetId.equals(nwSet.refId))
+			.findAny()
+			.orElse(null);
+	}
+
+	private void mapVariants(Project p, ProtoProject proto) {
+		p.variants.clear();
+
+		for (int i = 0; i < proto.getVariantsCount(); i++) {
+			var protoVar = proto.getVariants(i);
+			var v = new ProjectVariant();
+			v.productSystem = Util.getProductSystem(
+				resolver, protoVar.getProductSystem());
+			if (v.productSystem == null)
+				continue;
+
+			// flow, property, and unit
+			var flow = v.productSystem.referenceExchange != null
+				? v.productSystem.referenceExchange.flow
+				: null;
+			if (flow == null)
+				continue;
+			var quantity = Quantity.of(flow)
+				// TODO: .withProperty(protoVar.)
+				.withUnit(protoVar.getUnit())
+				.get();
+			v.flowPropertyFactor = quantity.factor();
+			v.unit = quantity.unit();
+
+			// attributes
+			v.name = protoVar.getName();
+			v.amount = protoVar.getAmount();
+			v.allocationMethod = Util.allocationMethodOf(
+				protoVar.getAllocationMethod());
+			v.description = protoVar.getDescription();
+			v.isDisabled = protoVar.getIsDisabled();
+
+			// parameter redefinitions
+			var redefs = Util.parameterRedefsOf(
+				protoVar.getParameterRedefsList(), resolver);
+			v.parameterRedefs.addAll(redefs);
+
+			p.variants.add(v);
+		}
 	}
 }
