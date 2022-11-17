b460a92d3b948e8ab60fc23c23031914676c27c8
msrocka <michael.srocka@gmail.com>
2022-11-17 13:27:16 +0100

    refdata: remove outdated exports
    LCIA pack: exported in 6.693 seconds

--- olca-io/src/main/java/org/openlca/io/refdata/RefDataExport.java
+++ olca-io/src/main/java/org/openlca/io/refdata/RefDataExport.java
@@ -35,25 +35,9 @@ public class RefDataExport implements Runnable {
 			new FlowExport(config).run();
 			new CurrencyExport(config).run();
 
-
-			export("lcia_methods.csv", new ImpactMethodExport());
-			export("lcia_categories.csv", new ImpactCategoryExport());
-			export("lcia_factors.csv", new ImpactFactorExport());
-			export("nw_sets.csv", new NwSetExport());
-			export("nw_set_factors.csv", new NwSetFactorExport());
-
 			exportMappingFiles();
 		} catch (Exception e) {
 			log.error("Reference data export failed", e);
-		}
-	}
-
-	private void export(String fileName, Export export) {
-		var file = new File(dir, fileName);
-		if (file.exists()) {
-			log.warn("the file already exists; skipped it");
-		} else {
-			export.run(file, db);
 		}
 	}
 
--- olca-io/src/test/java/org/openlca/io/RefDataExample.java
+++ olca-io/src/test/java/org/openlca/io/RefDataExample.java
@@ -3,19 +3,37 @@ package org.openlca.io;
 import java.io.File;
 
 import org.openlca.core.DataDir;
+import org.openlca.io.refdata.RefDataExport;
 import org.openlca.io.refdata.RefDataImport;
 import org.openlca.util.Dirs;
 
 public class RefDataExample {
 
 	public static void main(String[] args) {
+		runExport();
+		// runImport();
+	}
+
+	private static void runExport() {
+		var home = new File(System.getProperty("user.home"));
+		var refDir = new File(home, "Desktop/rems/refdata2");
+		var dataDir = DataDir.get();
+		try (var db = dataDir.openDatabase("lciapack")) {
+			var start = System.nanoTime();
+			new RefDataExport(refDir, db).run();
+			var ns = System.nanoTime() - start;
+			var seconds = ((double) ns) / 1e9;
+			System.out.printf("exported in %.3f seconds%n", seconds);
+		}
+	}
+
+	private static void runImport() {
 		var home = new File(System.getProperty("user.home"));
 		var refDir = new File(home, "Desktop/rems/refdata2");
 		var dataDir = DataDir.get();
 		Dirs.delete(dataDir.getDatabaseDir("refdata"));
 		try (var db = dataDir.openDatabase("refdata")) {
 			var start = System.nanoTime();
-			// new RefDataImport(refDir, db).run();
 			new RefDataImport(refDir, db).run();
 			var ns = System.nanoTime() - start;
 			var seconds = ((double) ns) / 1e9;
