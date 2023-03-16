eacf7ceb17dc2a7da006407c452b68aefc55f172
msrocka <michael.srocka@gmail.com>
2023-03-16 16:34:05 +0100

    add locations to exchanges when
    saving a regionalized result as system process; see https://github.com/GreenDelta/olca-app/issues/253

--- olca-core/src/main/java/org/openlca/core/results/SystemProcess.java
+++ olca-core/src/main/java/org/openlca/core/results/SystemProcess.java
@@ -1,40 +1,38 @@
 package org.openlca.core.results;
 
-import java.util.Date;
 import java.util.UUID;
 
-import org.openlca.core.database.FlowDao;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.model.CalculationSetup;
 import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowType;
+import org.openlca.core.model.Location;
 import org.openlca.core.model.Process;
 import org.openlca.core.model.ProcessType;
-import org.openlca.core.model.SocialAspect;
 
 public class SystemProcess {
 
 	public static Process create(
-		IDatabase database,
-		CalculationSetup setup,
-		LcaResult result,
-		String name) {
-		return new SystemProcess(database, setup, result, name).create(false);
+			IDatabase db, CalculationSetup setup, LcaResult result, String name
+	) {
+		return new SystemProcess(db, setup, result, name).create(false);
 	}
 
-	public static Process createWithMetaData(IDatabase database,
-		CalculationSetup setup, LcaResult result, String name) {
-		return new SystemProcess(database, setup, result, name).create(true);
+	public static Process createWithMetaData(
+			IDatabase db, CalculationSetup setup, LcaResult result, String name
+	) {
+		return new SystemProcess(db, setup, result, name).create(true);
 	}
 
-	private final FlowDao flowDao;
+	private final IDatabase db;
 	private final CalculationSetup setup;
 	private final LcaResult result;
 	private final String name;
 
-	private SystemProcess(IDatabase database, CalculationSetup setup,
-			LcaResult result, String name) {
-		this.flowDao = new FlowDao(database);
+	private SystemProcess(
+			IDatabase db, CalculationSetup setup, LcaResult result, String name
+	) {
+		this.db = db;
 		this.setup = setup;
 		this.result = result;
 		this.name = name;
@@ -60,40 +58,43 @@ public class SystemProcess {
 			return;
 		double amount = Math.abs(setup.demand());
 		p.quantitativeReference = flow.flowType == FlowType.WASTE_FLOW
-			? p.input(flow, amount)
-			: p.output(flow, amount);
+				? p.input(flow, amount)
+				: p.output(flow, amount);
 	}
 
 	private void addElemFlows(Process p) {
 		result.enviIndex().each((i, f) -> {
 			double amount = result.getTotalFlowValueOf(f);
-			if (amount == 0)
+			if (amount == 0 || f.flow() == null || f.isVirtual())
 				return;
-			Flow flow = flowDao.getForId(f.flow().id);
+			var flow = db.get(Flow.class, f.flow().id);
 			if (flow == null)
 				return;
-			if (f.isInput()) {
-				p.input(flow, amount);
-			} else {
-				p.output(flow, amount);
+			var exchange = f.isInput()
+					? p.input(flow, amount)
+					: p.output(flow, amount);
+			if (f.location() != null) {
+				exchange.location = db.get(Location.class, f.location().id);
 			}
 		});
 	}
 
 	private void copyMetaData(Process p) {
-		Process refProc = setup.process();
+		var refProc = setup.process();
 		if (refProc == null)
 			return;
-		for (SocialAspect sa : refProc.socialAspects)
+		for (var sa : refProc.socialAspects) {
 			p.socialAspects.add(sa.copy());
+		}
 		p.socialDqSystem = refProc.socialDqSystem;
 		p.category = refProc.category;
 		p.defaultAllocationMethod = refProc.defaultAllocationMethod;
 		p.description = refProc.description;
-		if (refProc.documentation != null)
+		if (refProc.documentation != null) {
 			p.documentation = refProc.documentation.copy();
+		}
 		p.infrastructureProcess = refProc.infrastructureProcess;
-		p.lastChange = new Date().getTime();
+		p.lastChange = System.currentTimeMillis();
 		p.location = refProc.location;
 	}
 }
