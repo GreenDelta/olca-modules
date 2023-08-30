ed3044c5705a99c251f0ca5ddde489d19021d654
msrocka <michael.srocka@gmail.com>
2023-08-30 10:53:03 +0200

    update make_mysql_schema script
    so that it can be executed from any location

--- olca-core/src/main/scripts/make_mysql_schema.py
+++ olca-core/src/main/scripts/make_mysql_schema.py
@@ -1,40 +1,43 @@
+from pathlib import Path
+
 
 replacements = [
-    ('BLOB(16 M)', 'MEDIUMBLOB'),
-    ('BLOB(32 M)', 'LONGBLOB'),
-    ('CLOB(64 K)', 'TEXT'),
-    ('SMALLINT default 0', 'TINYINT default 0'),
-    ('VARCHAR(32672)', 'VARCHAR(16383)'),
-
-    (' comment  ', ' `comment`'),
-    (' time  ', ' `time`'),
-    (' value  ', ' `value`'),
+    ("BLOB(16 M)", "MEDIUMBLOB"),
+    ("BLOB(32 M)", "LONGBLOB"),
+    ("CLOB(64 K)", "TEXT"),
+    ("SMALLINT default 0", "TINYINT default 0"),
+    ("VARCHAR(32672)", "VARCHAR(16383)"),
+    (" comment  ", " `comment`"),
+    (" time  ", " `time`"),
+    (" value  ", " `value`"),
 ]
 
 
 def main():
-    schema_dir = '../resources/org/openlca/core/database/internal'
+    folder = (
+        Path(__file__).parent
+        / "../resources/org/openlca/core/database/internal"
+    )
 
-    in_file = schema_dir + '/current_schema_derby.sql'
+    in_file = folder / "current_schema_derby.sql"
     lines = [
-        '-- generated with make_mysql_schema.py; do not edit\n'
-        '\n',
-        '-- CREATE DATABASE openlca;\n',
-        '-- USE openlca',
+        "-- generated with make_mysql_schema.py; do not edit\n" "\n",
+        "-- CREATE DATABASE openlca;\n",
+        "-- USE openlca",
     ]
-    with open(in_file, 'r', encoding='utf-8', newline='\n') as inp:
+    with open(in_file, "r", encoding="utf-8", newline="\n") as inp:
         for r in inp:
-            if r.strip().startswith('--'):
+            if r.strip().startswith("--"):
                 continue
             line = r
             for (a, b) in replacements:
                 line = line.replace(a, b)
             lines.append(line)
 
-    out_file = schema_dir + '/current_schema_mysql.sql'
-    with open(out_file, 'w', encoding='utf-8', newline='\n') as out:
+    out_file = folder / "current_schema_mysql.sql"
+    with open(out_file, "w", encoding="utf-8", newline="\n") as out:
         out.writelines(lines)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
