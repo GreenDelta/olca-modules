e12965d5e236365bc74a5416c6bfa3c5f480169a
msrocka <michael.srocka@gmail.com>
2023-11-16 16:02:07 +0100

    this fixes self-intersecting polygons
    example:
    
    #+begin_src java
    public static void main(String[] args) {
      try (var db = DataDir.get().openDatabase("lc-impact-tests")) {
        var file = new File("/home/ms/Desktop/lc-impact-pocp-setup.json");
        var setup = GeoFactorSetup.read(file, db);
        FeatureValidator.of(setup.features).fix();
        var fixed = new File("/home/ms/Desktop/lc-impact-pocp-setup_fixed.json");
        setup.writeTo(fixed);
    	}
    }
    #+end_src
    
    The ~buffer(0)~ fix is the not needed:
    
    #+begin_src java
    var fixedFeatures = new FeatureCollection();
    setup.features.features.forEach(f -> {
      var jts = JTS.fromGeoJSON(f.geometry).buffer(0);
      var feature = new Feature();
      feature.geometry = JTS.toGeoJSON(jts);
      feature.properties = f.properties;
      fixedFeatures.features.add(feature);
    });
    #+end_src

---
+++ olca-core/src/main/java/org/openlca/geo/calc/FeatureValidator.java
@@ -0,0 +1,129 @@
+package org.openlca.geo.calc;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.operation.polygonize.Polygonizer;
+import org.openlca.geo.geojson.Feature;
+import org.openlca.geo.geojson.FeatureCollection;
+
+import java.util.ArrayList;
+
+public class FeatureValidator {
+
+	private final FeatureCollection coll;
+
+	private FeatureValidator(FeatureCollection coll) {
+		this.coll = coll;
+	}
+
+	public static FeatureValidator of(FeatureCollection coll) {
+		return new FeatureValidator(coll);
+	}
+
+	/**
+	 * Checks if the geometries of the features in the collection are valid.
+	 *
+	 * @return {@code true} when all geometries are valid, {@code false} otherwise
+	 */
+	public boolean check() {
+		if (coll == null)
+			return false;
+		for (var f : coll.features) {
+			if (f.geometry == null)
+				return false;
+			var geo = JTS.fromGeoJSON(f.geometry);
+			if (!geo.isValid())
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Tries to fix invalid geometries of the features in the collection in place.
+	 * Currently, this is only done for self-intersecting polygons because the
+	 * intersection calculation would fail for them. We currently apply the
+	 * approach <a href="https://stackoverflow.com/a/31474580">here</a>. An
+	 * alternative is to run {@code buffer(0)} on such polygons as described
+	 * <a href="https://github.com/locationtech/jts/issues/657">here</a>.
+	 */
+	public void fix() {
+		if (coll == null)
+			return;
+		var fixed = new ArrayList<Feature>();
+		for (var f : coll.features) {
+			if (f.geometry == null)
+				continue;
+			var geo = fix(JTS.fromGeoJSON(f.geometry));
+			if (geo == null)
+				continue;
+			var feature = new Feature();
+			feature.geometry = JTS.toGeoJSON(geo);
+			feature.properties = f.properties;
+			fixed.add(feature);
+		}
+		coll.features.clear();
+		coll.features.addAll(fixed);
+	}
+
+	private Geometry fix(Geometry geom) {
+		if (geom == null)
+			return null;
+		if (geom.isValid()) {
+			geom.normalize();
+			return geom;
+		}
+
+		if (geom instanceof Polygon polygon) {
+			var polygonizer = new Polygonizer();
+			addPolygon(polygon, polygonizer);
+			return makeGeometry(polygonizer);
+		}
+
+		if (geom instanceof MultiPolygon) {
+			var polygonizer = new Polygonizer();
+			for (int i = geom.getNumGeometries(); i > 0; i--) {
+				if (geom.getGeometryN(i - 1) instanceof Polygon polygon) {
+					addPolygon(polygon, polygonizer);
+				}
+			}
+			return makeGeometry(polygonizer);
+		}
+
+		return geom;
+	}
+
+	private void addPolygon(Polygon polygon, Polygonizer p) {
+		addLine(polygon.getExteriorRing(), p);
+		for (int i = polygon.getNumInteriorRing(); i > 0; i--) {
+			addLine(polygon.getInteriorRingN(i - 1), p);
+		}
+	}
+
+	private void addLine(LineString line, Polygonizer p) {
+		if (line instanceof LinearRing) {
+			line = line.getFactory()
+					.createLineString(line.getCoordinateSequence());
+		}
+		// makes any self intersections explicit
+		var point = line.getFactory().createPoint(line.getCoordinateN(0));
+		var geometry = line.union(point);
+		p.add(geometry);
+	}
+
+	private Geometry makeGeometry(Polygonizer p) {
+		var it = p.getPolygons().iterator();
+		if (!it.hasNext())
+			return null;
+		if (!(it.next() instanceof Geometry g))
+			return null;
+		while (it.hasNext()) {
+			if (it.next() instanceof Geometry next) {
+				g = g.symDifference(next);
+			}
+		}
+		return g;
+	}
+}
--- olca-core/src/test/java/examples/GeoFactorExample.java
+++ olca-core/src/test/java/examples/GeoFactorExample.java
@@ -3,9 +3,6 @@ package examples;
 import org.openlca.core.DataDir;
 import org.openlca.core.model.ImpactCategory;
 import org.openlca.core.model.Location;
-import org.openlca.geo.calc.JTS;
-import org.openlca.geo.geojson.Feature;
-import org.openlca.geo.geojson.FeatureCollection;
 import org.openlca.geo.lcia.GeoFactorCalculator;
 import org.openlca.geo.lcia.GeoFactorSetup;
 
@@ -17,26 +14,11 @@ public class GeoFactorExample {
 	public static void main(String[] args) {
 
 		try (var db = DataDir.get().openDatabase("lc-impact-tests")) {
-			var setupFile = new File("/home/ms/Desktop/lc-impact-pocp-setup.json");
-
+			var setupFile = new File("/home/ms/Desktop/lc-impact-pocp-setup_fixed.json");
 			var setup = GeoFactorSetup.read(setupFile, db);
-
-			var fixedFeatures = new FeatureCollection();
-			setup.features.features.forEach(f -> {
-				var jts = JTS.fromGeoJSON(f.geometry).buffer(0);
-				var feature = new Feature();
-				feature.geometry = JTS.toGeoJSON(jts);
-				feature.properties = f.properties;
-				fixedFeatures.features.add(feature);
-			});
-
-			var fixedSetup = new GeoFactorSetup(fixedFeatures);
-			fixedSetup.bindings.addAll(setup.bindings);
-			fixedSetup.properties.addAll(setup.properties);
-
 			var impact = db.get(ImpactCategory.class, "39ec61cf-a337-4535-b36b-98b5dee816f5");
 			var afg = db.get(Location.class, "f0357a3f-154b-32ff-a2bf-f55055457068");
-			var calc = new GeoFactorCalculator(db, fixedSetup, impact, List.of(afg));
+			var calc = new GeoFactorCalculator(db, setup, impact, List.of(afg));
 			calc.run();
 
 			for (var factor : impact.impactFactors) {
