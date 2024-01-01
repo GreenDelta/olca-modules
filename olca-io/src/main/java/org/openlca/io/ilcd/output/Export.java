d60d072df03b23244b463590b4cdf0e056fecbd0
msrocka <michael.srocka@gmail.com>
2024-01-01 16:44:50 +0100

    remove DataSetRef util

--- olca-io/src/main/java/org/openlca/io/ilcd/output/Export.java
+++ olca-io/src/main/java/org/openlca/io/ilcd/output/Export.java
@@ -1,12 +1,16 @@
 package org.openlca.io.ilcd.output;
 
 import org.openlca.core.model.Actor;
+import org.openlca.core.model.Epd;
+import org.openlca.core.model.ImpactCategory;
+import org.openlca.core.model.ProductSystem;
 import org.openlca.core.model.RootEntity;
 import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowProperty;
 import org.openlca.core.model.Process;
 import org.openlca.core.model.Source;
 import org.openlca.core.model.UnitGroup;
+import org.openlca.ilcd.commons.DataSetType;
 import org.openlca.ilcd.commons.Ref;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,19 +30,19 @@ class Export {
 	 * Runs an export of the given model to the ILCD data store and returns the
 	 * data set reference to the exported model in the store.
 	 */
-	public static Ref of(RootEntity model, ILCDExport config) {
+	public static Ref of(RootEntity model, ILCDExport exp) {
 		if (model instanceof Source)
-			return source((Source) model, config);
+			return source((Source) model, exp);
 		if (model instanceof Actor)
-			return actor((Actor) model, config);
+			return actor((Actor) model, exp);
 		if (model instanceof Flow)
-			return flow((Flow) model, config);
+			return flow((Flow) model, exp);
 		if (model instanceof FlowProperty)
-			return property((FlowProperty) model, config);
+			return property((FlowProperty) model, exp);
 		if (model instanceof Process)
-			return process((Process) model, config);
+			return process((Process) model, exp);
 		if (model instanceof UnitGroup)
-			return unitGroup((UnitGroup) model, config);
+			return unitGroup((UnitGroup) model, exp);
 		log.error("Cannot export {}", model);
 		return null;
 	}
@@ -47,7 +51,7 @@ class Export {
 		try {
 			var export = new SourceExport(config);
 			export.run(source);
-			return DataSetRef.refOf(source, config);
+			return refOf(source, config);
 		} catch (Exception e) {
 			log.error("Export of source failed: " + source, e);
 			return null;
@@ -58,7 +62,7 @@ class Export {
 		try {
 			var export = new ActorExport(config);
 			export.run(actor);
-			return DataSetRef.refOf(actor, config);
+			return refOf(actor, config);
 		} catch (Exception e) {
 			log.error("Export of actor failed: " + actor, e);
 			return null;
@@ -69,7 +73,7 @@ class Export {
 		try {
 			var export = new FlowExport(config);
 			export.run(flow);
-			return DataSetRef.refOf(flow, config);
+			return refOf(flow, config);
 		} catch (Exception e) {
 			log.error("Export of flow failed: " + flow, e);
 			return null;
@@ -80,7 +84,7 @@ class Export {
 		try {
 			var export = new FlowPropertyExport(config);
 			export.run(prop);
-			return DataSetRef.refOf(prop, config);
+			return refOf(prop, config);
 		} catch (Exception e) {
 			log.error("Export of flow property failed: " + prop, e);
 			return null;
@@ -91,7 +95,7 @@ class Export {
 		try {
 			var export = new UnitGroupExport(config);
 			export.run(unitGroup);
-			return DataSetRef.refOf(unitGroup, config);
+			return refOf(unitGroup, config);
 		} catch (Exception e) {
 			log.error("Export of unit group failed: " + unitGroup, e);
 			return null;
@@ -102,11 +106,60 @@ class Export {
 		try {
 			var export = new ProcessExport(config);
 			export.run(process);
-			return DataSetRef.refOf(process, config);
+			return refOf(process, config);
 		} catch (Exception e) {
 			log.error("Export of process failed: " + process, e);
 			return null;
 		}
+	}
+
+	public static Ref refOf(RootEntity e, ILCDExport exp) {
+		if (e == null) {
+			return new Ref();
+		}
+		var ref = new Ref();
+		ref.version = "01.00.000";
+		exp.add(ref.name, e.name);
+		ref.uuid = e.refId;
+		ref.type = refTypeOf(e);
+		ref.uri = "../" + pathOf(ref.type) + "/" + e.refId + ".xml";
+		return ref;
+	}
+
+	private static DataSetType refTypeOf(RootEntity e) {
+		if (e instanceof Actor)
+			return DataSetType.CONTACT;
+		if (e instanceof Source)
+			return DataSetType.SOURCE;
+		if (e instanceof UnitGroup)
+			return DataSetType.UNIT_GROUP;
+		if (e instanceof FlowProperty)
+			return DataSetType.FLOW_PROPERTY;
+		if (e instanceof Flow)
+			return DataSetType.FLOW;
+		if (e instanceof ImpactCategory)
+			return DataSetType.LCIA_METHOD;
+		if (e instanceof Process || e instanceof Epd)
+			return DataSetType.PROCESS;
+		if (e instanceof ProductSystem)
+			return DataSetType.MODEL;
+		return null;
+	}
+
+	private static String pathOf(DataSetType type) {
+		if (type == null)
+			return "?";
+		return switch (type) {
+			case CONTACT -> "contacts";
+			case SOURCE -> "sources";
+			case UNIT_GROUP -> "unitgroups";
+			case FLOW_PROPERTY -> "flowproperties";
+			case FLOW -> "flows";
+			case PROCESS -> "processes";
+			case MODEL -> "lifecyclemodels";
+			case LCIA_METHOD -> "lciamethods";
+			case EXTERNAL_FILE -> "external_docs";
+		};
 	}
 
 }
