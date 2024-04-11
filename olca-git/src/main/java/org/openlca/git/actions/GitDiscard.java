2205d000288a67400b028a3fc32ee3f182b7798d
Sebastian Greve <greve@greendelta.com>
2024-04-11 16:18:04 +0200

    Splitting StashCreate and Discard

---
+++ olca-git/src/main/java/org/openlca/git/actions/GitDiscard.java
@@ -0,0 +1,82 @@
+package org.openlca.git.actions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openlca.git.Compatibility;
+import org.openlca.git.Compatibility.UnsupportedClientVersionException;
+import org.openlca.git.model.Change;
+import org.openlca.git.model.Change.ChangeType;
+import org.openlca.git.model.Commit;
+import org.openlca.git.repo.ClientRepository;
+
+public class GitDiscard extends GitProgressAction<String> {
+
+	protected final ClientRepository repo;
+	protected List<Change> changes;
+
+	protected GitDiscard(ClientRepository repo) {
+		this.repo = repo;
+	}
+
+	public static GitDiscard on(ClientRepository repo) {
+		return new GitDiscard(repo);
+	}
+
+	public GitDiscard changes(List<Change> changes) {
+		this.changes = changes;
+		return this;
+	}
+
+	@Override
+	public String run() throws IOException {
+		checkValidInputs();
+		updateDatabase();
+		progressMonitor.beginTask("Reloading descriptors");
+		repo.descriptors.reload();
+		return null;
+	}
+
+	protected void checkValidInputs() throws UnsupportedClientVersionException {
+		if (repo == null || repo.database == null)
+			throw new IllegalStateException("Git repository and database must be set");
+		if (changes == null) {
+			changes = Change.of(repo.diffs.find().withDatabase());
+		}
+		if (changes.isEmpty())
+			throw new IllegalStateException("No changes found");
+		Compatibility.checkRepositoryClientVersion(repo);
+	}
+
+	private void updateDatabase() throws IOException {
+		var commit = repo.commits.head();
+		if (commit != null) {
+			restoreDataFrom(commit);
+		}
+		deleteAddedData();
+	}
+
+	private void restoreDataFrom(Commit commit) {
+		var toImport = changes.stream()
+				.filter(c -> c.changeType != ChangeType.ADD)
+				.map(c -> repo.references.get(c.type, c.refId, commit.id))
+				.collect(Collectors.toList());
+		var gitStore = new GitStoreReader(repo, commit, toImport);
+		ImportData.from(gitStore)
+				.with(progressMonitor)
+				.into(repo.database)
+				.run();
+	}
+
+	private void deleteAddedData() {
+		var toDelete = changes.stream()
+				.filter(c -> c.changeType == ChangeType.ADD)
+				.collect(Collectors.toList());
+		DeleteData.from(repo.database)
+				.with(progressMonitor)
+				.data(toDelete)
+				.run();
+	}
+
+}
--- olca-git/src/main/java/org/openlca/git/actions/GitStashCreate.java
+++ olca-git/src/main/java/org/openlca/git/actions/GitStashCreate.java
@@ -2,37 +2,32 @@ package org.openlca.git.actions;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
-import org.openlca.git.Compatibility;
 import org.openlca.git.Compatibility.UnsupportedClientVersionException;
 import org.openlca.git.model.Change;
-import org.openlca.git.model.Change.ChangeType;
 import org.openlca.git.model.Commit;
 import org.openlca.git.repo.ClientRepository;
 import org.openlca.git.writer.DbCommitWriter;
 
-public class GitStashCreate extends GitProgressAction<String> {
+public class GitStashCreate extends GitDiscard {
 
-	private final ClientRepository repo;
-	private List<Change> changes;
 	private PersonIdent committer;
 	private Commit parent;
-	private boolean discard;
 
 	private GitStashCreate(ClientRepository repo) {
-		this.repo = repo;
+		super(repo);
 	}
 
 	public static GitStashCreate on(ClientRepository repo) {
 		return new GitStashCreate(repo);
 	}
 
+	@Override
 	public GitStashCreate changes(List<Change> changes) {
-		this.changes = changes;
+		super.changes(changes);
 		return this;
 	}
 
@@ -46,35 +41,19 @@ public class GitStashCreate extends GitProgressAction<String> {
 		return this;
 	}
 
-	public GitStashCreate discard() {
-		this.discard = true;
-		return this;
-	}
-
 	@Override
 	public String run() throws IOException {
 		checkValidInputs();
-		String commitId = null;
-		if (!discard) {
-			commitId = writeStashCommit();
-		}
-		updateDatabase();
-		progressMonitor.beginTask("Reloading descriptors");
-		repo.descriptors.reload();
+		var commitId = writeStashCommit();
+		super.run();
 		return commitId;
 	}
 
-	private void checkValidInputs() throws UnsupportedClientVersionException {
-		if (repo == null || repo.database == null)
-			throw new IllegalStateException("Git repository and database must be set");
-		if (!discard && committer == null)
+	@Override
+	protected void checkValidInputs() throws UnsupportedClientVersionException {
+		super.checkValidInputs();
+		if (committer == null)
 			throw new IllegalStateException("Committer must be set");
-		if (changes == null) {
-			changes = Change.of(repo.diffs.find().withDatabase());
-		}
-		if (changes.isEmpty())
-			throw new IllegalStateException("No changes found");
-		Compatibility.checkRepositoryClientVersion(repo);
 	}
 
 	private String writeStashCommit() throws IOException {
@@ -87,38 +66,6 @@ public class GitStashCreate extends GitProgressAction<String> {
 				.parent(parent)
 				.with(progressMonitor)
 				.write("Stashed changes", changes);
-	}
-
-	private void updateDatabase() throws IOException {
-		var commit = parent == null
-				? repo.commits.head()
-				: parent;
-		if (commit != null) {
-			restoreDataFrom(commit);
-		}
-		deleteAddedData();
-	}
-
-	private void restoreDataFrom(Commit commit) {
-		var toImport = changes.stream()
-				.filter(c -> c.changeType != ChangeType.ADD)
-				.map(c -> repo.references.get(c.type, c.refId, commit.id))
-				.collect(Collectors.toList());
-		var gitStore = new GitStoreReader(repo, commit, toImport);
-		ImportData.from(gitStore)
-				.with(progressMonitor)
-				.into(repo.database)
-				.run();
-	}
-
-	private void deleteAddedData() {
-		var toDelete = changes.stream()
-				.filter(c -> c.changeType == ChangeType.ADD)
-				.collect(Collectors.toList());
-		DeleteData.from(repo.database)
-				.with(progressMonitor)
-				.data(toDelete)
-				.run();
 	}
 
 }
