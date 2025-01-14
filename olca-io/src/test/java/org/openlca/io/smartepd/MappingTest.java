9d28f4d693cbb97b1b5b49742af4262a691e48a3
msrocka <michael.srocka@gmail.com>
2025-01-14 14:48:07 +0100

    smartepd: convert method mappings

--- olca-io/src/test/java/org/openlca/io/smartepd/MappingTest.java
+++ olca-io/src/test/java/org/openlca/io/smartepd/MappingTest.java
@@ -4,16 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class MethodMappingTest {
+public class MappingTest {
 
 	@Test
-	public void testGetAll() {
+	public void testMethodMappings() {
 		var mappings = MethodMapping.getDefault();
 		assertFalse(mappings.isEmpty());
 		for (var m : mappings) {
 			assertNotNull(m.method());
 			assertNotNull(m.ref());
 			assertFalse(m.indicators().isEmpty());
+		}
+	}
+
+	@Test
+	public void testIndicatorMappings() {
+		var mappings = IndicatorMapping.getDefault();
+		assertFalse(mappings.isEmpty());
+		for (var m : mappings) {
+			assertNotNull(m.indicator());
+			assertFalse(m.refs().isEmpty());
 		}
 	}
 
