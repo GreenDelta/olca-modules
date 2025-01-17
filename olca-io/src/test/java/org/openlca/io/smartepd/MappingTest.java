4fc16f6ab42d75efc139dac314e23f569075e035
msrocka <michael.srocka@gmail.com>
2025-01-17 12:38:52 +0100

    test indicator and method mappings by code

--- olca-io/src/test/java/org/openlca/io/smartepd/MappingTest.java
+++ olca-io/src/test/java/org/openlca/io/smartepd/MappingTest.java
@@ -2,10 +2,21 @@ package org.openlca.io.smartepd;
 
 import static org.junit.Assert.*;
 
+import java.util.EnumMap;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openlca.core.DataDir;
+import org.openlca.core.model.Epd;
+import org.openlca.core.model.EpdModule;
+import org.openlca.core.model.Flow;
+import org.openlca.core.model.FlowProperty;
+import org.openlca.core.model.ImpactCategory;
 import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.ImpactResult;
+import org.openlca.core.model.Result;
+import org.openlca.core.model.UnitGroup;
 
 public class MappingTest {
 
@@ -80,4 +91,83 @@ public class MappingTest {
 		}
 	}
 
+	@Test
+	public void testMapIndicatorCodesInWriter() {
+		var units = UnitGroup.of("Units of mass", "kg");
+		var mass = FlowProperty.of("Mass", units);
+		var p = Flow.product("p", mass);
+		var epd = Epd.of("test epd", p);
+
+		var result = Result.of("A1", p);
+		epd.modules.add(EpdModule.of("A1", result));
+		var method = ImpactMethod.of("EF 3.1");
+		method.code = SmartMethod.EF_3_1.id();
+		result.impactMethod = method;
+
+		var values = new EnumMap<SmartIndicator, Double>(SmartIndicator.class);
+		var rand = ThreadLocalRandom.current();
+		for (var i : SmartIndicator.values()) {
+			var impact = ImpactCategory.of("I1", i.unitFor(SmartMethod.EF_3_1));
+			impact.code = i.id();
+			double value = rand.nextDouble();
+			values.put(i, value);
+			var r = ImpactResult.of(impact, value);
+			result.impactResults.add(r);
+		}
+
+		var smart = SmartEpdWriter.of(epd).write();
+
+		for (var i : SmartIndicator.values()) {
+			var impact = smart.resultsOf(i.type())
+					.getFirst()
+					.results()
+					.stream()
+					.filter(r -> {
+						var id = i.isImpact() ? r.impact() : r.indicator();
+						return id.equals(i.id());
+					}).findFirst()
+					.orElseThrow()
+					.values()
+					.getFirst();
+			assertEquals("A1", impact.module().name());
+			assertEquals(values.get(i), impact.value(), 1e-9);
+		}
+	}
+
+	@Test
+	public void testMapMethodCodesInWriter() {
+		var units = UnitGroup.of("Units of mass", "kg");
+		var mass = FlowProperty.of("Mass", units);
+		var p = Flow.product("p", mass);
+
+		for (var m : SmartMethod.values()) {
+			var epd = Epd.of("test epd", p);
+			var result = Result.of("A1", p);
+			epd.modules.add(EpdModule.of("A1", result));
+			var method = ImpactMethod.of("M");
+			method.code = m.id();
+			result.impactMethod = method;
+			var impact = ImpactCategory.of(
+					"I1", SmartIndicator.GWP_TOTAL.defaultUnit());
+			impact.code = SmartIndicator.GWP_TOTAL.id();
+			var value = ThreadLocalRandom.current().nextDouble();
+			var r = ImpactResult.of(impact, value);
+			result.impactResults.add(r);
+
+			var smart = SmartEpdWriter.of(epd).write();
+			var smartImpact = smart.resultsOf(SmartIndicatorType.IMPACT)
+					.getFirst()
+					.results()
+					.stream()
+					.filter(res -> res.method().equals(m.id()))
+					.findFirst()
+					.orElseThrow();
+			assertEquals(m.id(), smartImpact.method());
+			assertEquals(SmartIndicator.GWP_TOTAL.id(), smartImpact.impact());
+			var vs = smartImpact.values();
+			assertEquals(1, vs.size());
+			assertEquals(value, vs.getFirst().value(), 1e-9);
+			assertEquals("A1", vs.getFirst().module().name());
+		}
+	}
 }
