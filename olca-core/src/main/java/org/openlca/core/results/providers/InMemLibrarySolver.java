40c97b9791ca56447c13634c76b6a874c0948f38
msrocka <michael.srocka@gmail.com>
2025-04-03 16:50:31 +0200

    handle impacts & costs in the InMemLib solver

--- olca-core/src/main/java/org/openlca/core/results/providers/InMemLibrarySolver.java
+++ olca-core/src/main/java/org/openlca/core/results/providers/InMemLibrarySolver.java
@@ -8,29 +8,37 @@ import org.openlca.core.matrix.MatrixData;
 import org.openlca.core.matrix.format.MatrixBuilder;
 import org.openlca.core.matrix.format.MatrixReader;
 import org.openlca.core.matrix.index.EnviIndex;
+import org.openlca.core.matrix.index.ImpactIndex;
+import org.openlca.core.matrix.index.MatrixIndex;
 import org.openlca.core.matrix.index.TechIndex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gnu.trove.list.array.TDoubleArrayList;
 
 /// A solver that loads all library data in to memory first and then uses a
 /// standard solver to calculate the results.
 public class InMemLibrarySolver {
 
-	public static ResultProvider create(SolverContext ctx) {
+	public static ResultProvider solve(SolverContext ctx) {
 		if (!ctx.hasLibraryLinks())
 			return ResultProviders.solve(ctx);
 
-		var dataF = ctx.data();
-		var fullTechIdx = new TechIndex();
-
-		var libQueue = new ArrayDeque<String>();
-		for (var techFlow : dataF.techIndex) {
-			fullTechIdx.add(techFlow);
-			if (techFlow.isFromLibrary()) {
-				libQueue.add(techFlow.library());
-			}
-		}
+		var fullData = new Builder(ctx).build();
+		var fullCtx = SolverContext.of(ctx.db(), fullData)
+				.withSolver(ctx.solver());
+		var n = fullData.techIndex.size();
+		var isSmall = ctx.solver().isNative()
+				? n < 1000
+				: n < 500;
+		return isSmall || !fullData.isSparse()
+				? InversionResult.of(fullCtx).calculate().provider()
+				: FactorizationSolver.solve(fullCtx);
 	}
 
-	private final class Builder {
+	private static final class Builder {
+
+		private final Logger log = LoggerFactory.getLogger(getClass());
 
 		private final SolverContext ctx;
 		private final MatrixData origin;
@@ -39,47 +47,139 @@ public class InMemLibrarySolver {
 		Builder(SolverContext ctx) {
 			this.ctx = ctx;
 			this.origin = ctx.data();
-			this.full = new MatrixData();
+			full = new MatrixData();
+			full.demand = origin.demand;
 		}
 
-		void build() {
-
-			var procLibs = new HashSet<String>();
-			for (var techFlow : origin.techIndex) {
-				if (techFlow.isFromLibrary()) {
-					procLibs.add(techFlow.library());
-				}
-			}
-			if (!procLibs.isEmpty()) {
-				fillInventory(procLibs);
-			}
-
-			// TODO: costs & LCIA factors
+		MatrixData build() {
+			fillInventory();
+			fillImpactModel();
 
 			if (full.techIndex == null) {
 				full.techIndex = origin.techIndex;
 			}
+			if (full.techMatrix == null) {
+				full.techMatrix = origin.techMatrix;
+			}
 
+			if (full.enviIndex == null) {
+				full.enviIndex = origin.enviIndex;
+			}
+			if (full.enviMatrix == null) {
+				full.enviMatrix = origin.enviMatrix;
+			}
+
+			if (full.costVector == null) {
+				full.costVector = origin.costVector;
+			}
+
+			if (full.impactIndex == null) {
+				full.impactIndex = origin.impactIndex;
+			}
+			if (full.impactMatrix == null) {
+				full.impactMatrix = origin.impactMatrix;
+			}
+			return full;
 		}
 
-		private void fillInventory(HashSet<String> libs) {
+		private void fillImpactModel() {
+			if (MatrixIndex.isAbsent(origin.impactIndex)
+					|| MatrixIndex.isAbsent(full.enviIndex))
+				return;
+			var impIdx = full.impactIndex = origin.impactIndex;
+			var enviIdx = full.enviIndex;
+			var impBuffer = new MatrixBuilder();
+			impBuffer.minSize(impIdx.size(), enviIdx.size());
+			mapImpactMatrix(impIdx, origin.enviIndex, origin.impactMatrix, impBuffer);
+
+			var libs = new HashSet<String>();
+			for (var imp : impIdx) {
+				if (!imp.isFromLibrary() || libs.contains(imp.library))
+					continue;
+				libs.add(imp.library);
+				var lib = ctx.libraries().get(imp.library);
+				if (lib == null) {
+					log.error("could not find library {}", imp.library);
+					continue;
+				}
+				if (!lib.hasImpactData())
+					continue;
+				var libImpIdx = lib.impactIndex();
+				var libEnviIdx = lib.enviIndex();
+				var matrix = lib.matrixOf(LibMatrix.C);
+				if (MatrixIndex.isAbsent(libImpIdx)
+						|| MatrixIndex.isAbsent(libEnviIdx)
+						|| matrix == null)
+					continue;
+				log.info("map impact model of library {}", imp.library);
+				mapImpactMatrix(libImpIdx, libEnviIdx, matrix, impBuffer);
+			}
+		}
+
+		private void mapImpactMatrix(
+				ImpactIndex impIdx,
+				EnviIndex enviIdx,
+				MatrixReader m,
+				MatrixBuilder buffer
+		) {
+			if (impIdx == null || enviIdx == null || m == null)
+				return;
+
+			var fullImpIdx = full.impactIndex;
+			var fullEnvIdx = full.enviIndex;
+			m.iterate((row, col, val) -> {
+				var rowImp = impIdx.at(row);
+				var colFlow = enviIdx.at(col);
+				var targetRow = fullImpIdx.of(rowImp);
+				var targetCol = fullEnvIdx.of(colFlow);
+				buffer.set(targetRow, targetCol, val);
+			});
+		}
+
+		private void fillInventory() {
+			var libs = new HashSet<String>();
+			for (var techFlow : origin.techIndex) {
+				if (techFlow.isFromLibrary()) {
+					libs.add(techFlow.library());
+				}
+			}
+			if (!libs.isEmpty())
+				return;
+
 			var techBuffer = new MatrixBuilder();
 			var enviBuffer = new MatrixBuilder();
+			var costBuffer = origin.costVector != null
+					? new TDoubleArrayList(origin.techIndex.size())
+					: null;
 
 			mapTechMatrix(origin.techIndex, origin.techMatrix, techBuffer);
 			mapEnviMatrix(
 					origin.enviIndex, origin.techIndex, origin.enviMatrix, enviBuffer);
+			if (costBuffer != null) {
+				mapCosts(origin.techIndex, origin.costVector, costBuffer);
+			}
 
 			var queue = new ArrayDeque<>(libs);
 			while (!queue.isEmpty()) {
+
 				var libId = queue.poll();
 				var lib = ctx.libraries().get(libId);
+				if (lib == null) {
+					log.error("could not find library {}", libId);
+					continue;
+				}
+				log.info("map inventory of library {}", libId);
 				var libTechIdx = lib.techIndex();
 				var libA = lib.matrixOf(LibMatrix.A);
 				mapTechMatrix(libTechIdx, libA, techBuffer);
+
 				if (lib.hasEnviData()) {
 					var libB = lib.matrixOf(LibMatrix.B);
 					mapEnviMatrix(lib.enviIndex(), libTechIdx, libB, enviBuffer);
+				}
+
+				if (lib.hasCostData() && costBuffer != null) {
+					mapCosts(libTechIdx, lib.costs(), costBuffer);
 				}
 
 				for (var techFlow : libTechIdx) {
@@ -88,6 +188,16 @@ public class InMemLibrarySolver {
 						queue.add(techFlow.library());
 					}
 				}
+			}
+
+			if (!techBuffer.isEmpty()) {
+				full.techMatrix = techBuffer.finish();
+			}
+			if (!enviBuffer.isEmpty()) {
+				full.enviMatrix = enviBuffer.finish();
+			}
+			if (costBuffer != null && !costBuffer.isEmpty()) {
+				full.costVector = costBuffer.toArray();
 			}
 		}
 
@@ -127,6 +237,8 @@ public class InMemLibrarySolver {
 			fullEnviIdx.addAll(enviIdx);
 			var fullTechIdx = full.techIndex;
 			buffer.minSize(fullEnviIdx.size(), fullTechIdx.size());
+			if (m == null)
+				return;
 
 			m.iterate((row, col, val) -> {
 				var rowFlow = enviIdx.at(row);
@@ -135,6 +247,20 @@ public class InMemLibrarySolver {
 				var targetCol = fullTechIdx.of(colFlow);
 				buffer.set(targetRow, targetCol, val);
 			});
+		}
+
+		private void mapCosts(
+				TechIndex idx, double[] costs, TDoubleArrayList buffer
+		) {
+			var fullIdx = full.techIndex;
+			buffer.ensureCapacity(fullIdx.size());
+			if (costs == null)
+				return;
+			for (var col = 0; col < costs.length; col++) {
+				var techFlow = idx.at(col);
+				var targetCol = fullIdx.of(techFlow);
+				buffer.set(targetCol, costs[col]);
+			}
 		}
 	}
 }
