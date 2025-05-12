3e4e078afb2011f919c3dbc324965ac1f06e3019
msrocka <michael.srocka@gmail.com>
2025-05-12 11:41:57 +0200

    bug: JSON import generates 2 times the reference currency

---
+++ olca-core/src/test/java/org/openlca/jsonld/io/RefCurrencyTest.java
@@ -0,0 +1,51 @@
+package org.openlca.jsonld.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openlca.core.Tests;
+import org.openlca.core.database.IDatabase;
+import org.openlca.core.model.Currency;
+import org.openlca.jsonld.MemStore;
+import org.openlca.jsonld.input.JsonImport;
+import org.openlca.jsonld.output.JsonExport;
+
+public class RefCurrencyTest {
+
+	private final IDatabase db = Tests.getDb();
+
+	@Test
+	public void testRefLink() {
+		var eur = Currency.of("EUR");
+		eur.referenceCurrency = eur;
+		var pln = Currency.of("PLN");
+		pln.referenceCurrency = eur;
+		pln.conversionFactor = 0.25;
+		db.insert(eur, pln);
+
+		var store = new MemStore();
+		var exp = new JsonExport(db, store);
+		exp.write(eur);
+		exp.write(pln);
+		db.clear();
+
+		new JsonImport(store, db).run();
+
+		var cs = db.getAll(Currency.class);
+		assertEquals(2, cs.size());
+		eur = cs.stream()
+				.filter(c -> c.name.equals("EUR"))
+				.findFirst()
+				.orElseThrow();
+		pln = cs.stream()
+				.filter(c -> c.name.equals("PLN"))
+				.findFirst()
+				.orElseThrow();
+
+		assertSame(eur, eur.referenceCurrency);
+		assertSame(eur, pln.referenceCurrency);
+
+		db.delete(eur, pln);
+	}
+}
+
