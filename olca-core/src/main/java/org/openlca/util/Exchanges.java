d622a39d4d6d2ca0d1f67d1303721fc5d1fde34d
msrocka <michael.srocka@gmail.com>
2025-11-13 12:15:27 +0100

    Exchanges: allow AbstractExchange instances
    also, make the methods a bit easier to read. We do not need
    the `isAvoided` check in the `isProviderFlow` test, as
    avoided are "linkable" and thus, cannot ever be provider flows

--- olca-core/src/main/java/org/openlca/util/Exchanges.java
+++ olca-core/src/main/java/org/openlca/util/Exchanges.java
@@ -1,6 +1,6 @@
 package org.openlca.util;
 
-import org.openlca.core.model.Exchange;
+import org.openlca.core.model.AbstractExchange;
 import org.openlca.core.model.FlowType;
 
 public class Exchanges {
@@ -8,35 +8,37 @@ public class Exchanges {
 	private Exchanges() {
 	}
 
-	/**
-	 * A provider flow is a product output or waste input of a process
-	 * (which is not tagged as avoided flow).
-	 */
-	public static boolean isProviderFlow(Exchange e) {
-		if (e == null || e.isAvoided || e.flow == null)
-			return false;
-		var type = e.flow.flowType;
-		if (type == null || type == FlowType.ELEMENTARY_FLOW)
-			return false;
-		return (type == FlowType.PRODUCT_FLOW) != e.isInput;
-	}
-
-	public static boolean isLinkable(Exchange e) {
+	/// A provider flow is a product output or waste input of a process. They
+	/// can be linked to exchanges with the same flow of the opposite direction.
+	public static boolean isProviderFlow(AbstractExchange e) {
 		if (e == null || e.flow == null)
 			return false;
-		var type = e.flow.flowType;
-		if (type == null || type == FlowType.ELEMENTARY_FLOW)
-			return false;
-		return (type == FlowType.PRODUCT_FLOW) == e.isInput;
+		return switch (e.flow.flowType) {
+			case PRODUCT_FLOW -> !e.isInput;
+			case WASTE_FLOW -> e.isInput;
+			case null, default -> false;
+		};
 	}
 
-	public static boolean isProduct(Exchange e) {
+	/// Returns `true` when the given exchange can be linked to a provider. This
+	/// is the case, when it is a product input or waste output.
+	public static boolean isLinkable(AbstractExchange e) {
+		if (e == null || e.flow == null)
+			return false;
+		return switch (e.flow.flowType) {
+			case PRODUCT_FLOW -> e.isInput;
+			case WASTE_FLOW -> !e.isInput;
+			case null, default -> false;
+		};
+	}
+
+	public static boolean isProduct(AbstractExchange e) {
 		return e != null
 				&& e.flow != null
 				&& e.flow.flowType == FlowType.PRODUCT_FLOW;
 	}
 
-	public static boolean isWaste(Exchange e) {
+	public static boolean isWaste(AbstractExchange e) {
 		return e != null
 				&& e.flow != null
 				&& e.flow.flowType == FlowType.WASTE_FLOW;
