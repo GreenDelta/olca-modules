836ffdea1e10d6a24e20f5ab97bfad3cef79e200
Sebastian Greve <greve@greendelta.com>
2026-01-19 15:54:18 +0100

    Added sql for retrieving baseline parameter redef set for a system id

--- olca-core/src/main/java/org/openlca/util/ParameterRedefSets.java
+++ olca-core/src/main/java/org/openlca/util/ParameterRedefSets.java
@@ -1,5 +1,6 @@
 package org.openlca.util;
 
+import org.openlca.commons.Strings;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.NativeSql;
 import org.openlca.core.model.ModelType;
@@ -60,4 +61,41 @@ public class ParameterRedefSets {
 
 		return set;
 	}
+	
+	/**
+	 * Loads the baseline parameter redef set of the product system with the given systemId
+	 */
+	public static ParameterRedefSet baselineOf(IDatabase db, long systemId) {
+		var set = new ParameterRedefSet();
+		set.name = "Baseline";
+		set.isBaseline = true;
+		if (db == null || systemId  == 0l)
+			return set;
+		
+		var sql = "select " +
+			/* 1 */ "name, " +
+			/* 2 */ "description, " +
+			/* 3 */ "value , " +
+			/* 4 */ "context_type, " +
+			/* 5 */ "f_context from tbl_parameter_redefs where f_owner = " +
+			/* 6 */ "(select id from tbl_parameter_redef_sets where is_baseline = 1 " +
+			/* 7 */	"and f_product_system = " + systemId + ")";
+		
+		NativeSql.on(db).query(sql, r -> {
+			var redef = new ParameterRedef();
+			redef.name = r.getString(1);
+			redef.description = r.getString(2);
+			redef.value = r.getDouble(3);
+			var typeStr = r.getString(4);
+			if (Strings.isNotBlank(typeStr)) {
+				redef.contextType = ModelType.valueOf(typeStr);
+				redef.contextId = r.getLong(5);
+			}			
+			set.parameters.add(redef);
+			return true;
+		});
+
+		return set;
+	}
+	
 }
